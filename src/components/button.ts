3f5401bb878d90adbed476b8571e695e88c27569
William Geller <wgeller@nvidia.com>
2022-01-18 13:08:10 -0500

    chore: init commit of minimal scaffold - Includes storybook / api-viewer demo - Embeds Figma in Storybook - Utilizes Vite dev server - Components and Stories for: Button / Icon / Card
    Co-authored-by: Kabriel Robichaux <krobichaux@nvidia.com>

---
+++ src/components/button.ts
@@ -0,0 +1,57 @@
+import { html, css, LitElement } from 'lit';
+import { when } from 'lit/directives/when.js';
+import { property, customElement } from 'lit/decorators.js';
+import { IconNames } from './svg-icon';
+
+@customElement('nve-button')
+export class Button extends LitElement {
+  @property({ type: String, reflect: true }) label = 'button';
+  @property({ type: Boolean, reflect: true }) disabled = false;
+  @property({ reflect: true}) icon: IconNames;
+  @property({ type: Boolean, reflect: true }) prefixIcon = false;
+
+  static styles = [
+    css`
+      button {
+        display: inline-flex;
+        align-items: center;
+        appearance: none;
+        border: none;
+        cursor: pointer;
+
+        background-color: var(--nve-color-brand-500);
+        border-radius: var(--nve-radius-full);
+        color: var(--nve-color-neutral-50);
+        font-size: var(--nve-font-size-200);
+        gap: var(--nve-spacing-2);
+        padding: var(--nve-spacing-2) var(--nve-spacing-4);
+      }
+
+      button:disabled {
+        background-color: var(--nve-color-neutral-500);
+        cursor: auto;
+      }
+
+      :host([prefixIcon]) button {
+        flex-direction: row-reverse;
+      }
+    `
+  ];
+
+  render() {
+    console.log(this.icon);
+    return html`
+      <button part="base" ?disabled=${this.disabled}>
+        ${this.label}
+        <slot></slot>
+        ${when(this.icon, () => html`<nve-svg-icon variant="current" .name="${this.icon}"></nve-svg-icon>`)}
+      </button>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'nve-button': Button,
+  }
+}
