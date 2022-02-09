9a98ce4946917e3f72f4938a769feca863eb4cb0
William Geller <wgeller@nvidia.com>
2022-02-09 12:50:57 -0500

    chore(core): setup storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,15 +7,29 @@ module.exports = {
     'storybook-dark-mode',
     'storybook-addon-designs',
     '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: '@storybook/web-components',
   core: {
     builder: 'storybook-builder-vite',
   },
+  features: {
+    interactionsDebugger: true,
+  },
   async viteFinal(config) {
     config.server.port = 7777;
     config.server.hmr = {
       protocol: 'ws',
+    };
+
+    config.optimizeDeps = {
+      ...(config.optimizeDeps || {}),
+      include: [
+        ...(config?.optimizeDeps?.include || []),
+        // Fix: `@storybook/addon-interactions` exports is not defined or `jest-mock` does not provide an export named 'fn'
+        // Fix: fn() is not defined, see: https://github.com/storybookjs/storybook/issues/15391
+        'jest-mock',
+      ],
     };
 
     return config;
