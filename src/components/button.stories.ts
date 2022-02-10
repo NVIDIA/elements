ff04e3a80e22bfa382f66e5035d105ac9c09fa4d
William Geller <wgeller@nvidia.com>
2022-02-10 13:32:58 -0500

    chore(elements): integrate new icon Set
    - adds a preview of all icons
    - updates icon script to cleanup outlined icons

--- src/components/button.stories.ts
+++ src/components/button.stories.ts
@@ -4,9 +4,9 @@ import { expect } from '@storybook/jest';
 import { userEvent, within } from '@storybook/testing-library';
 
 import { awaitTimeout, generateFigmaEmbed } from '../util/storybook-utils';
-
+import { ICON_NAMES } from '../generated/icon-names';
+import { IconNames } from './svg-icon';
 import { Button }  from './button';
-import { IconNames, ICON_NAMES } from './svg-icon';
 const _components = { Button };
 
 const figmaEmbedNodeId = '163%3A25';
@@ -45,7 +45,7 @@ interface ArgTypes {
 }
 
 export const Default = {
-  render: (args: ArgTypes) => html`<nve-button data-testid="button" .label=${args.label} .disabled=${args.disabled} .icon=${args.icon} ?prefixIcon=${args.prefixIcon}>${args.content}</nve-button>`,
+  render: (args: ArgTypes) => html`<nve-button data-testid="button" label=${args.label} ?disabled=${args.disabled} icon=${args.icon} ?prefixIcon=${args.prefixIcon}>${args.content}</nve-button>`,
   parameters: generateFigmaEmbed(figmaEmbedNodeId),
   args: { label: 'My Story ', disabled: false, content: '' },
   play: async ({ args, canvasElement }) => {
@@ -79,7 +79,7 @@ export const ButtonWithIcon = { ...Default, args: { label: 'Copy', icon: 'copy',
 
 export const ButtonWithIconSlotted = {
   render: (args: ArgTypes) => html`
-  <nve-button .label=${args.label} .disabled=${args.disabled}>
+  <nve-button label=${args.label} ?disabled=${args.disabled}>
     <nve-svg-icon variant="current" name="arrow"></nve-svg-icon>
   </nve-button>`,
   parameters: generateFigmaEmbed(figmaEmbedNodeId),
