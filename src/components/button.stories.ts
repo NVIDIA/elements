30562a008343b940bf9cffc26522b87db41a2eb8
William Geller <wgeller@nvidia.com>
2022-02-10 13:32:58 -0500

    chore(core): integrate new icon set
    - Adds a preview of all icons
    - Updates icon script to cleanup outlined icons
    - Also adds variants to button

--- src/components/button.stories.ts
+++ src/components/button.stories.ts
@@ -6,7 +6,7 @@ import { userEvent, within } from '@storybook/testing-library';
 import { awaitTimeout, generateFigmaEmbed } from '../util/storybook-utils';
 import { ICON_NAMES } from '../generated/icon-names';
 import { IconNames } from './svg-icon';
-import { Button }  from './button';
+import { Button, ButtonVariants, BUTTON_VARIANTS }  from './button';
 const _components = { Button };
 
 const figmaEmbedNodeId = '163%3A25';
@@ -25,19 +25,24 @@ export default {
       }
     },
     actions: {
-      handles: ['mouseover mlv-button', 'mouseout mlv-button', 'click mlv-button'],
+      handles: ['mouseover mlv-button', 'mouseout mlv-button', 'mousedown mlv-button', 'click mlv-button'],
     },
   },
-  argTypes: {
+  argTypes: { // ******* TODO: Track this github issue https://github.com/storybookjs/storybook/issues/17063 (bug in 6.4 that resets radio/select args to !undefined)
     icon: {
       control: 'inline-radio',
       options: ICON_NAMES
+    },
+    variant: {
+      control: 'inline-radio',
+      options: BUTTON_VARIANTS
     }
   }
 };
 
 interface ArgTypes {
   label?: string;
+  variant?: ButtonVariants;
   content?: string;
   disabled?: boolean;
   icon?: IconNames;
@@ -45,9 +50,18 @@ interface ArgTypes {
 }
 
 export const Default = {
-  render: (args: ArgTypes) => html`<mlv-button data-testid="button" label=${args.label} ?disabled=${args.disabled} icon=${args.icon} ?prefixIcon=${args.prefixIcon}>${args.content}</mlv-button>`,
+  render: (args: ArgTypes) => html`<mlv-button data-testid="button" label=${args.label} variant=${args.variant} ?disabled=${args.disabled} icon=${args.icon} ?prefixIcon=${args.prefixIcon}>${args.content}</mlv-button>`,
   parameters: generateFigmaEmbed(figmaEmbedNodeId),
-  args: { label: 'My Story ', disabled: false, content: '' },
+  args: { label: 'Primary', disabled: false, content: '', variant: 'primary' }
+};
+
+export const Secondary = { ...Default, args: { label: 'Secondary', variant: 'secondary' } };
+export const Destructive = { ...Default, args: { label: 'Destructive', variant: 'destructive' } };
+export const Disabled = { ...Default, args: { label: 'Disabled Button', disabled: true } };
+export const SlottedText = { ...Default, args: { content: 'Slotted Text' } };
+export const ButtonWithIcon = { ...Default, args: { label: 'Copy', icon: 'copy', prefixIcon: false } };
+
+export const simulateHoverStates = { ...Default,
   play: async ({ args, canvasElement }) => {
     const canvas = within(canvasElement);
     const button = canvas.getByTestId('button').shadowRoot.querySelector('button');
@@ -71,10 +85,6 @@ export const Default = {
     await userEvent.unhover(button);
   }
 };
-
-export const Disabled = { ...Default, args: { label: 'Disabled Button', disabled: true } };
-export const SlottedText = { ...Default, args: { content: 'Slotted Text' } };
-export const ButtonWithIcon = { ...Default, args: { label: 'Copy', icon: 'copy', prefixIcon: false } };
 
 
 export const ButtonWithIconSlotted = {
--- src/components/button.ts
+++ src/components/button.ts
@@ -4,12 +4,17 @@ import { when } from 'lit/directives/when.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { IconNames } from './svg-icon';
 
+export const BUTTON_VARIANTS = ['primary', 'secondary', 'destructive'];
+export type ButtonVariants = typeof BUTTON_VARIANTS[number];
+
 @customElement('mlv-button')
 export class Button extends LitElement {
   /**  Label txt for the button */
   @property({ type: String, reflect: true }) label = 'button';
   /** If 'disabled' attribute present on element, set disabled state on button */
   @property({ type: Boolean, reflect: true }) disabled = false;
+  /** Color Variant of the Button */
+  @property({ type: String, reflect: true }) variant: ButtonVariants = 'primary';
   /** If present use icon for name of icon to show */
   @property({ reflect: true}) icon: IconNames;
   /** If 'prefixicon' attribute present on element, show icon on left */
@@ -23,34 +28,60 @@ export class Button extends LitElement {
         display: inline-flex;
         align-items: center;
         appearance: none;
-        border: none;
+        border: 4px solid transparent;
         cursor: pointer;
 
-        background-color: var(--mlv-color-brand-500);
         border-radius: var(--mlv-radius-full);
         color: var(--mlv-color-neutral-50);
-        font-size: var(--mlv-font-size-200);
+        font-size: var(--mlv-font-size-75);
+        font-weight: var(--mlv-font-weight-semi-bold);
         gap: var(--mlv-spacing-2);
         padding: var(--mlv-spacing-2) var(--mlv-spacing-4);
-      }
-
-      button:disabled {
-        background-color: var(--mlv-color-neutral-500);
-        cursor: auto;
-      }
-
-      button.hover:not(:disabled) {
-        background-color: var(--mlv-color-negative-500);
       }
 
       :host([prefixIcon]) button {
         flex-direction: row-reverse;
       }
+
+      button:focus-visible, button:active {
+        border-color:var(--text-color);
+      }
+
+      button.primary {
+        background-color: var(--mlv-system-color-interactive-primary);
+      }
+
+      button.primary.hover {
+        background-color: var(--mlv-system-color-interactive-primary-hover);
+      }
+      button.destructive {
+        background-color: var(--mlv-system-color-interactive-destructive);
+      }
+
+      button.destructive.hover {
+        background-color: var(--mlv-system-color-interactive-destructive-hover);
+      }
+
+      button.secondary {
+        color: var(--background-color);
+        background-color: var(--text-color);
+      }
+
+      button.secondary.hover {
+        background-color: var(--background-color);
+        color: var(--text-color);
+      }
+
+      button:disabled {
+        background-color: var(--mlv-system-color-interactive-disabled);
+        color: var(--mlv-system-color-interactive-on-disabled);
+        cursor: auto;
+      }
     `
   ];
 
   render() {
-    const classes = { hover: this._hover };
+    const classes = { hover: this._hover, primary: this.variant === 'primary' || !this.variant, secondary: this.variant === 'secondary', destructive: this.variant === 'destructive' };
 
     return html`
       <button class=${classMap(classes)} @mouseover="${() => this._hover = true}" @mouseout="${() => this._hover = false}" ?disabled=${this.disabled}>
