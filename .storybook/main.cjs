9ccbaf347a724e1e3713766e897c9924fe10c022
John Yanarella <jyanarella@nvidia.com>
2022-02-25 09:57:59 -0800

    chore(core): upgrade storybook
    - added some explicit dependencies to counter phantom dependencies from root package

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   // stories: [
@@ -7,19 +9,22 @@ module.exports = {
     'storybook-dark-mode',
     'storybook-addon-designs',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    '@storybook/addon-interactions'
   ],
   framework: '@storybook/web-components',
   core: {
-    builder: 'storybook-builder-vite',
+    builder: 'storybook-builder-vite'
   },
   features: {
-    interactionsDebugger: true,
+    interactionsDebugger: true
   },
   async viteFinal(config) {
+    // Workaround: https://github.com/storybookjs/storybook/issues/10887#issuecomment-901109891
+    config.resolve.dedupe = ['@storybook/client-api'];
+
     config.server.port = 7777;
     config.server.hmr = {
-      protocol: 'ws',
+      protocol: 'ws'
     };
 
     config.optimizeDeps = {
@@ -28,10 +33,21 @@ module.exports = {
         ...(config?.optimizeDeps?.include || []),
         // Fix: `@storybook/addon-interactions` exports is not defined or `jest-mock` does not provide an export named 'fn'
         // Fix: fn() is not defined, see: https://github.com/storybookjs/storybook/issues/15391
-        'jest-mock',
-      ],
+        'jest-mock'
+      ]
+    };
+
+    // TODO: Revisit when Storybook 6.5 ships with a fix for impacts from Emotion 11 release on docs addon
+    // Workaround: https://github.com/eirslett/storybook-builder-vite/issues/219#issuecomment-1023666193
+    // Related: https://github.com/storybookjs/storybook/issues/16716
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@emotion/react': path.resolve(path.join(__dirname, '../node_modules/@emotion/react')),
+      '@emotion/styled': path.resolve(path.join(__dirname, '../node_modules/@emotion/styled')),
+      '@emotion/core': path.resolve(path.join(__dirname, '../node_modules/@emotion/react')),
+      'emotion-theming': path.resolve(path.join(__dirname, '../node_modules/@emotion/react'))
     };
 
     return config;
-  },
+  }
 };
