4f737279c2e1776671f09d3b14b869508beabb7b
William Geller <wgeller@nvidia.com>
2022-03-08 14:25:42 -0500

    chore(core): button
    - Changes variants to use attribute selectors
    - Use psuedo CSS hover class instead of state
    - Remove @storybook/jest to fix build

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,7 +1,6 @@
 import { html, css, LitElement } from 'lit';
 import { property, state, customElement } from 'lit/decorators.js';
 import { when } from 'lit/directives/when.js';
-import { classMap } from 'lit/directives/class-map.js';
 import { IconNames } from './svg-icon';
 
 export const BUTTON_VARIANTS = ['primary', 'secondary', 'destructive'];
@@ -10,17 +9,17 @@ export type ButtonVariants = typeof BUTTON_VARIANTS[number];
 @customElement('nve-button')
 export class Button extends LitElement {
   /**  Label txt for the button */
-  @property({ type: String, reflect: true }) label = 'button';
+  @property({ type: String}) label = 'button';
   /** If 'disabled' attribute present on element, set disabled state on button */
-  @property({ type: Boolean, reflect: true }) disabled = false;
+  @property({ type: Boolean }) disabled = false;
   /** Color Variant of the Button */
   @property({ type: String, reflect: true }) variant: ButtonVariants = 'primary';
   /** If present use icon for name of icon to show */
-  @property({ reflect: true}) icon: IconNames;
+  @property() icon: IconNames;
   /** If 'prefixicon' attribute present on element, show icon on left */
-  @property({ type: Boolean, reflect: true }) prefixIcon = false;
+  @property({ type: Boolean }) prefixIcon = false;
 
-  @state() private _hover = false;
+  @state() private _hover = false; // No longer used but left for testing/demoing private field exclusion from Storybook API Docs
 
   static styles = [
     css`
@@ -30,61 +29,65 @@ export class Button extends LitElement {
         appearance: none;
         border: 4px solid transparent;
         cursor: pointer;
-
+        background-color: var(--nve-system-color-interactive-primary);
         border-radius: var(--nve-radius-full);
         color: var(--nve-color-neutral-50);
         font-size: var(--nve-font-size-75);
         font-weight: var(--nve-font-weight-semi-bold);
         gap: var(--nve-spacing-2);
         padding: var(--nve-spacing-2) var(--nve-spacing-4);
+        line-height: var(--nve-font-size-200);
       }
 
       :host([prefixIcon]) button {
         flex-direction: row-reverse;
       }
 
-      button:focus-visible, button:active {
-        border-color:var(--text-color);
+      button:focus-visible {
+        border-color: var(--nve-system-color-interactive-secondary);
       }
 
-      button.primary {
+      button:active {
+        filter: contrast(1.25) brightness(1.1);
+      }
+
+      :host([variant="primary"]) button {
         background-color: var(--nve-system-color-interactive-primary);
       }
 
-      button.primary.hover {
+      :host([variant="primary"]) button:hover {
         background-color: var(--nve-system-color-interactive-primary-hover);
       }
-      button.destructive {
+      :host([variant="destructive"]) button {
         background-color: var(--nve-system-color-interactive-destructive);
       }
 
-      button.destructive.hover {
+      :host([variant="destructive"]) button:hover {
         background-color: var(--nve-system-color-interactive-destructive-hover);
       }
 
-      button.secondary {
-        color: var(--background-color);
-        background-color: var(--text-color);
+      :host([variant="secondary"]) button {
+        color: var(--nve-system-color-interactive-neutral-hover);
+        background-color: var(--nve-system-color-interactive-neutral);
       }
 
-      button.secondary.hover {
-        background-color: var(--background-color);
-        color: var(--text-color);
+      :host([variant="secondary"]) button:hover {
+        color: var(--nve-system-color-interactive-neutral);
+        background-color: var(--nve-system-color-interactive-neutral-hover);
       }
 
-      button:disabled {
+      :host button:disabled {
         background-color: var(--nve-system-color-interactive-disabled);
+        opacity: 40%;
         color: var(--nve-system-color-interactive-on-disabled);
-        cursor: auto;
+        cursor: not-allowed;
       }
     `
   ];
 
   render() {
-    const classes = { hover: this._hover, primary: this.variant === 'primary' || !this.variant, secondary: this.variant === 'secondary', destructive: this.variant === 'destructive' };
-
     return html`
-      <button class=${classMap(classes)} @mouseover="${() => this._hover = true}" @mouseout="${() => this._hover = false}" ?disabled=${this.disabled}>
+      <button ?disabled=${this.disabled}>
         ${this.label}
         <slot></slot>
         ${when(this.icon, () => html`<nve-svg-icon variant="current" name="${this.icon}"></nve-svg-icon>`)}
