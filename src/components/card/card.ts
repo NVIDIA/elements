2d4205830e8f04e38a3c8090d37353310f38b158
William Geller <wgeller@nvidia.com>
2022-03-17 11:02:03 -0400

    chore(core): postCSS nesting & mixing
    - Introduces PostCSS, PostCSS Nested Plugin, PostCSS Mixins Plugin
    - Reduces Component CSS and Increases Readability via Nesting & Mixins
    - Externalizes CSS to a separate file from component implementation
    - Restructures components folder to use a nested and grouped directory organization
    - Adds a custom script configured to hot reload externalized CSS
    - Updates button attribute for iconPlacement
    - Additional work on Header Titles of Card component

---
+++ src/components/card/card.ts
@@ -0,0 +1,51 @@
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import styleSheet from './card.css';
+
+
+const componentStyling = new CSSStyleSheet();
+componentStyling.replace(styleSheet);
+
+
+/**
+ * @slot default/unnamed - This is a default/unnamed slot for card content
+ * @slot image - HTML elements slotted here for image header
+ * @slot footer - HTML elements slotted here for card footer
+*/
+
+@customElement('mlv-card')
+export class Card extends LitElement {
+  static styles = componentStyling;
+
+
+  /**  If present render a title in the header */
+  @property({ type: String}) title: string;
+  /**  If present render a subTitle in the header */
+  @property({ type: String}) subTitle: string;
+
+  render() {
+    return html`
+      <slot name="header" id="header">
+       <h1 id="title">${this.title}</h1>
+
+       <h2 id="subTitle">${this.subTitle}</h2>
+      </slot>
+
+      <slot name="image"></slot>
+
+      <div id="content">
+        <slot></slot>
+      </div>
+
+      <div id="footer">
+        <slot name="footer"></slot>
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mlv-card': Card;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './components/svg-icon';
-export * from './components/button';
-export * from './components/card';
+export * from './components/svg-icon/svg-icon';
+export * from './components/button/button';
+export * from './components/card/card';
