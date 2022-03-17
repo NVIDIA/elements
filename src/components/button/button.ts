fa13e4ec653b9e0b3d720ee0fe06faba61a27024
William Geller <wgeller@nvidia.com>
2022-03-17 11:02:03 -0400

    chore(elements): postCSS nesting & mixing
    - Introduces PostCSS, PostCSS Nested Plugin, PostCSS Mixins Plugin
    - Reduces Component CSS and Increases Readability via Nesting & Mixins
    - Externalizes CSS to a separate file from component implementation
    - Restructures components folder to use a nested and grouped directory organization
    - Adds a custom script configured to hot reload externalized CSS
    - Updates button attribute for iconPlacement
    - Additional work on Header Titles of Card component

---
+++ src/components/button/button.ts
@@ -0,0 +1,46 @@
+import { html, LitElement } from 'lit';
+import { property, state, customElement } from 'lit/decorators.js';
+import { when } from 'lit/directives/when.js';
+import { IconNames } from '../svg-icon/svg-icon';
+import styleSheet from './button.css';
+
+export enum ButtonVariants { Primary = 'primary', Secondary = 'secondary', Destructive = 'destructive' }
+export enum IconPlacements { Trailing = 'trailing', Leading = 'leading' }
+
+const componentStyling = new CSSStyleSheet();
+componentStyling.replace(styleSheet);
+
+
+@customElement('nve-button')
+export class Button extends LitElement {
+  static styles = componentStyling;
+
+  /**  Label txt for the button */
+  @property({ type: String }) label = 'button';
+  /** If 'disabled' attribute present on element, set disabled state on button */
+  @property({ type: Boolean }) disabled = false;
+  /** Color Variant of the Button */
+  @property({ type: String, reflect: true }) variant: ButtonVariants = ButtonVariants.Primary;
+  /** If present use icon for name of icon to show */
+  @property({ type: String }) icon: IconNames;
+  /** Left or right placement of the icon, defaults to 'trailing' */
+  @property({ type: String }) iconPlacement: IconPlacements = IconPlacements.Trailing;
+
+  @state() private _hover = false; // No longer used but left for testing/demoing private field exclusion from Storybook API Docs
+
+  render() {
+    return html`
+      <button ?disabled=${this.disabled}>
+        ${this.label}
+        <slot></slot>
+        ${when(this.icon, () => html`<nve-svg-icon variant="current" name="${this.icon}"></nve-svg-icon>`)}
+      </button>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'nve-button': Button,
+  }
+}
