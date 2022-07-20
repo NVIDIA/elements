299089fcac0a69cc20de5cbe647b13fda294da6b
John Yanarella <jyanarella@nvidia.com>
2022-07-20 09:57:49 -0700

    chore: platform integrations

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import { setCustomElementsManifest } from '@storybook/web-components';
+import { setCustomElementsManifest } from '@elements/custom-elements-storybook/preview';
 import { themes } from '@storybook/theming';
 import { excludePrivateFields } from '@elements/elements/internal';
 import customElements from '@elements/elements/custom-elements.json';
