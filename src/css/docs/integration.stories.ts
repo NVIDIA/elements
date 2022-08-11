7b13ced4e7706b0c3f6bef22139757f1de56d31b
Cory Rylan <crylan@nvidia.com>
2022-08-11 12:09:36 -0500

    feat(core): typography utilities
    - add CSS utilities that represent the composition tokens from figma
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ src/css/docs/integration.stories.ts
@@ -0,0 +1,30 @@
+import { html, unsafeCSS, LitElement } from 'lit';
+import { customElement } from 'lit/decorators/custom-element.js';
+import typography from '@elements/elements/css/module.typography.css';
+import layout from '@elements/elements/css/module.layout.css';
+
+export default {
+  title: 'Foundation/Examples/Integration'
+};
+
+@customElement('my-element')
+class MyElement extends LitElement {
+
+  static styles = [unsafeCSS(`${typography}${layout}`)];
+
+  render() {
+    return html`
+      <div nve-layout="column gap:lg">
+        <p nve-text="display">display</p>
+        <p nve-text="heading">heading</p>
+        <p nve-text="body">body</p>
+        <p nve-text="label">label</p>
+        <p nve-text="eyebrow">eyebrow</p>
+      </div>
+    `;
+  }
+}
+
+export const Lit = {
+  render: () => html`<my-element></my-element>`
+}
