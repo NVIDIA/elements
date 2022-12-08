64d2009a954df3f6a81e0bc8be3a913f55b08c29
Cory Rylan <crylan@nvidia.com>
2022-12-08 13:36:35 -0600

    fix(elements): popover / control fixes
    - fix form group label alignment when using inline labels
    - fix default dropdown alignment and offset spacing
    - fix missing side effect registration for popovers using icons and icon buttons
    - fix alignment issue with form controls and button control within same control group
    - fix default font size for control text types
    - fix wait until animation complete to remove notification snackbar element
    - fix popover alignments with center content and scrollable body content
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/notification/define.ts
+++ projects/elements/src/notification/define.ts
@@ -1,4 +1,5 @@
 import { Notification, NotificationGroup } from '@elements/elements/notification';
+import '@elements/elements/icon-button/define.js';
 
 customElements.get('nve-notification') || customElements.define('nve-notification', Notification);
 customElements.get('nve-notification-group') || customElements.define('nve-notification-group', NotificationGroup);
