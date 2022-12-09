ad8197e6c36d535959eef6fe83dc847c2f0829c1
Cory Rylan <crylan@nvidia.com>
2022-12-09 16:33:53 -0600

    chore(core): ts and static bundle demos
    - add static bundle demo for non-js based envs
    - add typescript demo for basic js based envs
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/demos/typescript/src/index.ts
@@ -0,0 +1,13 @@
+import type { Dialog } from '@elements/elements/dialog';
+import type { Button } from '@elements/elements/button';
+import '@elements/elements/button/define.js';
+import '@elements/elements/dialog/define.js';
+import './index.css';
+
+const dialog = document.querySelector<Dialog>('mlv-dialog');
+const button = document.querySelector<Button>('mlv-button');
+
+if (button && dialog) {
+  button.addEventListener('click', () => dialog.hidden = false);
+  dialog.addEventListener('close', () => dialog.hidden = true);
+}
