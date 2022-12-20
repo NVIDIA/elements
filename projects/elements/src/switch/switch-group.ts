2d422c2ea9261da41fcd3fdf8cc4ee95ddfd78ed
Cory Rylan <crylan@nvidia.com>
2022-12-20 12:38:03 -0600

    chore: update vite 4 and wireit
    - update to latest vite 4 + rollup 3
    - update project to wireit
    - update demos to leverage wireit library dependency
    - update eslit support for lit
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/switch/switch-group.ts
+++ projects/elements/src/switch/switch-group.ts
@@ -1,7 +1,8 @@
+import type { CSSResult } from 'lit';
 import { useStyles } from '@elements/elements/internal';
 import { ControlGroup } from '@elements/elements/forms';
 import styles from './switch-group.css?inline';
 
 export class SwitchGroup extends ControlGroup {
-  static styles = useStyles([...ControlGroup.styles, styles]);
+  static styles: CSSResult[] = useStyles([...ControlGroup.styles, styles]);
 }
