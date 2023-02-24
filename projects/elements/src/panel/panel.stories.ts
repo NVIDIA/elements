e85cc8336ce1f4ab0d94a8d1e6b80225f859e8b9
William Geller <wgeller@nvidia.com>
2023-02-24 11:23:47 -0500

    feat(core): panel
    - creates a new panel elements and associated sub-components
    - stilizes expanded Controlled
    - supports collapsable or dismissible modes

---
+++ projects/elements/src/panel/panel.stories.ts
@@ -0,0 +1,172 @@
+import { html } from 'lit';
+import { when } from 'lit/directives/when.js';
+import { Panel } from './panel';
+import '@elements/elements/panel/define.js';
+import '@elements/elements/button/define.js';
+import '@elements/elements/icon-button/define.js';
+import '@elements/elements/icon/define.js';
+import '@elements/elements/notification/define.js';
+
+export default {
+  title: 'Elements/Panel/Examples',
+  component: 'nve-panel',
+  parameters: { badges: ['alpha'] },
+  argTypes: {
+    width: {
+      control: { type: 'range', min: 280, max: 500 }
+    },
+    height: {
+      control: { type: 'range', min: 50, max: 500 }
+    },
+    side: {
+      control: { type: 'inline-radio', options: ['left', 'right'] }
+    }
+  }
+};
+
+interface ArgTypes {
+  width?: number;
+  height?: number;
+  expanded?: boolean;
+  closable?: boolean;
+  showActionIcon?: boolean;
+  showTrigger?: boolean;
+  showFooter?: boolean;
+  title?: string;
+  subtitle?: string;
+  side?: string;
+  theme?: string;
+}
+
+const customActionHandler = () => {
+  const notification = document.createElement('nve-notification');
+  notification.closable = true;
+  notification.status = 'success';
+  notification.innerHTML = `<h3 nve-text="label">Clicked!</h3><p nve-text="body">Custom action triggered.</p>`;
+  notification.addEventListener('close', () => notification.remove(), { once: true });
+
+
+  document.querySelector('nve-notification-group').prepend(notification);
+  setTimeout(() => notification.remove(), 2000 * (document.querySelectorAll('nve-notification').length));
+}
+
+const togglePanel = (isClosable: boolean) => {
+  const panel = document.querySelector(`nve-panel#trigger-closable-${isClosable}`) as Panel;
+  panel.expanded = !panel.expanded;
+}
+
+export const Default = {
+  render: (args: ArgTypes) => html`
+    <div nve-theme="root ${args.theme}" nve-layout="row align:space-between pad:sm">
+      <nve-panel id=${args.showTrigger ? `trigger-closable-${args.closable}` : ''} ?expanded=${args.expanded} ?closable=${args.closable} @close=${() => togglePanel(args.closable)} title=${args.title} subtitle=${args.subtitle} side=${args.side} style=${'width:' + args.width + 'px; height:' + args.height + 'px'}>
+        ${when(
+          args.showActionIcon,
+          () => html`<nve-icon-button interaction="ghost" slot="action-icon" icon-name="additional-actions" @click=${() => customActionHandler()}></nve-icon-button>`
+        )}
+
+        <nve-panel-content nve-layout="column gap:xl">
+          <div nve-layout="column gap:xs">
+            <label nve-text="body sm default muted">Release</label>
+            <p nve-text="eyebrow sm">RainbowBridge/08-18-2021AM/A2A</p>
+          </div>
+
+          <div nve-layout="column gap:xs">
+            <label nve-text="body sm default muted">Date</label>
+            <p nve-text="eyebrow sm">2021-08-18</p>
+          </div>
+
+          <div nve-layout="column gap:xs">
+            <label nve-text="body sm default muted">State</label>
+            <nve-button nve-control>Indexed</nve-button>
+          </div>
+
+          <div nve-layout="column gap:xs">
+            <label nve-text="body sm default muted">Driver</label>
+            <p nve-text="eyebrow sm">Kenjiro Ono</p>
+          </div>
+
+          <div nve-layout="column gap:xs">
+            <label nve-text="body sm default muted">Copilot</label>
+            <p nve-text="eyebrow sm">Kenichi Yoshii</p>
+          </div>
+
+          <div nve-layout="column gap:xs">
+            <label nve-text="body sm default muted">GVS</label>
+            <a href="#" nve-text="link body sm">http://testbot/testbot/view/content...</a>
+          </div>
+
+          <div nve-layout="column gap:xs">
+            <label nve-text="body sm default muted">Session ID</label>
+            <a href="#" nve-text="link body sm">Experiment 12345</a>
+          </div>
+        </nve-panel-content>
+
+        ${when(
+          args.showFooter,
+          () => html`
+          <nve-panel-footer>
+            <nve-button interaction="ghost-destructive">Destructive</nve-button>
+            <nve-button interaction="default">Default</nve-button>
+          </nve-panel-footer>
+          `
+        )}
+      </nve-panel>
+
+      ${when(
+          args.showTrigger,
+          () => html`
+            <nve-button interaction="emphasize" @click=${() => togglePanel(args.closable)}>Toggle Panel</nve-button>
+          `
+        )}
+    </div>
+
+    <nve-notification-group position="bottom" alignment="end"></nve-notification-group>
+  `,
+  args: { title: 'Details', side: 'left', expanded: true, closable: false, width: 280, height: 550 }
+};
+
+export const LeftSidePanel = {
+  ...Default,
+  args: { title: 'Left Side Panel', side: 'left', expanded: true, closable: false, width: 280, height: 550 }
+};
+
+export const RightSidePanel = {
+  ...Default,
+  args: { title: 'Right Side Panel', side: 'right', expanded: true, closable: false, width: 280, height: 550 }
+};
+
+export const ClosablePanel = {
+  ...Default,
+  args: { title: 'Closable Panel', side: 'left', expanded: true, closable: true, showTrigger: true, width: 280, height: 550 }
+};
+
+export const PanelWithTrigger = {
+  ...Default,
+  args: { title: 'Panel with Trigger', side: 'left', expanded: true, closable: false, showTrigger: true, width: 280, height: 550 }
+};
+
+export const PanelWithSubtitleAndActionIcon = {
+  ...Default,
+  args: { title: 'Title', subtitle: 'Subtitle', side: 'left', expanded: true, closable: false, showActionIcon: true, width: 280, height: 550 }
+};
+
+export const PanelWithFooter = {
+  ...Default,
+  args: { title: 'Panel Title', side: 'left', expanded: true, closable: false, showFooter: true, width: 280, height: 600 }
+};
+
+
+export const LightTheme = {
+  ...Default,
+  args: { title: 'Light Theme', side: 'left', expanded: true, closable: false, theme: 'light', width: 280, height: 600 }
+};
+
+export const DarkTheme = {
+  ...Default,
+  args: { title: 'Dark Theme', side: 'left', expanded: true, closable: false, theme: 'dark', width: 280, height: 600 }
+};
+
+export const HighContrastTheme = {
+  ...Default,
+  args: { title: 'High Contrast Theme', side: 'left', expanded: true, closable: false, theme: 'high-contrast', width: 280, height: 600 }
+};
---
+++ projects/elements/src/panel/panel.ts
@@ -0,0 +1,184 @@
+import { html, LitElement } from 'lit';
+import { property } from 'lit/decorators/property.js';
+import { when } from 'lit/directives/when.js';
+import { stateExpanded, I18nController, TypeClosableController, useStyles } from '@elements/elements/internal';
+import { IconButton } from '@elements/elements/icon-button/icon-button';
+import panelStyleSheet from './panel.css?inline';
+import panelHeaderStyleSheet from './panel-header.css?inline';
+import panelContentStyleSheet from './panel-content.css?inline';
+import panelFooterStyleSheet from './panel-footer.css?inline';
+
+
+
+/**
+ * @element nve-panel-header
+ * @slot title - Title Text
+ * @slot subtitle - Subtitle Text
+ * @slot action-icon - Extra Action Button
+ * @cssprop --padding
+ * @cssprop --border-bottom
+ * @cssprop --line-height
+ */
+export class PanelHeader extends LitElement {
+  static styles = useStyles([panelHeaderStyleSheet]);
+
+  static readonly metadata = {
+    tag: 'nve-panel-header',
+    version: 'PACKAGE_VERSION'
+  };
+
+  render() {
+    return html`
+      <header internal-host>
+        <div id="titles">
+          <slot name="title"></slot>
+          <slot name="subtitle"></slot>
+        </div>
+
+        <div id="icon-buttons">
+          <slot name="action-icon"></slot>
+          <slot name="close-button"></slot>
+        </div>
+      </header>
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback(); // Do not override connectedCallback w/out supering
+    this.slot = 'header';
+  }
+}
+
+
+/**
+ * @element nve-panel-content
+ * @slot - This is a default/unnamed slot for panel content content
+ * @cssprop --padding
+ */
+ export class PanelContent extends LitElement {
+  static styles = useStyles([panelContentStyleSheet]);
+
+  static readonly metadata = {
+    tag: 'nve-panel-content',
+    version: 'PACKAGE_VERSION'
+  };
+
+  render() {
+    return html`
+      <slot></slot>
+    `;
+  }
+}
+
+
+/**
+ * @element nve-panel-footer
+ * @slot - This is a default/unnamed slot for panel footer content
+ * @cssprop --padding
+ */
+ export class PanelFooter extends LitElement {
+  static styles = useStyles([panelFooterStyleSheet]);
+
+  static readonly metadata = {
+    tag: 'nve-panel-footer',
+    version: 'PACKAGE_VERSION'
+  };
+
+  render() {
+    return html`
+      <footer internal-host>
+        <slot></slot>
+      </footer>
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback(); // Do not override connectedCallback w/out supering
+    this.slot = 'footer';
+  }
+}
+
+
+
+/**
+ * @alpha
+ * @element nve-panel
+ * @slot - This is a default/unnamed slot for panel content
+ * @slot action-icon - Extra Action Button
+ * @slot content - content element (Use <nve-panel-content> or custom content)
+ * @slot footer - footer element (Use <nve-panel-footer> or custom content)
+ * @cssprop --background
+ * @cssprop --color
+ * @cssprop --box-shadow
+ */
+
+@stateExpanded<Panel>()
+export class Panel extends LitElement {
+  declare _internals: ElementInternals;
+
+  static styles = useStyles([panelStyleSheet]);
+
+  static readonly metadata = {
+    tag: 'nve-panel',
+    version: 'PACKAGE_VERSION'
+  };
+
+  static elementDefinitions = {
+    'nve-icon-button': IconButton,
+    'nve-panel-header': PanelHeader
+  };
+
+
+  #i18nController: I18nController<this> = new I18nController<this>(this);
+  #typeClosableController = new TypeClosableController(this);
+
+  @property({ type: Object, attribute: 'nve-i18n' }) i18n = this.#i18nController.i18n;
+  @property({ type: Boolean, reflect: true }) expanded: boolean = true;
+  @property({ type: Boolean }) closable: boolean = false;
+  @property({ type: String }) side: 'left' | 'right' = 'left';
+  @property({ type: String }) title: string;
+  @property({ type: String }) subtitle: string;
+
+  render() {
+    return html`
+      <div internal-host>
+          <nve-panel-header>
+            ${when(this.title, () => html`<div slot="title">${this.title}</div>`)}
+
+            ${when(this.subtitle, () => html`<div slot="subtitle">${this.subtitle}</div>`)}
+
+            <div slot="action-icon">
+              <slot name="action-icon"></slot>
+            </div>
+
+            ${when(
+              !this.closable,
+              () => html`
+                <nve-icon-button slot="close-button" interaction=${this.expanded ? 'ghost' : ''} icon-name=${this.expanded ? 'collapse-panel' : 'expand-panel'}
+                  @click=${() => this.expanded = !this.expanded}
+                  direction=${this.side}
+                  .expanded=${this.expanded}
+                  .ariaLabel=${this.expanded ? this.i18n.close : this.i18n.expand}
+                ></nve-icon-button>
+              `,
+              () => html`
+                <nve-icon-button slot="close-button" interaction="ghost" icon-name="cancel"
+                  @click=${() => this.#typeClosableController.close()}
+                  .expanded=${this.expanded}
+                  .ariaLabel=${this.expanded ? this.i18n.hide : this.i18n.show}
+                ></nve-icon-button>
+              `)}
+          </nve-panel-header>
+
+        <slot></slot>
+
+        <slot name="footer"></slot>
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._internals.role = 'region';
+  }
+}
