419636f1691818ab7b48bf27e79e11603806d0ab
Cory Rylan <crylan@nvidia.com>
2023-02-24 12:20:13 -0600

    fix(core): tag scoping
    - adjust tag scope fn to follow repo mlv prefix convention
    - clarify use case in docs
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/scoped/index.ts
+++ projects/elements/src/scoped/index.ts
@@ -2,7 +2,7 @@ import { ScopedRegistryHost } from '@lit-labs/scoped-registry-mixin';
 import { define } from '@elements/elements/internal';
 
 /**
- * Utility for applying mixin recursively to all custom elements, used for scoped element registry polyfills and shims
+ * Utility for applying mixin recursively to all Maglev Elements, used for scoped element registry polyfills and shims
  */
 export function scope(element: any, Mixin = ScopedRegistryHost) {
   return class extends Mixin(element as any) {
@@ -11,8 +11,8 @@ export function scope(element: any, Mixin = ScopedRegistryHost) {
 }
 
 /**
- * Utility for registering elements with a scope prefix for global scope when Custom Element Scoped Registries are not available
+ * Utility for registering MagLev Elements with a scope suffix when Custom Element Scoped Registries are not available
  */
-export function defineScopedElement(prefix: string, Element: any, Mixin = ScopedRegistryHost) {
-  define(scope(Element, Mixin) as any, { prefix });
+export function defineScopedElement(suffix: string, Element: any, Mixin = ScopedRegistryHost) {
+  define(scope(Element, Mixin) as any, { suffix });
 }
