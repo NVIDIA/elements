3abf65ce347df09bcf07a8f0b1f51f31630455a5
William Geller <wgeller@nvidia.com>
2023-03-17 14:50:48 -0400

    chore(elements): panel unit tests
    - adds robust unit testing for panel
    - adds further documentation and includes in the package.json export

---
+++ projects/elements/src/panel/panel-footer.test.ts
@@ -0,0 +1,33 @@
+import { html } from 'lit';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { createFixture, removeFixture, elementIsStable } from '@elements/elements/test';
+import { PanelFooter } from '@elements/elements/panel';
+import '@elements/elements/panel/define.js';
+
+describe('nve-panel-footer', () => {
+  let fixture: HTMLElement;
+  let element: PanelFooter;
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-panel>
+        <nve-panel-footer>hello</nve-panel-footer>
+      </nve-panel>
+    `);
+    element = fixture.querySelector('nve-panel-footer');
+    await elementIsStable(element);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should define element', () => {
+    expect(customElements.get('nve-panel-footer')).toBeDefined();
+  });
+
+  it('should render with the footer default slot', async () => {
+    await elementIsStable(element);
+    expect(element.slot).toBe('footer');
+  });
+});
