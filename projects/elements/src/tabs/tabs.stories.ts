8328c50410a9ce46d6b3a564281b6f0ddbb332e5
William Geller <wgeller@nvidia.com>
2023-04-04 15:24:33 -0400

    chore(elements): tabs documentation
    - minor visual adjustments & additional written docs
    - adds mlv-tabs components to package

--- projects/elements/src/tabs/tabs.stories.ts
+++ projects/elements/src/tabs/tabs.stories.ts
@@ -26,10 +26,7 @@ export const LightTheme = {
   render: () => html`
   <nve-card nve-theme="light" style="--border-radius: none">
     <nve-tabs behavior-select>
-      <nve-tabs-item selected>
-        Tab 1
-        <nve-dot status="scheduled"></nve-dot>
-      </nve-tabs-item>
+      <nve-tabs-item selected>Tab 1</nve-tabs-item>
       <nve-tabs-item>Tab 2</nve-tabs-item>
       <nve-tabs-item>Tab 3</nve-tabs-item>
       <nve-tabs-item disabled>Disabled</nve-tabs-item>
@@ -43,24 +40,7 @@ export const DarkTheme = {
   render: () => html`
   <nve-card nve-theme="dark" style="--border-radius: none">
     <nve-tabs behavior-select>
-      <nve-tabs-item selected>
-        Tab 1
-        <nve-dot status="starting"></nve-dot>
-      </nve-tabs-item>
-      <nve-tabs-item>Tab 2</nve-tabs-item>
-      <nve-tabs-item>Tab 3</nve-tabs-item>
-      <nve-tabs-item disabled>Disabled</nve-tabs-item>
-      <nve-tabs-item>Tab 5</nve-tabs-item>
-    </nve-tabs>
-  </nve-card>
-  `
-};
-
-export const VerticalTabs = {
-  render: () => html`
-  <nve-card style="--border-radius: none; width: 250px;">
-    <nve-tabs vertical behavior-select>
-      <nve-tabs-item selected>Tab 1</nve-tabs-item>
+      <nve-tabs-item selected> Tab 1</nve-tabs-item>
       <nve-tabs-item>Tab 2</nve-tabs-item>
       <nve-tabs-item>Tab 3</nve-tabs-item>
       <nve-tabs-item disabled>Disabled</nve-tabs-item>
@@ -82,25 +62,64 @@ export const BorderlessTabs = {
   `
 };
 
+export const TabsWithDots = {
+  render: () => html`
+  <nve-tabs behavior-select>
+    <nve-tabs-item>Tab 1</nve-tabs-item>
+    <nve-tabs-item>Tab 2</nve-tabs-item>
+    <nve-tabs-item>Tab 3 </nve-tabs-item>
+    <nve-tabs-item selected>
+      Tab 4
+      <nve-dot status="warning" aria-label="10 notifications">10</nve-dot>
+    </nve-tabs-item>
+    <nve-tabs-item>Tab 5</nve-tabs-item>
+  </nve-tabs>
+  `
+};
 
+export const VerticalTabs = {
+  render: () => html`
+  <nve-card style="--border-radius: none; width: 250px;">
+    <nve-tabs vertical behavior-select>
+      <nve-tabs-item selected>Tab 1</nve-tabs-item>
+      <nve-tabs-item>Tab 2</nve-tabs-item>
+      <nve-tabs-item>Tab 3</nve-tabs-item>
+      <nve-tabs-item disabled>Disabled</nve-tabs-item>
+      <nve-tabs-item>Tab 5</nve-tabs-item>
+    </nve-tabs>
+  </nve-card>
+  `
+};
 
 export const BorderlessVerticalTabs = {
   render: () => html`
   <nve-card style="--border-radius: none; width: 250px;">
     <nve-tabs vertical borderless behavior-select>
       <nve-tabs-item>
-        <nve-icon status="accent" name="settings"></nve-icon> Tab 1
+        <nve-icon name="settings"></nve-icon> Tab 1
       </nve-tabs-item>
       <nve-tabs-item>
-        <nve-icon status="success" name="user"></nve-icon> Tab 2
+        <nve-icon name="user"></nve-icon> Tab 2
       </nve-tabs-item>
       <nve-tabs-item selected>
-        <nve-icon status="warning" name="beaker"></nve-icon> Tab 3
+        <nve-icon name="beaker"></nve-icon> Tab 3
       </nve-tabs-item>
       <nve-tabs-item>
-        <nve-icon status="danger" name="dashboard"></nve-icon> Tab 4
+        <nve-icon name="dashboard"></nve-icon> Tab 4
       </nve-tabs-item>
     </nve-tabs>
   </nve-card>
+  `
+};
+
+export const StatelessTabs = {
+  render: () => html`
+  <nve-tabs>
+    <nve-tabs-item selected>Tab 1</nve-tabs-item>
+    <nve-tabs-item>Tab 2</nve-tabs-item>
+    <nve-tabs-item>Tab 3</nve-tabs-item>
+    <nve-tabs-item>Tab 4</nve-tabs-item>
+    <nve-tabs-item disabled>Disabled</nve-tabs-item>
+  </nve-tabs>
   `
 };
