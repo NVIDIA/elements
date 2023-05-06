2836ec29b807eb3618947824af9d19a38f80e062
William Geller <wgeller@nvidia.com>
2023-05-05 20:06:48 -0400

    feat(core): divider vertical
    - adds vertical mode
    - simplifies CSS

--- projects/elements/src/divider/divider.stories.ts
+++ projects/elements/src/divider/divider.stories.ts
@@ -9,28 +9,50 @@ export default {
 
 export const Default = {
   render: () => html`
-<nve-divider></nve-divider>
+    <nve-divider></nve-divider>
   `
 };
 
 export const Emphasis = {
   render: () => html`
-<nve-divider emphasis></nve-divider>
+    <nve-divider style="--color: var(--nve-ref-border-color-emphasis)"></nve-divider>
+  `
+};
+
+export const Muted = {
+  render: () => html`
+    <nve-divider style="--color: var(--nve-ref-border-color-muted)"></nve-divider>
+  `
+};
+
+export const Vertical = {
+  render: () => html`
+    <div nve-layout="row gap:sm align:vertical-center" style="height: 50px">
+      <nve-divider orientation="vertical"></nve-divider>
+      <nve-icon-button icon-name="information"></nve-icon-button>
+      <nve-icon-button icon-name="additional-actions"></nve-icon-button>
+    </div>
+  `
+};
+
+export const Rounded = {
+  render: () => html`
+    <nve-divider style="--size: var(--nve-ref-border-width-xl); --border-radius: var(--nve-ref-border-radius-xs); --color: var(--nve-sys-accent-secondary-background);"></nve-divider>
   `
 };
 
 export const LightTheme = {
   render: () => html`
-<div nve-theme="root light" nve-layout="row gap:md pad:md">
-  <nve-divider></nve-divider>
-</div>
+    <nve-card nve-theme="light">
+      <nve-divider></nve-divider>
+    </nve-card>
   `
 }
 
 export const DarkTheme = {
   render: () => html`
-<div nve-theme="root dark" nve-layout="row gap:md pad:md">
-  <nve-divider></nve-divider>
-</div>
+    <nve-card nve-theme="dark">
+      <nve-divider></nve-divider>
+    </nve-card>
   `
 }
