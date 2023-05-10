ec0da45ccfe3022bf6c3bfe4dbba22f6283df015
Scott Mathis <smathis@nvidia.com>
2023-05-10 10:19:21 -0400

    feat(core): breadcrumb
    Signed-off-by: Scott Mathis <smathis@nvidia.com>

---
+++ projects/elements/src/breadcrumb/define.ts
@@ -0,0 +1,11 @@
+import { define } from '@elements/elements/internal';
+import { Breadcrumb } from '@elements/elements/breadcrumb';
+import '@elements/elements/icon/define.js';
+
+define(Breadcrumb);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'nve-breadcrumb': Breadcrumb;
+  }
+}
