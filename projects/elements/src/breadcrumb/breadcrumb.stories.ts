171e6bdf38511fe4716c6eb6a5dd24b8b9975d6e
Scott Mathis <smathis@nvidia.com>
2023-05-10 10:19:21 -0400

    feat(core): breadcrumb
    Signed-off-by: Scott Mathis <smathis@nvidia.com>

---
+++ projects/elements/src/breadcrumb/breadcrumb.stories.ts
@@ -0,0 +1,102 @@
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators/custom-element.js';
+import { state } from 'lit/decorators/state.js';
+import '@elements/elements/breadcrumb/define.js';
+import '@elements/elements/icon/define.js';
+import '@elements/elements/button/define.js';
+import '@elements/elements/icon-button/define.js';
+import '@elements/elements/menu/define.js';
+
+export default {
+  title: 'Elements/Breadcrumb/Examples',
+  component: 'mlv-breadcrumb',
+  parameters: { badges: ['alpha'] }
+};
+
+/* eslint-disable */
+export const Default = {
+  render: () => html`
+    <div style="display: flex; align-items: center; justify-content: center; height: 37px">
+      <mlv-breadcrumb>
+        <mlv-button><a href="javascript: void(0)">Item 1</a></mlv-button>
+        <mlv-button><a href="javascript: void(0)">Item 2</a></mlv-button>
+        <mlv-button><a href="javascript: void(0)">Item 3</a></mlv-button>
+        <span>You Are Here</span>
+      </mlv-breadcrumb>
+    </div>
+  `
+};
+
+export const WithIconButton = {
+  render: () => html`
+    <div style="display: flex; align-items: center; justify-content: center; height: 37px">
+      <mlv-breadcrumb>
+        <mlv-icon-button icon-name="home"><a href="javascript: void(0)" aria-label="link to first page"></a></mlv-icon-button>
+        <mlv-button><a href="javascript: void(0)">Item 1</a></mlv-button>
+        <mlv-button><a href="javascript: void(0)">Item 2</a></mlv-button>
+        <span>You Are Here</span>
+      </mlv-breadcrumb>
+    </div>
+  `
+};
+
+@customElement('breadcrumb-menu-demo')
+class BreadcrumbMenuDemo extends LitElement {
+  @state() menuHidden = true;
+
+  render() {
+    const ddownId = "more-links";
+    return html`
+<mlv-breadcrumb>
+  <mlv-icon-button id=${ddownId + '-btn'} @click=${() => this.menuHidden = false} icon-name="additional-actions"></mlv-icon-button>
+  <mlv-dropdown id=${ddownId}
+    anchor=${ddownId + '-btn'}
+    .hidden=${this.menuHidden}
+    @close=${() => this.menuHidden = true}
+    arrow>
+    <mlv-menu>
+      <mlv-menu-item>Item 1</mlv-menu-item>
+      <mlv-menu-item>Item 2</mlv-menu-item>
+      <mlv-menu-item>Item 3</mlv-menu-item>
+    </mlv-menu>
+  </mlv-dropdown>
+  <mlv-button><a href="javascript: void(0)">Item 4</a></mlv-button>
+  <mlv-button><a href="javascript: void(0)">Item 5</a></mlv-button>
+  <span>You Are Here</span>
+</mlv-breadcrumb>
+`
+  }
+}
+
+export const WithMenu = {
+  render: () => html`
+    <breadcrumb-menu-demo></breadcrumb-menu-demo>
+  `
+};
+
+export const LightTheme = {
+  render: () => html`
+    <mlv-card mlv-theme="light" style="--border-radius: 4px">
+      <mlv-breadcrumb>
+        <mlv-icon-button icon-name="home"><a href="javascript: void(0)" aria-label="link to first page"></a></mlv-icon-button>
+        <mlv-button><a href="javascript: void(0)">Item 1</a></mlv-button>
+        <mlv-button><a href="javascript: void(0)">Item 2</a></mlv-button>
+        <span>You Are Here</span>
+      </mlv-breadcrumb>
+    </mlv-card>
+  `
+}
+
+export const DarkTheme = {
+  render: () => html`
+    <mlv-card mlv-theme="dark" style="--border-radius: 4px">
+      <mlv-breadcrumb>
+        <mlv-icon-button icon-name="home"><a href="javascript: void(0)" aria-label="link to first page"></a></mlv-icon-button>
+        <mlv-button><a href="javascript: void(0)">Item 1</a></mlv-button>
+        <mlv-button><a href="javascript: void(0)">Item 2</a></mlv-button>
+        <span>You Are Here</span>
+      </mlv-breadcrumb>
+    </mlv-card>
+  `
+}
+/* eslint-enable */
---
+++ projects/elements/src/breadcrumb/breadcrumb.ts
@@ -0,0 +1,100 @@
+import { html, LitElement } from 'lit';
+import { state } from 'lit/decorators/state.js';
+import { useStyles, keyNavigationList, KeynavListConfig, attachInternals, generateId } from '@elements/elements/internal';
+import styles from './breadcrumb.css?inline';
+import { Icon } from '@elements/elements/icon';
+import { IconButton } from '@elements/elements/icon-button';
+import { Button } from '@elements/elements/button';
+
+type BreadcrumbFocusable = IconButton | Button;
+
+let idCounter = 0;
+
+export function getBreadcrumbItemId() {
+  return `${idCounter++}_${generateId()}`;
+}
+
+export function addSlotsAndAttributesToItems(items: Element[]): Element[] {
+  const clickableItemTags = ['MLV-BUTTON', 'MLV-ICON-BUTTON'];
+
+  return items.filter(el => {
+    const tagName = el.tagName;
+    return [...clickableItemTags, 'SPAN'].indexOf(tagName) > -1;
+  }).map((el, idx, ary) => {
+    const tagName = el.tagName;
+    const isLastItem = (idx === ary.length - 1);
+
+    el.setAttribute('slot', getBreadcrumbItemId());
+
+    if (clickableItemTags.indexOf(tagName) > -1) {
+      el.setAttribute('interaction', 'ghost');
+    }
+
+    if (isLastItem) {
+      el.setAttribute('last-item', '');
+    }
+    return el;
+  });
+}
+
+/**
+ * @element mlv-breadcrumb
+ * @storybook https://elements.nvidia.com/ui/storybook/elements?path=/story/elements-breadcrumb-documentation--page
+ * @figma https://www.figma.com/file/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog---WIP?type=design&node-id=173%3A29384&t=iXgqa5cQO84PPz1R-1
+ * @aria https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/navigation_role
+ * @stable false
+ */
+@keyNavigationList<Breadcrumb>()
+export class Breadcrumb extends LitElement {
+  get keynavListConfig(): KeynavListConfig {
+    return {
+      items: Array.from(this.querySelectorAll<BreadcrumbFocusable>('mlv-button, mlv-icon-button'))
+    }
+  }
+
+  declare _internals: ElementInternals;
+
+  static styles = useStyles([styles]);
+
+  static readonly metadata = {
+    tag: 'mlv-breadcrumb',
+    version: 'PACKAGE_VERSION'
+  };
+
+  static elementDefinitions = {
+    'mlv-icon': Icon
+  };
+
+  @state() private breadcrumbItems: Element[] = [];
+
+  render() {
+    const slotNames = this.slotNames;
+    const penultimateSlotLength = slotNames.length - 1;
+    return html`<div internal-host><ol>
+      ${slotNames.map(
+        (slotName, idx) => html`
+          <li>
+            <slot name="${slotName}"></slot>
+            ${(idx < penultimateSlotLength) ? this.separator : ''}
+          </li>
+        `)}</ol></div><slot hidden-slot @slotchange=${this.#slotUpdate}></slot>`;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    attachInternals(this);
+    this._internals.role = 'navigation';
+  }
+
+  private get separator() {
+    return html`<mlv-icon separator aria-hidden="true" name="chevron-right" size="sm"></mlv-icon>`;
+  }
+
+  #slotUpdate() {
+    this.breadcrumbItems = addSlotsAndAttributesToItems(Array.from(this.children));
+  }
+
+  private get slotNames(): string[] {
+    return this.breadcrumbItems.map(el => el.slot);
+  }
+}
