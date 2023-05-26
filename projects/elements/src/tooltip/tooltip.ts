1260722c78fe5c769a075c5974e13e0d84d8041c
William Geller <wgeller@nvidia.com>
2023-05-26 13:24:35 -0400

    chore(release): v0.11.0

--- projects/elements/src/tooltip/tooltip.ts
+++ projects/elements/src/tooltip/tooltip.ts
@@ -22,6 +22,8 @@ import styles from './tooltip.css?inline';
  * @storybook https://elements.nvidia.com/ui/storybook/elements?path=/story/elements-tooltip-documentation--page
  * @figma https://www.figma.com/file/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog---WIP?node-id=29-29&t=clRGqnKDRGNhR0Yu-0
  * @aria https://www.w3.org/WAI/ARIA/apg/patterns/tooltip/
+ * @performance false
+ * @stable false
  */
 export class Tooltip extends LitElement {
   /**
