b98de4f4fe669820327fe3d6a591a4081572b4fa
William Geller <wgeller@nvidia.com>
2023-05-26 13:24:35 -0400

    chore(release): v0.11.0

--- projects/elements/src/week/week.ts
+++ projects/elements/src/week/week.ts
@@ -8,6 +8,7 @@ import styles from './week.css?inline';
  * @element nve-week
  * @storybook https://elements.nvidia.com/ui/storybook/elements?path=/story/elements-week-documentation--page
  * @aria https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/week
+ * @vqa false
  */
 export class Week extends Control {
   static styles = useStyles([...Control.styles, inputStyles, styles]);
