b9e1b37ad979aa8f7b08a49b46a10408960e2ffe
Cory Rylan <crylan@nvidia.com>
2023-06-02 16:32:20 -0500

    chore: improved lint checking
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/elements/build/eslint/stateless-property.cjs
@@ -0,0 +1,46 @@
+const props = new Map();
+const mutations = new Map();
+
+/** https://github.com/stencil-community/stencil-eslint/blob/main/src/rules/strict-mutable.ts */
+const rule = {
+  meta: {
+    docs: {
+      description: 'Prevent stateful mutations on public API properties',
+      category: 'Possible Errors',
+    },
+    schema: [],
+    type: 'problem',
+  },
+  create(context) {
+    function getProps(node) {
+      const varName = node.parent.key.name;
+      props.set(varName, node);
+    }
+
+    function checkAssigment(node) {
+      const propName = node.left.property.name;
+      mutations.set(propName, node);
+    }
+
+    return {
+      'PropertyDefinition > Decorator[expression.callee.name=property]': getProps,
+      'AssignmentExpression[left.object.type=ThisExpression][left.property.type=Identifier]': checkAssigment,
+      'ClassDeclaration:exit': () => {
+        mutations.forEach((node, name) => {
+          const noExceptions = !['hidden', 'value'].find(i => i === name); // native overrides like input value are by default stateful in browser
+          if (noExceptions && props.has(name)) {
+            context.report({
+              node: node.parent,
+              message: `Public API "${name}" should not be stateful or stateful behavior must be opt-in. https://elements.nvidia.com/ui/storybook/elements?path=/story/about-api-design-stateless--page`,
+            });
+          }
+  
+        });
+        props.clear();
+        mutations.clear();
+      }
+    };
+  }
+};
+
+module.exports = rule;
