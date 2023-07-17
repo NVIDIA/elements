6ccfda8102a5e6f704a9bf12938d105c6e312d71
Cory Rylan <crylan@nvidia.com>
2023-07-17 15:40:18 -0500

    feat(core): icon alias support
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/icon/icon.test.ts
+++ projects/elements/src/icon/icon.test.ts
@@ -35,4 +35,24 @@ describe('nve-icon', () => {
     await elementIsStable(element);
     expect(element.getAttribute('name')).toBe('book');
   });
+
+  it('should allow icon aliasing', () => {
+    expect((customElements.get('nve-icon') as any)._icons['chevron-up']).toBe((customElements.get('nve-icon') as any)._icons['chevron']);
+  });
+
+  it('should allow icons to be registered', async () => {
+    (customElements.get('nve-icon') as any).add({
+      'test-svg': { svg: () => '<svg id="test-svg"><path d=""/></svg>' }
+    });
+
+    expect((customElements.get('nve-icon') as any)._icons['test-svg']).toBeDefined();
+  });
+
+  it('should allow dynamic paths', async () => {
+    element.name = './assets/icons.svg' as any;
+    await elementIsStable(element);
+    await new Promise(r => setTimeout(r, 100));
+    expect((customElements.get('nve-icon') as any)._icons['./assets/icons.svg']).toBeTruthy();
+  });
+
 });
