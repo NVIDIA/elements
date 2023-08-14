081cdd9684f7cf15a55d298faf7b7b02a864ff7b
Cory Rylan <crylan@nvidia.com>
2023-08-14 12:18:32 -0500

    chore(core): update nextjs doc
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/demos/nextjs/src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head as head, Main, NextScript as nextScript } from 'next/document';
+
+// casting to any due to monorepo hoisting issue with pnpm
+const Head = head as any;
+const NextScript = nextScript as any;
+
+export default function Document() {
+  return (
+    <Html lang="en" mlv-theme="dark">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
