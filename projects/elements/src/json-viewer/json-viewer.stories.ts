071cf994bf17826a288245b96442e695c8c3ea41
Cory Rylan <crylan@nvidia.com>
2023-08-15 20:26:15 -0500

    chore(core): debug json viewer
    - add internal json viewer for debugging
    - add description metadata support for metadata.json
    - fix drawer padding alignments for more flexibility
    - improve metrics report view
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/elements/src/json-viewer/json-viewer.stories.ts
@@ -0,0 +1,37 @@
+import { html, LitElement } from 'lit';
+import { define } from '@elements/elements/internal';
+import '@elements/elements/json-viewer/define.js';
+import metrics from 'metrics/data.json';
+
+export default {
+  title: 'Internal/JSON Viewer/Examples',
+  component: 'nve-json-viewer',
+};
+
+const json = {"tasks": [{ "id": 1, "status": "success", "date": "08/03/2023 14:22:44", "url": "https://elements.nvidia.com/av/ingestion/uploads", "message": "task completed" },{ "id": 2, "status": "failed", "date": "08/09/2023 14:22:44", "url": "https://elements.nvidia.com/av/ingestion/uploads", "message": "Error: cy.request() failed on:\n\nThe response we received from your web server was:\n\n  > 401: Unauthorized\n" }]};
+
+export const Default = {
+  render: () => html`
+  <nve-json-viewer expanded>${JSON.stringify(json)}</nve-json-viewer>
+  `
+};
+
+class DyanamicJSONDemo extends LitElement {
+  static metadata = {
+    tag: 'dynamic-json-demo',
+    version: 'demo'
+  }
+
+  render() {
+    return html`<nve-json-viewer .value=${metrics}></nve-json-viewer>`
+  }
+}
+
+define(DyanamicJSONDemo)
+
+
+export const Dynamic = {
+  render: () => html`
+  <dynamic-json-demo></dynamic-json-demo>
+  `
+};
