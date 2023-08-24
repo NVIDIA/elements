7a2433fa8c555baa1553aea884615d5773ac03f4
Cory Rylan <crylan@nvidia.com>
2023-08-24 13:03:39 -0600

    fix(core): panel and drawer fixes
    - fix panel collapse button direction with side right placement
    - add drawer footer component for better layout and alignment
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/drawer/define.ts
+++ projects/elements/src/drawer/define.ts
@@ -1,13 +1,15 @@
 import { define } from '@elements/elements/internal';
-import { Drawer, DrawerHeader } from '@elements/elements/drawer';
+import { Drawer, DrawerFooter, DrawerHeader } from '@elements/elements/drawer';
 import '@elements/elements/icon-button/define.js';
 
 define(Drawer);
+define(DrawerFooter);
 define(DrawerHeader);
 
 declare global {
   interface HTMLElementTagNameMap {
     'nve-drawer': Drawer;
+    'nve-drawer-footer': DrawerFooter;
     'nve-drawer-header': DrawerHeader;
   }
 }
