06b6e6d99d3332ae9dacf5d0f707a6ca79e4cc6f
Cory Rylan <crylan@nvidia.com>
2023-08-24 13:03:39 -0600

    fix(elements): panel and drawer fixes
    - fix panel collapse button direction with side right placement
    - add drawer footer component for better layout and alignment
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/elements/src/drawer/drawer-footer.test.ts
@@ -0,0 +1,33 @@
+import { html } from 'lit';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { createFixture, removeFixture, elementIsStable } from '@elements/elements/test';
+import { DrawerFooter } from '@elements/elements/drawer';
+import '@elements/elements/drawer/define.js';
+
+describe('nve-drawer-footer', () => {
+  let fixture: HTMLElement;
+  let element: DrawerFooter;
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-drawer>
+        <nve-drawer-footer>hello</nve-drawer-footer>
+      </nve-drawer>
+    `);
+    element = fixture.querySelector('nve-drawer-footer');
+    await elementIsStable(element);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should define element', () => {
+    expect(customElements.get('nve-drawer-footer')).toBeDefined();
+  });
+
+  it('should render with the footer default slot', async () => {
+    await elementIsStable(element);
+    expect(element.slot).toBe('footer');
+  });
+});
---
+++ projects/elements/src/drawer/drawer-footer.ts
@@ -0,0 +1,32 @@
+import { html, LitElement } from 'lit';
+import { useStyles } from '@elements/elements/internal';
+import styles from './drawer-footer.css?inline';
+
+/**
+ * @element nve-drawer-footer
+ * @slot - default slot for the drawer footer
+ * @storybook https://elements.nvidia.com/ui/storybook/elements?path=/docs/elements-drawer-documentation--docs
+ * @figma https://www.figma.com/file/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog---WIP?node-id=30-39&t=CAAM7yEBvG18tRRa-0
+ * @aria https://www.w3.org/WAI/ARIA/apg/patterns/dialog-modal/
+ */
+export class DrawerFooter extends LitElement {
+  static styles = useStyles([styles]);
+
+  static readonly metadata = {
+    tag: 'nve-drawer-footer',
+    version: 'PACKAGE_VERSION'
+  };
+
+  render() {
+    return html`
+      <div internal-host>
+        <slot></slot>
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.slot = 'footer';
+  }
+}
