206e3158de15a8f25bfbab39b318f2756c4b17a7
Will Geller <wgeller@nvidia.com>
2023-09-19 09:28:33 -0400

    chore(elements): progress tests and docs - Adds unit tests to progress-bar and progress-ring components - Adds docs metadata and revamped MDX to both components

---
+++ projects/elements/src/progress-bar/progress-bar.test.ts
@@ -0,0 +1,48 @@
+import { html } from 'lit';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { createFixture, removeFixture, elementIsStable } from '@elements/elements/test';
+import { ProgressBar } from '@elements/elements/progress-bar';
+import '@elements/elements/progress-bar/define.js';
+
+
+describe('nve-progress-bar', () => {
+  let fixture: HTMLElement;
+  let element: ProgressBar;
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-progress-bar></nve-progress-bar>
+    `);
+    element = fixture.querySelector('nve-progress-bar');
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should define element', () => {
+    expect(customElements.get('nve-progress-bar')).toBeDefined();
+  });
+
+  it('should set properties on its native progress element', async () => {
+    element.value = 50;
+    element.max = 75;
+    await elementIsStable(element);
+
+    const nativeProgress = element.shadowRoot.querySelector('progress') as HTMLProgressElement;
+
+    expect(nativeProgress.value).toBe(element.value);
+    expect(nativeProgress.max).toBe(element.max);
+  });
+
+  it('should properly set full and minWidth styles', async () => {
+    element.value = 50;
+    element.max = 50;
+    await elementIsStable(element);
+
+    const nativeProgress = element.shadowRoot.querySelector('progress') as HTMLProgressElement;
+
+    expect(nativeProgress.classList.contains('full')).toBe(true);
+    expect(nativeProgress.classList.contains('minWidth')).toBe(true);
+  });
+});
--- projects/elements/src/progress-bar/progress-bar.ts
+++ projects/elements/src/progress-bar/progress-bar.ts
@@ -6,13 +6,12 @@ import styles from './progress-bar.css?inline';
 
 /**
  * @element nve-progress-bar
- * @description A progress bar is a control that indicates the progress of a task.
+ * @description A progress bar is a visual indicator of the status of a running task. Under the hood the native HTML `progress` element is used to achieve proper a11y concerns.
  * @since 0.16.0
  * @storybook https://elements.nvidia.com/ui/storybook/elements?path=/docs/elements-progress-bar-documentation--docs
  * @aria https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/progressbar_role
  * @figma https://www.figma.com/file/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog---WIP?node-id=29%3A20&mode=dev
- * @vqa true
- * @unitTests false
+ * @stable false
  */
 export class ProgressBar extends LitElement {
   static styles = useStyles([styles]);
@@ -21,21 +20,18 @@ export class ProgressBar extends LitElement {
     tag: 'nve-progress-bar',
     version: 'PACKAGE_VERSION'
   };
+ 
+  /** The current `value` of the progress indicator, set to the native HTML progress element. */  
+  @property({ type: Number }) value?: number = 0;
 
-  /** current value of the progress indicator */  
-  @property({ type: Number }) value?: number;
-
-  /** minimum value of the progress indicator */  
-  @property({ type: Number }) min? = 0;
-
-  /** max value of the progress indicator */  
+  /** The `max` property sets the highest value that `value` will be scaled to. */  
   @property({ type: Number }) max? = 100;
 
   render() {
     const classes = { full: this.value === this.max, minWidth: this.value > 0 };
     
     return html`
-      <progress .min=${this.min} .max=${this.max} .value=${this.value} class=${classMap(classes)}></progress>
+      <progress .max=${this.max} .value=${this.value} class=${classMap(classes)}></progress>
     `;
   }
 }
