ecbed9804445b7dc6c3df2f721cf0aa12c3ae31c
William Geller <wgeller@nvidia.com>
2023-10-06 22:28:09 -0400

    feat(core): page loader

---
+++ projects/elements/src/page-loader/page-loader.test.ts
@@ -0,0 +1,43 @@
+import { html } from 'lit';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { createFixture, elementIsStable, removeFixture } from '@elements/elements/test';
+import { PageLoader } from '@elements/elements/page-loader';
+import '@elements/elements/page-loader/define.js';
+
+describe('nve-page-loader', () => {
+  let fixture: HTMLElement;
+  let element: PageLoader;
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-page-loader></nve-page-loader>
+    `);
+    element = fixture.querySelector('nve-page-loader');
+    await elementIsStable(element);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should define element', () => {
+    expect(customElements.get('nve-page-loader')).toBeDefined();
+  });
+
+  it('should use auto behavior when modal', async () => {
+    element.modal = true;
+    await elementIsStable(element);
+    expect(element.popoverType).toBe('auto');
+  });
+
+  it('should use default to center position', async () => {
+    await elementIsStable(element);
+    expect(element.position).toBe('center');
+  });
+
+  it('should not be closable', async () => {
+    element.modal = true;
+    await elementIsStable(element);
+    expect(element.closable).toBe(false);
+  });
+});
