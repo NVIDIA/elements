5e4bf3881db042060abd2837c1a32ff6e38c732a
Cory Rylan <crylan@nvidia.com>
2023-10-12 14:46:03 -0500

    feat(elements): drawer content element - add mlv-drawer-content element - add notification + drawer example
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/drawer/drawer.stories.ts
+++ projects/elements/src/drawer/drawer.stories.ts
@@ -1,6 +1,4 @@
 import { html } from 'lit';
-import { spread } from '@elements/elements/internal';
-import { Drawer } from '@elements/elements/drawer';
 import '@elements/elements/card/define.js';
 import '@elements/elements/button/define.js';
 import '@elements/elements/drawer/define.js';
@@ -8,52 +6,53 @@ import '@elements/elements/drawer/define.js';
 export default {
   title: 'Elements/Drawer/Examples',
   component: 'nve-drawer',
-  inline: false,
-  argTypes: {
-    position: {
-      control: 'inline-radio',
-      options: ['top', 'right', 'bottom', 'left'],
-      defaultValue: 'center'
-    },
-    alignment: {
-      control: 'inline-radio',
-      options: ['start', 'center', 'end'],
-      defaultValue: 'center'
-    }
-  }
+  inline: false
 };
 
-type ArgTypes = Drawer;
-
 export const Default = {
-  inline: false,
-  render: (args: ArgTypes) => html`
-    <nve-drawer ${spread(args)} closable>
-      <nve-drawer-header>
-        <h3 nve-text="heading semibold sm">Title</h3>
-      </nve-drawer-header>
-      <p nve-text="body">some text content in a closable drawer</p>
-      <nve-drawer-footer>
-        <div nve-layout="grid gap:sm span-items:6">
-          <nve-button>cancel</nve-button>
-          <nve-button>details</nve-button>
-        </div>
-      </nve-drawer-footer>
-    </nve-drawer>
+  render: () => html`
+<nve-drawer closable>
+  <nve-drawer-header>
+    <h3 nve-text="heading semibold sm">Drawer Header</h3>
+  </nve-drawer-header>
+  <nve-drawer-content>
+    <p nve-text="body">drawer content</p>
+  </nve-drawer-content>
+  <nve-drawer-footer>
+    <p nve-text="body">drawer footer</p>
+  </nve-drawer-footer>
+</nve-drawer>
+  `,
+};
+
+export const Scroll = {
+  render: () => html`
+<nve-drawer closable>
+  <nve-drawer-header>
+    <h3 nve-text="heading semibold sm">Drawer Header</h3>
+  </nve-drawer-header>
+  <nve-drawer-content>
+    <p nve-text="body" style="height: 2500px">drawer content</p>
+  </nve-drawer-content>
+  <nve-drawer-footer>
+    <p nve-text="body">drawer footer</p>
+  </nve-drawer-footer>
+</nve-drawer>
   `,
 };
 
 export const Interactive = {
-  inline: false,
   render: () => html`
 <nve-button id="drawer-btn">open</nve-button>
 <nve-drawer closable modal hidden position="right">
   <nve-drawer-header>
-    <h3 nve-text="heading">Heading</h3>
+    <h3 nve-text="heading">Drawer Header</h3>
   </nve-drawer-header>
-  <p nve-text="body">some text content in a closable drawer</p>
+  <nve-drawer-content>
+    <p nve-text="body">drawer content</p>
+  </nve-drawer-content>
   <nve-drawer-footer>
-    <p nve-text="body">some text footer content</p>
+    <p nve-text="body">drawer footer</p>
   </nve-drawer-footer>
 </nve-drawer>
 <script type="module">
@@ -69,11 +68,13 @@ export const Small = {
   render: () => html`
 <nve-drawer size="sm" closable>
   <nve-drawer-header>
-    <h3 nve-text="heading semibold sm">Small</h3>
+    <h3 nve-text="heading semibold sm">Drawer Header</h3>
   </nve-drawer-header>
-  <p nve-text="body">some text content in a small drawer</p>
+  <nve-drawer-content>
+    <p nve-text="body">drawer content</p>
+  </nve-drawer-content>
   <nve-drawer-footer>
-    <p nve-text="body">some text footer content</p>
+    <p nve-text="body">drawer footer</p>
   </nve-drawer-footer>
 </nve-drawer>
   `
@@ -83,11 +84,13 @@ export const Large = {
   render: () => html`
 <nve-drawer size="lg" closable>
   <nve-drawer-header>
-    <h3 nve-text="heading semibold sm">Large</h3>
+    <h3 nve-text="heading semibold sm">Drawer Header</h3>
   </nve-drawer-header>
-  <p nve-text="body">some text content in a large drawer</p>
+  <nve-drawer-content>
+    <p nve-text="body">drawer content</p>
+  </nve-drawer-content>
   <nve-drawer-footer>
-    <p nve-text="body">some text footer content</p>
+    <p nve-text="body">drawer footer</p>
   </nve-drawer-footer>
 </nve-drawer>
   `
@@ -97,10 +100,14 @@ export const NonClosable = {
   render: () => html`
 <nve-button id="open-btn">open</nve-button>
 <nve-drawer hidden modal>
-  <h3 nve-text="heading">Non-Closable Drawer</h3>
-  <p nve-text="body">escape key and light dismiss will not work here</p>
+  <nve-drawer-header>
+    <h3 nve-text="heading">Drawer Header</h3>
+  </nve-drawer-header>
+  <nve-drawer-content>
+    <p nve-text="body">drawer content</p>
+  </nve-drawer-content>
   <nve-drawer-footer>
-    <p nve-text="body">some text footer content</p>
+    <p nve-text="body">drawer footer</p>
   </nve-drawer-footer>
 </nve-drawer>
 <script type="module">
@@ -123,19 +130,23 @@ export const Position = {
 
 <nve-drawer closable modal hidden position="left">
   <nve-drawer-header>
-    <h3 nve-text="heading">Left</h3>
+    <h3 nve-text="heading">Drawer Header Left</h3>
   </nve-drawer-header>
-  <p nve-text="body">some text content in a left closable drawer</p>
+  <nve-drawer-content>
+    <p nve-text="body">drawer content</p>
+  </nve-drawer-content>
   <nve-drawer-footer>
-    <p nve-text="body">some text footer content</p>
+    <p nve-text="body">drawer footer</p>
   </nve-drawer-footer>
 </nve-drawer>
 
 <nve-drawer closable modal hidden position="right">
   <nve-drawer-header>
-    <h3 nve-text="heading">Right</h3>
+    <h3 nve-text="heading">Drawer Header Right</h3>
   </nve-drawer-header>
-  <p nve-text="body">some text content in a right closable drawer</p>
+  <nve-drawer-content>
+    <p nve-text="body">some text content in a right closable drawer</p>
+  </nve-drawer-content>
   <nve-drawer-footer>
     <p nve-text="body">some text footer content</p>
   </nve-drawer-footer>
@@ -174,11 +185,13 @@ export const Inline = {
 
   <nve-drawer hidden closable modal inline position="right">
     <nve-drawer-header>
-      <h3 nve-text="heading semibold sm">Small</h3>
+      <h3 nve-text="heading semibold sm">Drawer Header</h3>
     </nve-drawer-header>
-    <p nve-text="body">some text content in a small drawer</p>
+    <nve-drawer-content>
+      <p nve-text="body">drawer content</p>
+    </nve-drawer-content>
     <nve-drawer-footer>
-      <p nve-text="body">some text footer content</p>
+      <p nve-text="body">drawer footer</p>
     </nve-drawer-footer>
   </nve-drawer>
 </div>
--- projects/elements/src/drawer/index.ts
+++ projects/elements/src/drawer/index.ts
@@ -1,3 +1,4 @@
 export * from './drawer.js';
+export * from './drawer-content.js';
 export * from './drawer-footer.js';
 export * from './drawer-header.js';
