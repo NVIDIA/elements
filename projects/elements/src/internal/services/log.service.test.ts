84030ccf68d4687af142cd607d7cd665124f0c18
Cory Rylan <crylan@nvidia.com>
2023-11-09 12:01:07 -0600

    fix(core): style fixes - Fix missing rounded property type for input/search - Fix ellipsis/clipping on badges - Fix icon render flash on large sets - Fix dot default display - Improved Logging - Detect possible invalid grid structures - Fix missing card container styles - Fix missing toolbar container styles
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/internal/services/log.service.test.ts
+++ projects/elements/src/internal/services/log.service.test.ts
@@ -12,38 +12,50 @@ describe('LogService', () => {
 
   it('should log out values if in development', async () => {
     const original = console.log;
+    const originalWarn = console.warn;
     const originalError = console.error;
     console.log = () => null;
+    console.warn = () => null;
     console.error = () => null;
 
     vi.spyOn(console, 'log');
+    vi.spyOn(console, 'warn');
     vi.spyOn(console, 'error');
 
     LogService.log('test');
+    LogService.warn('test');
     LogService.error('test');
     expect(console.log).toHaveBeenCalled();
+    expect(console.warn).toHaveBeenCalled();
     expect(console.error).toHaveBeenCalled();
 
     console.log = original;
+    console.warn = originalWarn;
     console.error = originalError;
   });
 
   it('should not log out values if in production', async () => {
     const original = console.log;
+    const originalWarn = console.warn;
     const originalError = console.error;
     console.log = () => null;
+    console.warn = () => null;
     console.error = () => null;
 
     vi.spyOn(console, 'log');
+    vi.spyOn(console, 'warn');
     vi.spyOn(console, 'error');
 
     window.MLV_ELEMENTS.state.env = 'production';
     LogService.log('test');
+    LogService.warn('test');
     LogService.error('test');
     expect(console.log).not.toHaveBeenCalled();
+    expect(console.warn).not.toHaveBeenCalled();
     expect(console.error).not.toHaveBeenCalled();
 
     console.log = original;
+    console.warn = originalWarn;
     console.error = originalError;
   });
 });
