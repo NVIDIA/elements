c7df83f991ce68446799a7ccf89848d98036cfc3
Cory Rylan <crylan@nvidia.com>
2023-12-04 12:56:51 -0600

    fix(core): tooltip menu items - fix issue with tooltip rendering in menu-item
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/menu/menu.stories.ts
+++ projects/elements/src/menu/menu.stories.ts
@@ -8,6 +8,7 @@ import '@elements/elements/search/define.js';
 import '@elements/elements/drawer/define.js';
 import '@elements/elements/panel/define.js';
 import '@elements/elements/card/define.js';
+import '@elements/elements/tooltip/define.js';
 
 export default {
   title: 'Elements/Menu/Examples',
@@ -174,3 +175,17 @@ export const VerticalNavigationPanel = {
   </div>
   `
 };
+
+export const MenuItemTooltip = {
+  render: () => html`
+  <mlv-menu>
+    <mlv-menu-item>item 1</mlv-menu-item>
+    <mlv-menu-item id="menu-item-2">
+      item 2
+      <mlv-icon id="warning-icon" size="md" name="exclamation-triangle" style="margin-left: auto"></mlv-icon>
+      <mlv-tooltip trigger="warning-icon" behavior-trigger hidden>This is a warning tooltip</mlv-tooltip>
+    </mlv-menu-item>
+    <mlv-menu-item>item 3</mlv-menu-item>
+  </mlv-menu>
+  `
+}
