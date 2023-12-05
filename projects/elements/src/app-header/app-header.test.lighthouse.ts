e51a361b517526be93527c62f915471f218ab20b
Cory Rylan <crylan@nvidia.com>
2023-12-05 17:43:18 -0600

    chore(core): improve perf test scripts - update missing icon alias from perf script - add fast run for lighthouse bundle checks - add better bazel integration - cleanup relative paths in vitest script
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/app-header/app-header.test.lighthouse.ts
+++ projects/elements/src/app-header/app-header.test.lighthouse.ts
@@ -18,6 +18,6 @@ describe('app-header lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(20.5);
+    expect(report.payload.javascript.kb).toBeLessThan(21);
   });
 });
--- projects/elements/src/icon/define.ts
+++ projects/elements/src/icon/define.ts
@@ -4,6 +4,41 @@ import { Icon, mergeIcons } from '@elements/elements/icon';
 define(Icon);
 mergeIcons(customElements.get('nve-icon') as typeof Icon);
 
+/** @deprecated aliases */
+Icon.alias({
+  'chevron-right': 'chevron',
+  'chevron-down': 'chevron',
+  'chevron-left': 'chevron',
+  'additional-actions': 'more-actions',
+  'analytics': 'pie-chart',
+  'annotation': 'transparent-box',
+  'app-switcher': 'switch-apps',
+  'assist': 'chat-bubble',
+  'checkmark': 'check',
+  'date': 'calendar',
+  'docs': 'book',
+  'expand-full-screen': 'maximize',
+  'expand-panel': 'arrow-stop',
+  'collapse-panel': 'arrow-stop',
+  'failed': 'x-circle',
+  'favorite-filled': 'star',
+  'favorite-outline': 'star-stroke',
+  'information': 'information-circle-stroke',
+  'maintenance': 'wrench',
+  'navigate-to': 'arrow',
+  'open-external-link': 'arrow-angle',
+  'location': 'map-pin',
+  'pinned-1': 'pin',
+  'project': 'folder',
+  'settings': 'gear',
+  'user': 'person',
+  'video-pause': 'pause',
+  'video-play': 'play',
+  'video-stop': 'stop',
+  'visible': 'eye',
+  'warning': 'exclamation-triangle'
+});
+
 declare global {
   interface HTMLElementTagNameMap {
     'nve-icon': Icon;
