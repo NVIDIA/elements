a0a3ac33f34dc4f479bbd429f2787902b48e7824
Cory Rylan <crylan@nvidia.com>
2023-12-11 15:56:08 -0600

    chore(release): v0.21.0
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/pagination/pagination.ts
+++ projects/elements/src/pagination/pagination.ts
@@ -12,7 +12,6 @@ import styles from './pagination.css?inline';
  * @storybook https://elements.nvidia.com/ui/storybook/elements?path=/docs/elements-pagination-documentation--docs
  * @figma https://www.figma.com/file/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog---WIP?node-id=3689-87177&t=znx8f5Hs8oD2ySWm-0
  * @aria https://www.w3.org/WAI/ARIA/apg/patterns/toolbar/
- * @stable false
  */
 @keyNavigationList<Pagination>()
 export class Pagination extends LitElement {
