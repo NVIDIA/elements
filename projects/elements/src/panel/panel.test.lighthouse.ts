99c46a26c39c566cce023b1d07c719e5559416c1
Cory Rylan <crylan@nvidia.com>
2023-12-11 11:42:22 -0600

    chore(core): metadata cleanup and tests - fix missing metadata entrypoints for lighthouse - add missing light housetest for form control - add missing lighthouse test for panel - fix outdated stability tags - fix broken figma links
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/elements/src/panel/panel.test.lighthouse.ts
@@ -0,0 +1,27 @@
+import { expect, test, describe } from 'vitest';
+import { runner } from 'elements-lighthouse';
+
+describe('panel lighthouse report', () => {
+  test('panel should meet lighthouse benchmarks', async () => {
+    const report = await runner.getReport('nve-panel', /* html */`
+      <nve-panel behavior-expand expanded style="width:280px; height:100vh">
+        <nve-panel-header>
+          <div slot="title">Title</div>
+          <div slot="subtitle"></div>
+        </nve-panel-header>
+
+        <nve-panel-content nve-layout="column gap:md">
+          <p nve-text="body">content</p>
+        </nve-panel-content>
+      </nve-panel>
+      <script type="module">
+        import '@elements/elements/panel/define.js';
+      </script>
+    `);
+
+    expect(report.scores.performance).toBe(100);
+    expect(report.scores.accessibility).toBe(100);
+    expect(report.scores.bestPractices).toBe(100);
+    expect(report.payload.javascript.kb).toBeLessThan(20.5);
+  });
+});
