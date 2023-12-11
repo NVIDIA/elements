09a8fd173b4a90ff6629308390049e0360f5ef1b
Cory Rylan <crylan@nvidia.com>
2023-12-11 11:42:22 -0600

    chore(core): metadata cleanup and tests - fix missing metadata entrypoints for lighthouse - add missing light housetest for form control - add missing lighthouse test for panel - fix outdated stability tags - fix broken figma links
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/elements/src/forms/forms.test.lighthouse.ts
@@ -0,0 +1,37 @@
+import { expect, test, describe } from 'vitest';
+import { runner } from 'elements-lighthouse';
+
+describe('forms lighthouse report', () => {
+  test('forms should meet lighthouse benchmarks', async () => {
+    const report = await runner.getReport('nve-forms', /* html */`
+      <nve-control>
+        <label>label</label>
+        <input />
+        <nve-control-message>message</nve-control-message>
+      </nve-control>
+      <nve-control-group layout="vertical-inline">
+        <label>vertical-inline</label>
+        <nve-control>
+          <label>local</label>
+          <input type="checkbox" name="checkbox-group" value="1" checked />
+        </nve-control>
+        <nve-control>
+          <label>staging</label>
+          <input type="checkbox" name="checkbox-group" value="2" />
+        </nve-control>
+        <nve-control>
+          <label>production</label>
+          <input type="checkbox" name="checkbox-group" value="3" />
+        </nve-control>
+      </nve-control-group>
+      <script type="module">
+        import '@elements/elements/forms/define.js';
+      </script>
+    `);
+
+    expect(report.scores.performance).toBe(100);
+    expect(report.scores.accessibility).toBe(100);
+    expect(report.scores.bestPractices).toBe(100);
+    expect(report.payload.javascript.kb).toBeLessThan(25.5);
+  });
+});
