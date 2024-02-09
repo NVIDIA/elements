b19cf7c8cd093b0c89ef6e662d4ae6d3a4ad5528
Cory Rylan <crylan@nvidia.com>
2024-02-09 15:48:12 -0600

    fix(elements): popover dynamic triggers
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/dropdown/dropdown.stories.ts
+++ projects/elements/src/dropdown/dropdown.stories.ts
@@ -40,6 +40,26 @@ export const Interactive = {
   `
 };
 
+export const DynamicTrigger = {
+  render: () => html`
+<div id="dynamic-trigger-demo" nve-layout="row align:center" style="height: 250px">
+  <nve-dropdown behavior-trigger hidden>hello there</nve-dropdown>
+  <nve-button>button</nve-button>
+  <nve-button>button</nve-button>
+  <nve-button>button</nve-button>
+  <script type="module">
+    const dropdown = document.querySelector('#dynamic-trigger-demo nve-dropdown');
+    document.querySelector('#dynamic-trigger-demo').addEventListener('mousedown', e => {
+      if (e.target.tagName === 'MLV-BUTTON') {
+        dropdown.anchor = e.target;
+        dropdown.trigger = e.target;
+      }
+    });
+  </script>
+</div>
+  `
+};
+
 export const BehaviorTrigger = {
   inline: false,
   render: () => html`
