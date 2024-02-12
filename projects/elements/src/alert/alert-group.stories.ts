4d78b47d8c4740cf23f3748147ceeeedadc9baba
Cory Rylan <crylan@nvidia.com>
2024-02-12 11:29:41 -0600

    fix(core): alert multiline alignment
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/alert/alert-group.stories.ts
+++ projects/elements/src/alert/alert-group.stories.ts
@@ -2,6 +2,7 @@ import { html } from 'lit';
 import { AlertGroup } from '@elements/elements/alert';
 import '@elements/elements/alert/define.js';
 import '@elements/elements/button/define.js';
+import '@elements/elements/divider/define.js';
 
 export default {
   title: 'Elements/Alert Group/Examples',
@@ -181,9 +182,15 @@ export const Multiline = {
   render: () => html`
     <mlv-alert-group status="danger">
       <mlv-alert>
-        <div mlv-layout="column gap:xs">
-          <p mlv-text="body sm medium">Session Paused:</p>
+        Session Paused:
+        <div slot="content" mlv-layout="column gap:sm align:stretch">
           <p mlv-text="body sm">Failed to verify drives. <a href="#" mlv-text="link">Check Status</a></p>
+          <mlv-divider style="--color: var(--mlv-sys-text-muted-color)"></mlv-divider>
+          <pre mlv-text="code sm" style="background: transparent !important; margin: 0;">
+10:06:01 AM [verify] update /virtual:/verify/
+10:06:11 AM [build] update /virtual:/build/
+10:06:22 AM [error] update /virtual:/error/ - failed to connect to remote
+          </pre>
         </div>
       </mlv-alert>
     </mlv-alert-group>
