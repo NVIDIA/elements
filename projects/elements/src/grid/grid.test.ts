25fd5f5ce08c45f600ca5546db9ba42371e3c03d
Cory Rylan <crylan@nvidia.com>
2024-02-14 16:18:59 -0600

    feat(elements): enable scrollto positioning api for grid
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/grid/grid.test.ts
+++ projects/elements/src/grid/grid.test.ts
@@ -99,3 +99,60 @@ describe('nve-grid id check', () => {
     expect(element.id).toBe('test');
   });
 });
+
+describe('nve-grid', () => {
+  let fixture: HTMLElement;
+  let element: Grid;
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-grid style="height: 100px">
+        <nve-grid-header>
+          <nve-grid-column>column 1</nve-grid-column>
+          <nve-grid-column>column 2</nve-grid-column>
+          <nve-grid-column>column 3</nve-grid-column>
+          <nve-grid-column>column 4</nve-grid-column>
+        </nve-grid-header>
+        <nve-grid-row>
+          <nve-grid-cell>cell 1-1</nve-grid-cell>
+          <nve-grid-cell>cell 1-2</nve-grid-cell>
+          <nve-grid-cell>cell 1-3</nve-grid-cell>
+          <nve-grid-cell>cell 1-4</nve-grid-cell>
+        </nve-grid-row>
+        <nve-grid-row>
+          <nve-grid-cell>cell 2-1</nve-grid-cell>
+          <nve-grid-cell>cell 2-2</nve-grid-cell>
+          <nve-grid-cell>cell 2-3</nve-grid-cell>
+          <nve-grid-cell>cell 2-4</nve-grid-cell>
+        </nve-grid-row>
+        <nve-grid-row>
+          <nve-grid-cell>cell 3-1</nve-grid-cell>
+          <nve-grid-cell>cell 3-2</nve-grid-cell>
+          <nve-grid-cell>cell 3-3</nve-grid-cell>
+          <nve-grid-cell>cell 3-4</nve-grid-cell>
+        </nve-grid-row>
+        <nve-grid-row>
+          <nve-grid-cell>cell 4-1</nve-grid-cell>
+          <nve-grid-cell>cell 4-2</nve-grid-cell>
+          <nve-grid-cell>cell 4-3</nve-grid-cell>
+          <nve-grid-cell>cell 4-4</nve-grid-cell>
+        </nve-grid-row>
+      </nve-grid>
+    `);
+    element = fixture.querySelector('nve-grid');
+    await elementIsStable(element);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should allow scroll position to be set', async () => {
+    await elementIsStable(element);
+    expect(element.shadowRoot.querySelector('[part="scrollbox"]').scrollTop).toBe(0);
+
+    element.scrollTo({ top: 20 });
+    await elementIsStable(element);
+    expect(element.shadowRoot.querySelector('[part="scrollbox"]').scrollTop).toBe(20);
+  });
+});
