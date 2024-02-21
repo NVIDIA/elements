6fe20862712a817624e55fdf32af62c2c90d7bab
Cory Rylan <crylan@nvidia.com>
2024-02-21 09:35:54 -0600

    fix(core): filter chip validation
    - fixes missing validation state style for filter chip inputs
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/progressive-filter-chip/progressive-filter-chip.stories.ts
+++ projects/elements/src/progressive-filter-chip/progressive-filter-chip.stories.ts
@@ -12,18 +12,24 @@ export default {
 export const Default = {
   render: () => html`
     <mlv-progressive-filter-chip closable>
-      <select aria-label="dataset">
-        <option>workload</option>
-        <option selected>instance</option>
-      </select>
-      <select aria-label="condition">
-        <option>sort by</option>
-        <option selected>filter by</option>
-      </select>
-      <select aria-label="filter">
-        <option selected>utilization</option>
-        <option>status</option>
-      </select>
+      <mlv-select>
+        <select aria-label="dataset">
+          <option>workload</option>
+          <option selected>instance</option>
+        </select>
+      </mlv-select>
+      <mlv-select>
+        <select aria-label="condition">
+          <option>sort by</option>
+          <option selected>filter by</option>
+        </select>
+      </mlv-select>
+      <mlv-select>
+        <select aria-label="filter">
+          <option selected>utilization</option>
+          <option>status</option>
+        </select>
+      </mlv-select>
     </mlv-progressive-filter-chip>
   `
 }
@@ -31,18 +37,24 @@ export const Default = {
 export const Layer = {
   render: () => html`
     <mlv-progressive-filter-chip closable>
-      <select aria-label="dataset">
-        <option>workload</option>
-        <option selected>instance</option>
-      </select>
-      <select aria-label="condition">
-        <option>sort by</option>
-        <option selected>filter by</option>
-      </select>
-      <select aria-label="filter">
-        <option selected>utilization</option>
-        <option>status</option>
-      </select>
+      <mlv-select>
+        <select aria-label="dataset">
+          <option>workload</option>
+          <option selected>instance</option>
+        </select>
+      </mlv-select>
+      <mlv-select>
+        <select aria-label="condition">
+          <option>sort by</option>
+          <option selected>filter by</option>
+        </select>
+      </mlv-select>
+      <mlv-select>
+        <select aria-label="filter">
+          <option selected>utilization</option>
+          <option>status</option>
+        </select>
+      </mlv-select>
     </mlv-progressive-filter-chip>
   `
 }
@@ -50,20 +62,26 @@ export const Layer = {
 export const Multiple = {
 render: () => html`
     <mlv-progressive-filter-chip closable>
-      <select aria-label="dataset">
-        <option selected>status</option>
-        <option>workload</option>
-      </select>
-      <select aria-label="condition">
-        <option selected>filter by</option>
-        <option>sort by</option>
-      </select>
-      <select multiple aria-label="filter">
-        <option selected>progress</option>
-        <option>pending</option>
-        <option>success</option>
-        <option>failed</option>
-      </select>
+      <mlv-select>
+        <select aria-label="dataset">
+          <option selected>status</option>
+          <option>workload</option>
+        </select>
+      </mlv-select>
+      <mlv-select>
+        <select aria-label="condition">
+          <option selected>filter by</option>
+          <option>sort by</option>
+        </select>
+      </mlv-select>
+      <mlv-select>
+        <select multiple aria-label="filter">
+          <option selected>progress</option>
+          <option>pending</option>
+          <option>success</option>
+          <option>failed</option>
+        </select>
+      </mlv-select>
     </mlv-progressive-filter-chip>
   `
 }
@@ -71,16 +89,22 @@ render: () => html`
 export const TextInput = {
   render: () => html`
     <mlv-progressive-filter-chip closable>
-      <select aria-label="dataset">
-        <option selected>memory</option>
-        <option>CPU</option>
-      </select>
-      <select aria-label="condition">
-        <option selected>greater than</option>
-        <option>less than</option>
-        <option>equal to</option>
-      </select>
-      <input type="number" min="0" max="100" value="50" aria-label="filter" />
+      <mlv-select>
+        <select aria-label="dataset">
+          <option selected>memory</option>
+          <option>CPU</option>
+        </select>
+      </mlv-select>
+      <mlv-select>
+        <select aria-label="condition">
+          <option selected>greater than</option>
+          <option>less than</option>
+          <option>equal to</option>
+        </select>
+      </mlv-select>
+      <mlv-input>
+        <input type="number" min="0" max="100" value="50" aria-label="filter" />
+      </mlv-input>
     </mlv-progressive-filter-chip>
   `
 }
@@ -88,12 +112,18 @@ export const TextInput = {
 export const DateRange = {
   render: () => html`
     <mlv-progressive-filter-chip>
-      <select aria-label="date type">
-        <option>recording date</option>
-        <option>process date</option>
-      </select>
-      <input type="date" value="2022-05-11" aria-label="start date" />
-      <input type="date" value="2022-12-07" aria-label="end date" />
+      <mlv-select>
+        <select aria-label="date type">
+          <option>recording date</option>
+          <option>process date</option>
+        </select>
+      </mlv-select>
+      <mlv-date>
+        <input type="date" value="2022-05-11" aria-label="start date" />
+      </mlv-date>
+      <mlv-date>
+        <input type="date" value="2022-12-07" aria-label="end date" />
+      </mlv-date>
     </mlv-progressive-filter-chip>
     `
 }
@@ -101,14 +131,18 @@ export const DateRange = {
 export const Custom = {
   render: () => html`
     <mlv-progressive-filter-chip closable>
-      <select aria-label="dataset">
-        <option>GPS</option>
-        <option selected>Cellular</option>
-      </select>
-      <select aria-label="condition">
-        <option>contains</option>
-        <option>excludes</option>
-      </select>
+      <mlv-select>
+        <select aria-label="dataset">
+          <option>GPS</option>
+          <option selected>Cellular</option>
+        </select>
+      </mlv-select>
+      <mlv-select>
+        <select aria-label="condition">
+          <option>contains</option>
+          <option>excludes</option>
+        </select>
+      </mlv-select>
       <mlv-button style="width: 190px" id="map-btn">37.3706254,-121.9671894</mlv-button>
     </mlv-progressive-filter-chip>
     <mlv-dropdown anchor="map-btn" trigger="map-btn">
@@ -118,5 +152,30 @@ export const Custom = {
       </mlv-input>
       <img src="images/test-map-2.webp" width="300px" alt="non-interactive demo map" />
     </mlv-dropdown>
+  `
+}
+
+export const Validation = {
+  render: () => html`
+<div mlv-layout="column gap:lg">
+  <mlv-progressive-filter-chip closable>
+    <mlv-input>
+      <input type="text" value="validation" required />
+      <mlv-control-message error="valueMissing">required</mlv-control-message>
+    </mlv-input>
+    <mlv-input>
+      <input type="text" />
+    </mlv-input>
+  </mlv-progressive-filter-chip>
+
+  <mlv-progressive-filter-chip closable>
+    <mlv-input status="error">
+      <input type="text" value="manual validation" formnovalidate />
+    </mlv-input>
+    <mlv-input>
+      <input type="text" />
+    </mlv-input>
+  </mlv-progressive-filter-chip>
+</div>
   `
 }
