bca36e2d0e41a7ad66a2ef630f729be05a78526c
Cory Rylan <crylan@nvidia.com>
2024-02-21 17:49:03 -0600

    chore(elements): rename internal events
    - rename internal custom events to convention
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/notification/notification.ts
+++ projects/elements/src/notification/notification.ts
@@ -32,7 +32,6 @@ import styles from './notification.css?inline';
  * @cssprop --box-shadow
  * @cssprop --min-width
  * @cssprop --width
- * @cssprop --nve-sys-layer-popover-offset
  * @storybook https://NVIDIA.github.io/elements/api/?path=/docs/elements-notification-documentation--docs
  * @figma https://www.figma.com/file/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog---WIP?node-id=2876-64384&t=iOYah8Uct8CFd69k-0
  * @aria https://www.w3.org/WAI/ARIA/apg/patterns/alertdialog/
