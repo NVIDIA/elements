6ee368d0a92cdbd25c35762f926938c1a58327d8
Cory Rylan <crylan@nvidia.com>
2024-02-23 16:29:23 -0600

    chore(core): cleanup api doc interface
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/grid/grid.test.lighthouse.ts
+++ projects/elements/src/grid/grid.test.lighthouse.ts
@@ -24,7 +24,7 @@ describe('grid lighthouse report', () => {
     `);
 
     expect(report.scores.performance).toBe(100);
-    expect(report.scores.accessibility).toBe(76); // due to Lighthouse reporter not supporting Element Internals for grid attribute reflection
+    expect(report.scores.accessibility).toBe(83);
     expect(report.scores.bestPractices).toBe(100);
     expect(report.payload.javascript.kb).toBeLessThan(20);
   });
