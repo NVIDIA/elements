9d68574f86cd2a011b1c17d776551fbed7501317
Cory Rylan <crylan@nvidia.com>
2024-03-25 15:04:30 -0500

    fix(core): add custom-state-set polyfill
    - adds polyfill for Chromium < 123
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/polyfills/index.ts
+++ projects/elements/src/polyfills/index.ts
@@ -1,8 +1,7 @@
-
 /**
  * - Vitest (happy-dom/js-dom)
  * - Safari
- * 
+ *
  * Used for Custom Elements Form/A11y APIs
  * https://github.com/whatwg/html/pull/8467
  * https://bugs.webkit.org/show_bug.cgi?id=215911
@@ -11,17 +10,18 @@
 import 'construct-style-sheets-polyfill';
 import 'element-internals-polyfill';
 import 'formdata-polyfill';
+import './custom-state-set.js';
 
 /**
  * https://developer.mozilla.org/en-US/docs/Web/API/Element/toggleAttribute
  * - Vitest (happy-dom/js-dom)
  * - Safari
- * 
+ *
  * Used in element-internals-polyfill
  */
 if (!Element.prototype.toggleAttribute) {
-  Element.prototype.toggleAttribute = function(name, force) {
-    if(force !== void 0) {
+  Element.prototype.toggleAttribute = function (name, force) {
+    if (force !== void 0) {
       force = !!force;
     }
 
