d95cdc329174143e2a8ec36c077c52b386dace7a
Cory Rylan <crylan@nvidia.com>
2024-03-25 15:04:30 -0500

    fix(core): add custom-state-set polyfill
    - adds polyfill for Chromium < 123
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/elements/src/polyfills/custom-state-set.ts
@@ -0,0 +1,72 @@
+/**
+ * CustomStateSet Polyfill for :state() syntax downleveled to legacy :--state syntax (Chromium < v123)
+ * https://html.spec.whatwg.org/multipage/custom-elements.html#exposing-custom-element-states
+ */
+
+try {
+  globalThis.document.body.matches(':state(supported)');
+} catch {
+  customStateSetPolyfill();
+}
+
+class CustomStateSetPolyfill extends Set {
+  #stateSet = null;
+
+  constructor(stateSet) {
+    super();
+    this.#stateSet = stateSet;
+  }
+
+  add(state) {
+    super.add(state);
+    try {
+      // try latest syntax :state()
+      this.#stateSet?.add(state);
+    } catch {
+      // else fallback to legacy syntax :--state
+      this.#stateSet?.add(`--${state}`);
+    }
+    return this;
+  }
+
+  delete(state) {
+    super.delete(state);
+    this.#stateSet?.delete(state);
+    this.#stateSet?.delete(`--${state}`);
+    return true;
+  }
+
+  clear() {
+    for (const state of this) this.delete(state);
+  }
+}
+
+function customStateSetPolyfill() {
+  const attachInternals = HTMLElement.prototype.attachInternals;
+  Object.defineProperty(HTMLElement.prototype, 'attachInternals', {
+    value: function (...args) {
+      const internals = attachInternals.call(this, args);
+      Object.defineProperty(internals, 'states', { value: new CustomStateSetPolyfill(internals.states) });
+
+      return internals;
+    }
+  });
+
+  const matches = Element.prototype.matches;
+  Object.defineProperty(Element.prototype, 'matches', {
+    value: function (text) {
+      return matches.call(this, replaceToLegacyState(text));
+    }
+  });
+
+  const replaceSync = CSSStyleSheet.prototype.replaceSync;
+  Object.defineProperty(CSSStyleSheet.prototype, 'replaceSync', {
+    value: function (text) {
+      replaceSync.call(this, replaceToLegacyState(text));
+    }
+  });
+}
+
+function replaceToLegacyState(value: string) {
+  return value.replace(/:state\(([^\)]+)\)/g, ':where(:state($1), :--$1, [state-$1])'); // eslint-disable-line
+}
