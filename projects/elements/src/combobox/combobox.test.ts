5aab775394c70220b1d145b6e6bb1819090d8f25
Cory Rylan <crylan@nvidia.com>
2024-04-03 09:25:09 -0500

    fix(core): combobox icon slot
    - fixes regression with icon slot name
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/combobox/combobox.test.ts
+++ projects/elements/src/combobox/combobox.test.ts
@@ -206,6 +206,10 @@ describe('nve-combobox', () => {
 
     expect(element.matches(':state(footer-content)')).toBe(true);
   });
+
+  it('should provide a prefix-icon slot', async () => {
+    expect(element.shadowRoot.querySelector('slot[name="prefix-icon"]')).toBeTruthy();
+  });
 });
 
 describe('nve-combobox single select', () => {
