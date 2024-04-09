7e51a57c72fbf1d9886c5c7ad1dfbcb9cdfb1722
Cory Rylan <crylan@nvidia.com>
2024-04-09 17:21:49 -0500

    fix(core): test release
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/index.ts
+++ projects/elements/src/index.ts
@@ -3,4 +3,4 @@ export const VERSION = '0.0.0';
 export { I18nService, getThemeTokens } from '@nvidia-elements/core/internal';
 export * from './internal/types/index.js';
 
-// test release 2
+// test release 3
