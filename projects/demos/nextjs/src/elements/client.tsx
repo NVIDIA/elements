6ab36ac88dcd75a447c107a5a2603e740c35ef8d
Cory Rylan <crylan@nvidia.com>
2024-04-12 16:53:50 -0500

    chore(demos): update nextjs demo
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/demos/nextjs/src/elements/client.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-
-/**
- * This approach will load and render elements on the client side only.
- */
-export const MlvButton = dynamic(async () => (await import('@elements/elements-react/button')).MlvButton, { ssr: false });
-export const MlvAlert = dynamic(async () => (await import('@elements/elements-react/alert')).MlvAlert, { ssr: false });
-export const MlvDialog = dynamic(async () => (await import('@elements/elements-react/dialog')).MlvDialog, { ssr: false });
