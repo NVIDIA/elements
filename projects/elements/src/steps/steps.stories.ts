be098387e5ec69b89bd45794296cf8129996ca7e
Will Geller <wgeller@nvidia.com>
2024-04-16 16:29:33 -0400

    feat(core): rename stepper to steps

---
+++ projects/elements/src/steps/steps.stories.ts
@@ -0,0 +1,70 @@
+import { html } from 'lit';
+import '@nvidia-elements/core/steps/define.js';
+import '@nvidia-elements/core/card/define.js';
+import '@nvidia-elements/core/dot/define.js';
+import '@nvidia-elements/core/icon/define.js';
+
+export default {
+  title: 'Elements/Steps/Examples',
+  component: 'nve-steps',
+};
+
+export const Default = {
+  render: () => html`
+  <nve-steps behavior-select>
+    <nve-steps-item status="success">Step 1</nve-steps-item>
+    <nve-steps-item status="danger">Step 2</nve-steps-item>
+    <nve-steps-item selected>Step 3</nve-steps-item>
+    <nve-steps-item status="pending">Step 4</nve-steps-item>
+    <nve-steps-item disabled>Disabled</nve-steps-item>
+  </nve-steps>
+  `
+};
+
+export const Condensed = {
+  render: () => html`
+  <nve-steps behavior-select container="condensed">
+    <nve-steps-item status="success">Step 1</nve-steps-item>
+    <nve-steps-item status="danger">Step 2</nve-steps-item>
+    <nve-steps-item selected>Step 3</nve-steps-item>
+    <nve-steps-item status="pending">Step 4</nve-steps-item>
+    <nve-steps-item disabled>Disabled</nve-steps-item>
+  </nve-steps>
+  `
+};
+
+export const VerticalSteps = {
+  render: () => html`
+  <nve-steps vertical behavior-select style="width: 150px">
+    <nve-steps-item selected>Step 1</nve-steps-item>
+    <nve-steps-item>Step 2</nve-steps-item>
+    <nve-steps-item>Step 3</nve-steps-item>
+    <nve-steps-item disabled>Disabled</nve-steps-item>
+    <nve-steps-item>Step 5</nve-steps-item>
+  </nve-steps>
+  `
+};
+
+export const VerticalCondensedSteps = {
+  render: () => html`
+  <nve-steps vertical container="condensed" behavior-select>
+    <nve-steps-item selected>Step 1</nve-steps-item>
+    <nve-steps-item>Step 2</nve-steps-item>
+    <nve-steps-item>Step 3</nve-steps-item>
+    <nve-steps-item disabled>Disabled</nve-steps-item>
+    <nve-steps-item>Step 5</nve-steps-item>
+  </nve-steps>
+  `
+};
+
+export const StatelessSteps = {
+  render: () => html`
+  <nve-steps>
+    <nve-steps-item selected>Step 1</nve-steps-item>
+    <nve-steps-item>Step 2</nve-steps-item>
+    <nve-steps-item>Step 3</nve-steps-item>
+    <nve-steps-item>Step 4</nve-steps-item>
+    <nve-steps-item disabled>Disabled</nve-steps-item>
+  </nve-steps>
+  `
+};
---
+++ projects/elements/src/steps/steps.test.lighthouse.ts
@@ -0,0 +1,22 @@
+import { expect, test, describe } from 'vitest';
+import { runner } from 'elements-lighthouse';
+
+describe('steps lighthouse report', () => {
+  test('steps should meet lighthouse benchmarks', async () => {
+    const report = await runner.getReport('nve-steps', /* html */`
+      <nve-steps>
+        <nve-steps-item>Step 1</nve-steps-item>
+        <nve-steps-item selected>Step 2</nve-steps-item>
+        <nve-steps-item disabled>Step 3</nve-steps-item>
+      </nve-steps>
+      <script type="module">
+        import '@elements/elements/steps/define.js';
+      </script>
+    `);
+
+    expect(report.scores.performance).toBe(100);
+    expect(report.scores.accessibility).toBe(100);
+    expect(report.scores.bestPractices).toBe(100);
+    expect(report.payload.javascript.kb).toBeLessThan(20);
+  });
+});
