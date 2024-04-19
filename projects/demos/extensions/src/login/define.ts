739764e1e51d395eda281fedbb3316449bb398a5
Cory Rylan <crylan@nvidia.com>
2024-04-19 12:54:57 -0500

    chore(starters): update starters
    - update starters directory
    - update starter install steps
    - add npmrc to all starters
    - fix storybook list style issue
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/demos/extensions/src/login/define.ts
+++
@@ -1,9 +0,0 @@
-import { DomainLogin } from 'extensions-elements-starter/login';
-
-customElements.get('domain-login') || customElements.define('domain-login', DomainLogin);
-
-declare global {
-  interface HTMLElementTagNameMap {
-    ['domain-login']: DomainLogin;
-  }
-}
