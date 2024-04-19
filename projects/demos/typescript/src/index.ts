847d6b25a396ee6dd4f45756199d2c295fbff151
Cory Rylan <crylan@nvidia.com>
2024-04-19 12:54:57 -0500

    chore(starters): update starters
    - update starters directory
    - update starter install steps
    - add npmrc to all starters
    - fix storybook list style issue
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/demos/typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-import '@elements/elements/app-header/define.js';
-import '@elements/elements/button/define.js';
-import '@elements/elements/breadcrumb/define.js';
-import '@elements/elements/drawer/define.js';
-import '@elements/elements/menu/define.js';
-import './index.css';
