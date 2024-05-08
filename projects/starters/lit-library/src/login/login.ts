c5393cb4a77172d394d5591a3170d581daa0840d
Cory Rylan <crylan@nvidia.com>
2024-05-08 11:28:47 -0500

    chore(starters): add lit library starter
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/starters/lit-library/src/login/login.ts
@@ -0,0 +1,117 @@
+import { html, LitElement, unsafeCSS } from 'lit';
+import { property } from 'lit/decorators/property.js';
+import styles from './login.css?inline';
+
+export class DomainLogin extends LitElement {
+  @property({ type: String }) value = `{ "email": "", "password": "" }`;
+
+  get form() {
+    return this.#internals.form;
+  }
+
+  get name() {
+    return this.getAttribute('name');
+  }
+
+  get type() {
+    return this.localName;
+  }
+
+  get validity() {
+    return this.#internals.validity;
+  }
+
+  get validationMessage() {
+    return this.#internals.validationMessage;
+  }
+
+  get willValidate() {
+    return this.#internals.willValidate;
+  }
+
+  static styles = [unsafeCSS(styles)];
+
+  static formAssociated = true;
+
+  get #value() {
+    return JSON.parse(this.value);
+  }
+
+  get #form() {
+    return this.shadowRoot?.querySelector<HTMLFormElement>('form') as HTMLFormElement;
+  }
+
+  #internals = this.attachInternals();
+
+  render() {
+    return html`
+      <form>
+        <mlv-input>
+          <label>Email</label>
+          <input type="email" name="email" required pattern=".+@nvidia\.com" autocomplete="off" @input=${this.#input} .value=${this.#value.email} />
+          <mlv-control-message error="valueMissing">required</mlv-control-message>
+          <mlv-control-message error="patternMismatch">invalid NVIDIA email</mlv-control-message>
+        </mlv-input>
+
+        <mlv-password>
+          <label>Password</label>
+          <input type="password" name="password" required @input=${this.#input} .value=${this.#value.password} />
+          <mlv-control-message error="valueMissing">required</mlv-control-message>
+        </mlv-password>
+
+        <mlv-checkbox>
+          <label>remember me</label>
+          <input type="checkbox" name="remember" value="on" @change=${this.#change} @input=${this.#input} ?checked=${this.#value.remember} />
+        </mlv-checkbox>
+      </form>
+    `;
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    this.tabIndex = 0;
+    this.#internals.setFormValue(this.value);
+    await this.updateComplete;
+    this.checkValidity();
+  }
+
+  checkValidity() {
+    const validation = Array.from(this.#form.elements)
+      .flatMap((e: any) => e.validity)
+      .reduce((p, validity) => {
+        const keys = p;
+        for (const key in validity) {
+          if (validity[key]) {
+            keys[key] = true;
+          }
+        }
+        return keys;
+      }, {});
+    this.#internals.setValidity(validation, 'invalid login');
+    this.#internals.checkValidity();
+  }
+
+  reportValidity() {
+    this.#internals.reportValidity();
+  }
+
+  #input(e: Event) {
+    this.#update(e);
+    this.dispatchEvent(new Event('input', { bubbles: true }));
+  }
+
+  #change(e: Event) {
+    this.#update(e);
+    this.dispatchEvent(new Event('change', { bubbles: true }));
+  }
+
+  #update(e: Event) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    const values = Object.fromEntries(new FormData(this.#form) as any);
+    this.value = JSON.stringify(values);
+    this.#internals.setFormValue(this.value);
+    this.checkValidity();
+  }
+}
