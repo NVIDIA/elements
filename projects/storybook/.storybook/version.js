c2c444c52488a08e2c8e05481f58aaf794f4c3f8
Cory Rylan <crylan@nvidia.com>
2024-05-14 06:34:59 -0700

    chore(docs): version check timeout
    - fallback version if request is slow and prevent blocking render
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/storybook/.storybook/version.js
+++ projects/storybook/.storybook/version.js
@@ -1,2 +1,12 @@
 
-export const ELEMENTS_VERSION = (await fetch('https://https://esm.sh/@elements/elements@latest/package.json').then(r => r.json())).version;
+const controller = new AbortController();
+const timeout = setTimeout(() => controller.abort(), 3000);
+const LATEST_URL = 'https://https://esm.sh/@elements/elements@latest/package.json';
+
+export const ELEMENTS_VERSION = await fetch(LATEST_URL, { signal: controller.signal }).then(async (response) => {
+  clearTimeout(timeout);
+  return (await response.json()).version;
+}).catch(() => {
+  console.warn('Could not featch latest version from https://https://esm.sh');
+  return '0.0.0';
+});
