428141bafda28a1e40914381374a69078838b79f
Dhruvi Shah <dhruvis@nvidia.com>
2024-05-30 15:54:48 -0700

    feat(elements): dropdown header and footer

--- projects/elements/src/dropdown/define.ts
+++ projects/elements/src/dropdown/define.ts
@@ -1,11 +1,15 @@
 import { define } from '@nvidia-elements/core/internal';
-import { Dropdown } from '@nvidia-elements/core/dropdown';
+import { Dropdown, DropdownHeader, DropdownFooter } from '@nvidia-elements/core/dropdown';
 import '@nvidia-elements/core/icon-button/define.js';
 
 define(Dropdown);
+define(DropdownFooter);
+define(DropdownHeader);
 
 declare global {
   interface HTMLElementTagNameMap {
     'nve-dropdown': Dropdown;
+    'nve-dropdown-footer': DropdownFooter;
+    'nve-dropdown-header': DropdownHeader;
   }
 }
---
+++ projects/elements/src/dropdown/dropdown-footer.test.ts
@@ -0,0 +1,33 @@
+import { html } from 'lit';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { createFixture, removeFixture, elementIsStable } from '@nvidia-elements/testing';
+import { DropdownFooter } from '@nvidia-elements/core/dropdown';
+import '@nvidia-elements/core/dropdown/define.js';
+
+describe(DropdownFooter.metadata.tag, () => {
+  let fixture: HTMLElement;
+  let element: DropdownFooter;
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-dropdown>
+        <nve-dropdown-footer>hello</nve-dropdown-footer>
+      </nve-dropdown>
+    `);
+    element = fixture.querySelector(DropdownFooter.metadata.tag);
+    await elementIsStable(element);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should define element', () => {
+    expect(customElements.get(DropdownFooter.metadata.tag)).toBeDefined();
+  });
+
+  it('should render with the footer default slot', async () => {
+    await elementIsStable(element);
+    expect(element.slot).toBe('footer');
+  });
+});
