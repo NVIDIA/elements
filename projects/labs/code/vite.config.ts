5844445ffad3cf11898bc154a871c2baedc66df8
Cory Rylan <crylan@nvidia.com>
2024-06-04 15:48:57 -0500

    chore(labs-code): fix vite config paths
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/labs/code/vite.config.ts
+++ projects/labs/code/vite.config.ts
@@ -6,6 +6,21 @@ export default defineConfig(() => {
   const config: UserConfig = {
     resolve: {
       alias: { '@nvidia-elements/code': resolve(import.meta.dirname, './src') }
+    },
+    build: {
+      lib: {
+        entry: {
+          'codeblock/languages/css': resolve(import.meta.dirname, './src/codeblock/languages/css.ts'),
+          'codeblock/languages/go': resolve(import.meta.dirname, './src/codeblock/languages/go.ts'),
+          'codeblock/languages/javascript': resolve(import.meta.dirname, './src/codeblock/languages/javascript.ts'),
+          'codeblock/languages/json': resolve(import.meta.dirname, './src/codeblock/languages/json.ts'),
+          'codeblock/languages/markdown': resolve(import.meta.dirname, './src/codeblock/languages/markdown.ts'),
+          'codeblock/languages/python': resolve(import.meta.dirname, './src/codeblock/languages/python.ts'),
+          'codeblock/languages/typescript': resolve(import.meta.dirname, './src/codeblock/languages/typescript.ts'),
+          'codeblock/languages/xml': resolve(import.meta.dirname, './src/codeblock/languages/xml.ts'),
+          'codeblock/languages/yaml': resolve(import.meta.dirname, './src/codeblock/languages/yaml.ts')
+        }
+      }
     }
   };
 
