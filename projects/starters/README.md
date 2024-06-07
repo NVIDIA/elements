f04e6f66df1e035b0db739b6e1a427892aa76116
Cory Rylan <crylan@nvidia.com>
2024-06-07 11:53:53 -0500

    chore(starters): remove angular legacy example
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/starters/README.md
+++ projects/starters/README.md
@@ -3,7 +3,6 @@
 This directory contains a suite of standardized starter apps for kickstarting and using Elements in a variety of Web technologies.
 
 - `/angular` - Angular app built with [Angular CLI](https://angular.io/cli)
-- `/angular-legacy` - Angular 12 app built with [Angular CLI](https://angular.io/cli)
 - `/buildless` - Plain HTML with import maps, no build step required.
 - `/extensions` - Build setup for highly reusable and resiliant Web Components.
 - `/mpa` - Multi Page App setup for when you just need something simple.
--- projects/starters/vue/src/App.vue
+++ projects/starters/vue/src/App.vue
@@ -47,12 +47,6 @@ import '@elements/elements/menu/define.js';
               Angular
             </a>
           </nve-menu-item>
-          <nve-menu-item>
-            <a href="/elements/starters/angular-legacy/" style="text-transform: none;">
-              <img src="/angular-legacy.svg" width="18px" height="18px" alt="angular legacy logo" />
-              Angular Legacy (v12/2021)
-            </a>
-          </nve-menu-item>
           <nve-menu-item current="page">
             <a href="/elements/starters/vue/">
               <img src="/vue.svg" width="18px" height="18px" alt="vue logo" />
