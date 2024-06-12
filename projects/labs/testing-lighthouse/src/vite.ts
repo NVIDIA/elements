63b2c7eb37d27ecb610b69ae45e1e737f8660fc4
Cory Rylan <crylan@nvidia.com>
2024-06-12 17:23:23 -0500

    chore(labs-testing-lighthouse): remove incorrect default includes path
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/labs/testing-lighthouse/src/vite.ts
+++ projects/labs/testing-lighthouse/src/vite.ts
@@ -13,7 +13,7 @@ export const lighthouseConfig = {
     },
     reporters: ['basic', 'junit'],
     globalSetup: ['@nvidia-elements/testing-lighthouse/vite-setup.js'],
-    include: [process.env.LIGHTHOUSE_ALL ? 'src/**/*.test.lighthouse.ts' : 'src/index.test.lighthouse.ts'],
+    include: ['src/**/*.test.lighthouse.ts'],
     testTimeout: 60000,
     server: {
       deps: {
