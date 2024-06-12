434ef97edb98bf0cfeb569ba2ebc5a2cd37e4f8c
Cory Rylan <crylan@nvidia.com>
2024-06-12 17:25:38 -0500

    chore(core): simplify vite configs
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/vitest.axe.ts
+++ projects/elements/vitest.axe.ts
@@ -1,18 +1,9 @@
 import { resolve } from 'path';
-import { defineConfig, mergeConfig } from 'vitest/config';
-import { libraryTestConfig } from '@internals/vite';
+import { mergeConfig } from 'vitest/config';
+import { libraryAxeTestConfig } from '@internals/vite';
 
-export default defineConfig(() => {
-  const config = {
-    test: {
-      include: ['./src/**/*.test.axe.ts'],
-      alias: { '@nvidia-elements/core': resolve(import.meta.dirname, './src') },
-      setupFiles: [resolve(import.meta.dirname, './src/test/setup.ts')],
-      outputFile: {
-        junit: './coverage/axe/junit.xml'
-      }
-    }
-  };
-
-  return mergeConfig(libraryTestConfig, config);
+export default mergeConfig(libraryAxeTestConfig, {
+  test: {
+    alias: { '@nvidia-elements/core': resolve(import.meta.dirname, './dist') }
+  }
 });
