a2a28bee93d243f8528c6d7970779892bf5b4292
Cory Rylan <crylan@nvidia.com>
2024-06-13 00:54:09 -0500

    chore(ci): lighthouse pipeline
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/labs/code/src/codeblock/codeblock.test.lighthouse.ts
+++ projects/labs/code/src/codeblock/codeblock.test.lighthouse.ts
@@ -17,35 +17,4 @@ describe('codeblock lighthouse report', () => {
     expect(report.scores.bestPractices).toBe(100);
     expect(report.payload.javascript.kb).toBeLessThan(18.5);
   });
-
-  test('codeblock should meet bundle limits for all languages', async () => {
-    const report = await runner.getReport('nve-codeblock', /* html */`
-      <script type="module">
-      import('@nvidia-elements/code/codeblock/languages/css.js');
-      import('@nvidia-elements/code/codeblock/languages/go.js');
-      import('@nvidia-elements/code/codeblock/languages/html.js');
-      import('@nvidia-elements/code/codeblock/languages/javascript.js');
-      import('@nvidia-elements/code/codeblock/languages/json.js');
-      import('@nvidia-elements/code/codeblock/languages/markdown.js');
-      import('@nvidia-elements/code/codeblock/languages/python.js');
-      import('@nvidia-elements/code/codeblock/languages/typescript.js');
-      import('@nvidia-elements/code/codeblock/languages/xml.js');
-      import('@nvidia-elements/code/codeblock/languages/yaml.js');
-      import('@nvidia-elements/code/codeblock/define.js');
-      </script>
-    `);
-
-    expect(report.payload.javascript.kb).toBeLessThan(38);
-    expect(report.payload.javascript.requests['define.js'].kb).toBeLessThan(10);
-    expect(report.payload.javascript.requests['core.js'].kb).toBeLessThan(9);
-    expect(report.payload.javascript.requests['css.js'].kb).toBeLessThan(4);
-    expect(report.payload.javascript.requests['html.js'].kb).toBeLessThan(0.5);
-    expect(report.payload.javascript.requests['javascript.js'].kb).toBeLessThan(3);
-    expect(report.payload.javascript.requests['json.js'].kb).toBeLessThan(1);
-    expect(report.payload.javascript.requests['markdown.js'].kb).toBeLessThan(1.5);
-    expect(report.payload.javascript.requests['python.js'].kb).toBeLessThan(2);
-    expect(report.payload.javascript.requests['typescript.js'].kb).toBeLessThan(3.5);
-    expect(report.payload.javascript.requests['xml.js'].kb).toBeLessThan(0.5);
-    expect(report.payload.javascript.requests['yaml.js'].kb).toBeLessThan(1.5);
-  });
 });
