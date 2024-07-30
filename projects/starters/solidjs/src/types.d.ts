2f0a16d70acfa54a2347f0efab06e2823de95b07
Cory Rylan <crylan@nvidia.com>
2024-07-30 14:57:08 -0600

    chore(starters): update dependencies
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/starters/solidjs/src/types.d.ts
+++ projects/starters/solidjs/src/types.d.ts
@@ -1,11 +1,11 @@
-import type { AppHeader } from '@elements/elements/app-header';
-import type { Breadcrumb } from '@elements/elements/breadcrumb';
-import type { Button } from '@elements/elements/button';
-import type { IconButton } from '@elements/elements/icon-button';
-import type { Icon } from '@elements/elements/icon';
-import type { Drawer, DrawerContent, DrawerFooter, DrawerHeader } from '@elements/elements/drawer';
-import type { Logo } from '@elements/elements/logo';
-import type { Menu, MenuItem } from '@elements/elements/menu';
+import type { AppHeader } from '@nvidia-elements/core/app-header';
+import type { Breadcrumb } from '@nvidia-elements/core/breadcrumb';
+import type { Button } from '@nvidia-elements/core/button';
+import type { IconButton } from '@nvidia-elements/core/icon-button';
+import type { Icon } from '@nvidia-elements/core/icon';
+import type { Drawer, DrawerContent, DrawerFooter, DrawerHeader } from '@nvidia-elements/core/drawer';
+import type { Logo } from '@nvidia-elements/core/logo';
+import type { Menu, MenuItem } from '@nvidia-elements/core/menu';
 
 type CustomEvents<K extends string> = { [key in K]: (event: CustomEvent) => void };
 type CustomElement<T, K extends string = ''> = Partial<T & { children: any } & CustomEvents<`on${K}`>>;
