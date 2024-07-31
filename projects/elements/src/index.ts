26d4c2218f0402a39aa370b2d59befb29cbb32e2
Cory Rylan <crylan@nvidia.com>
2024-07-31 08:36:31 -0600

    feat(core): stable release
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/index.ts
+++ projects/elements/src/index.ts
@@ -1,5 +1,5 @@
 /// <reference types="./declarations" />
 export const VERSION = '0.0.0';
-export const SCOPE = '@elements';
+export const SCOPE = '@nve';
 export { I18nService, getThemeTokens } from '@nvidia-elements/core/internal';
 export * from './internal/types/index.js';
