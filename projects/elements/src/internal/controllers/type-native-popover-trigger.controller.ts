e9fe369bab9eb9e4f4239101272148e1eefc874e
Cory Rylan <crylan@nvidia.com>
2024-07-31 11:31:36 -0600

    feat(core): support popovertarget for buttons
    - adds support for native popover target triggers for button types
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/elements/src/internal/controllers/type-native-popover-trigger.controller.ts
@@ -0,0 +1,37 @@
+import { ReactiveElement, ReactiveController } from 'lit';
+import { getFlattenedDOMTree } from '../utils/dom';
+
+export interface NativePopoverTrigger extends ReactiveElement {
+  disabled: boolean;
+  popoverTargetAction: 'show' | 'hide' | 'toggle';
+  popoverTargetElement: HTMLElement;
+  popovertarget: string;
+}
+
+export function typeNativePopoverTrigger<T extends NativePopoverTrigger>(): ClassDecorator {
+  return (target: any) => target.addInitializer((instance: T) => new TypeNativePopoverTriggerController(instance));
+}
+
+export class TypeNativePopoverTriggerController<T extends NativePopoverTrigger> implements ReactiveController {
+  constructor(private host: T) {
+    this.host.addController(this);
+  }
+
+  hostConnected() {
+    this.host.addEventListener('click', () => {
+      const id = this.host.popovertarget?.length ? this.host.popovertarget : this.host.popoverTargetElement?.id;
+
+      if (id && !this.host.disabled) {
+        const popover = getFlattenedDOMTree(this.host.getRootNode()).find(e => e.id === id) as any;
+
+        if (this.host.popoverTargetAction === 'hide') {
+          popover?.hidePopover();
+        } else if (this.host.popoverTargetAction === 'show') {
+          popover?.showPopover();
+        } else {
+          popover?.togglePopover();
+        }
+      }
+    });
+  }
+}
