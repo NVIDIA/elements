2b34f3f86d4dd82ee7803faf7b15429a53483673
Cory Rylan <crylan@nvidia.com>
2024-07-31 08:39:04 -0600

    chore(ci): dependency updates
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/internals/vite/src/setup.js
+++ projects/internals/vite/src/setup.js
@@ -1,6 +1,8 @@
-import styles from '@nvidia-elements/core/index.css?inline';
+import fontInter from '@nvidia-elements/themes/fonts/inter.css?inline';
+import theme from '@nvidia-elements/themes/index.css?inline';
 
+// setup base theme/tokens
 const sheet = new CSSStyleSheet();
-sheet.replaceSync(`${styles}`);
+sheet.replaceSync(`${fontInter}${theme}`);
 document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
 document.documentElement.setAttribute('nve-theme', '');
