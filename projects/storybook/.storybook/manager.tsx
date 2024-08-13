9457b0cc4c9dcc434839de31efb17fd49266596d
Cory Rylan <crylan@nvidia.com>
2024-08-13 12:15:42 -0500

    chore(docs): fix playground url generation
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/storybook/.storybook/manager.tsx
+++ projects/storybook/.storybook/manager.tsx
@@ -82,12 +82,12 @@ const ThemePicker = () => {
           <nve-switch-group>
             <label>API <nve-icon-button slot="label" id="api-hint-btn" container="flat" icon-name="information-circle-stroke" size="sm" style={{'--height': '12px'}}></nve-icon-button></label>
             <nve-switch>
-              <label>Maglev Scope</label>
-              <input type="checkbox" value="scope" defaultChecked={globals.scope === 'mlv'} onChange={e => updateGlobals({ scope: e.target.checked ? 'mlv' : 'nve' })} />
-            </nve-switch>
-            <nve-switch>
               <label>React</label>
               <input type="checkbox" value="sourceType" defaultChecked={globals.sourceType === 'react'} onChange={e => updateGlobals({ sourceType: e.target.checked ? 'react' : 'html' })} />
+            </nve-switch>
+            <nve-switch>
+              <label>Maglev Scope (pre v1.0)</label>
+              <input type="checkbox" value="scope" defaultChecked={globals.scope === 'mlv'} onChange={e => updateGlobals({ scope: e.target.checked ? 'mlv' : 'nve' })} />
             </nve-switch>
           </nve-switch-group>
         </nve-drawer-content>
