e3ade4b30f4995ff19d3b8e7dd70f10dbd5c553f
Dhruvi Shah <dhruvis@nvidia.com>
2024-08-14 15:49:01 -0700

    feat(core): copy-button

---
+++ projects/elements/src/copy-button/copy-button.stories.ts
@@ -0,0 +1,48 @@
+import { html } from 'lit';
+import '@nvidia-elements/core/copy-button/define.js';
+
+
+export default {
+ title: 'Elements/Copy Button/Examples',
+ component: 'nve-copy-button',
+};
+
+
+export const Default = { 
+ render: () => html`
+   <nve-copy-button value="hello" behavior-copy></nve-copy-button>
+ `
+}
+
+export const Interaction = {
+    render: () => html`
+      <nve-copy-button value="hello"></nve-copy-button>
+      <nve-copy-button disabled value="hello"></nve-copy-button>
+    `
+   }
+
+export const Flat = {
+ render: () => html`
+   <nve-copy-button container="flat" value="hello"></nve-copy-button>
+   <nve-copy-button container="flat" disabled value="hello"></nve-copy-button>
+ `
+}
+
+export const BehaviorCopy = { 
+    render: () => html`
+      <nve-copy-button value="hello" behavior-copy></nve-copy-button>
+    `
+   }
+
+export const Size = {
+    render: () => html`
+      <nve-copy-button size="sm"></nve-copy-button>
+      <nve-copy-button></nve-copy-button>
+      <nve-copy-button size="lg"></nve-copy-button>
+    `
+  }
+  export const Hint = {
+    render: () => html`
+      <nve-copy-button container="flat" value="2d628479cf2db27cbdebbfe41a42f1c9e07c46a8" aria-label="2d628479cf2db27cbdebbfe41a42f1c9e07c46a8" behavior-copy>2d628479c...</nve-copy-button>
+    `
+  }
