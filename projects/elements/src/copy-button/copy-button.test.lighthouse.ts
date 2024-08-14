4515a389b2320061d70c94adc201428591de86c7
Dhruvi Shah <dhruvis@nvidia.com>
2024-08-14 15:49:01 -0700

    feat(elements): copy-button

---
+++ projects/elements/src/copy-button/copy-button.test.lighthouse.ts
@@ -0,0 +1,18 @@
+import { expect, test, describe } from 'vitest';
+import { lighthouseRunner } from '@nve-internals/vite';
+
+describe('copy-button lighthouse report', () => {
+  test('copy-button should meet lighthouse benchmarks', async () => {
+    const report = await lighthouseRunner.getReport('nve-copy-button', /* html */`
+      <nve-copy-button value="copy button"></nve-copy-button>
+      <script type="module">
+        import '@nvidia-elements/core/copy-button/define.js';
+      </script>
+    `);
+
+    expect(report.scores.performance).toBe(100);
+    expect(report.scores.accessibility).toBe(100);
+    expect(report.scores.bestPractices).toBe(100);
+    expect(report.payload.javascript.kb).toBeLessThan(33.1);
+  });
+});
