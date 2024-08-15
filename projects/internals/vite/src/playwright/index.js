535db6d2f988d6f6e32254239d216bba982c6516
Cory Rylan <crylan@nvidia.com>
2024-08-15 15:07:12 -0500

    chore(ci): improve dependency resolution and playwright test locking
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/internals/vite/src/playwright/index.js
@@ -0,0 +1,43 @@
+import { exec } from 'node:child_process';
+import { resolve } from 'path';
+import * as lockFile from 'proper-lockfile';
+
+/**
+ * creates a lockfile to force parrallel playright runners to execute one at a time, example:
+ *
+ * "test": "playwright-lock 'vite run'"
+ *
+ * This is needed due to the issue of wireit running tasks in parallel based on
+ * dependencies but playwright not allowing more than one chromium instance
+ * executing at a time see https://github.com/google/wireit/issues/325
+ */
+
+const cmd = process.argv[2];
+const path = resolve(import.meta.dirname, './index.js');
+
+const timerId = setInterval(() => {
+  lockFile
+    .lock(path)
+    .then(async release => {
+      clearInterval(timerId);
+      await new Promise(r => {
+        exec(cmd, (error, stdout, stderr) => {
+          console.log(stdout);
+
+          if (error) {
+            console.log(1);
+            console.error(error);
+            process.exit(1);
+          }
+
+          if (stderr) {
+            console.error(stderr);
+          }
+
+          r();
+        });
+      });
+      return release();
+    })
+    .catch(_ => {});
+}, 1000);
