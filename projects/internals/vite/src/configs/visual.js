2f1527e53327cc24923b77e623259b701194e18b
Cory Rylan <crylan@nvidia.com>
2024-08-15 15:08:04 -0500

    chore(ci): improve visual test runner performance
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/internals/vite/src/configs/visual.js
+++ projects/internals/vite/src/configs/visual.js
@@ -1,8 +1,4 @@
-/**
- * - https://vitejs.dev/config/
- * - https://lit.dev/docs/tools/production/
- * @type {import('vite').UserConfig}
- */
+/** @type {import('vite').UserConfig} */
 export const libraryVisualTestConfig = {
   logLevel: 'info',
   test: {
@@ -11,17 +7,16 @@ export const libraryVisualTestConfig = {
     bail: 2,
     isolate: false,
     fileParallelism: false,
-    onConsoleLog(log) {
-      if (log.includes('plugin vite-plugin-virtual-html')) {
-        return false;
-      }
-    },
     reporters: ['basic', 'junit'],
-    globalSetup: ['@internals/vite/setup/visual.js'],
     testTimeout: 60000,
     server: {
       deps: {
         external: ['**/node_modules/**']
+      }
+    },
+    onConsoleLog(log) {
+      if (log.includes('plugin vite-plugin-virtual-html')) {
+        return false;
       }
     }
   }
