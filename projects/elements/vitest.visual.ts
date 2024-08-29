793900cafc12b5224d3bc8aea6eadec0bdcd5e5a
Cory Rylan <crylan@nvidia.com>
2024-08-29 16:20:56 -0500

    fix(core): checkbox indeterminate
    - fix indeterminate style state selector
    - add indeterminate story
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/vitest.visual.ts
+++ projects/elements/vitest.visual.ts
@@ -6,7 +6,6 @@ export default mergeConfig(libraryVisualTestConfig, {
     include: [
       'src/**/*.test.visual.ts',
       '!src/internal/styles/*.test.visual.ts',
-      '!src/checkbox/*.test.visual.ts',
       '!src/switch/*.test.visual.ts',
       '!src/progress-bar/*.test.visual.ts'
     ],
