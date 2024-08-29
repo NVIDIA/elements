ddf02c3ce8b4ca31ca8ff32121ecb3046bbb3f6e
Cory Rylan <crylan@nvidia.com>
2024-08-29 16:22:30 -0500

    fix(elements): missing tree css prop
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/tree/tree-node.ts
+++ projects/elements/src/tree/tree-node.ts
@@ -23,7 +23,10 @@ import { updateNodeSelection } from './utils.js';
  * @event open - Dispatched when the node is opened.
  * @event close - Dispatched when the node is closed.
  * @slot - node content and tree nodes
- * @cssprop --gap
+ * @cssprop --color
+ * @cssprop --border-radius
+ * @cssprop --font-size
+ * @cssprop --min-height
  * @storybook https://NVIDIA.github.io/elements/api/?path=/docs/elements-tree-documentation--docs
  * @figma https://www.figma.com/design/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog?node-id=30-30&t=TiwKVvP1YHl3NylZ-0
  * @aria https://www.w3.org/WAI/ARIA/apg/patterns/treeview/
