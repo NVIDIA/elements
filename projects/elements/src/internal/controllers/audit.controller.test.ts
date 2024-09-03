45a9d7664ce206ff9a0d4a6b9ca25eb2727cd58c
Cory Rylan <crylan@nvidia.com>
2024-09-03 10:27:40 -0500

    fix(core): audit controller
    - fix audit controller default state when in production
    - fix missing grid slot audit for grid footer
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/internal/controllers/audit.controller.test.ts
+++ projects/elements/src/internal/controllers/audit.controller.test.ts
@@ -19,33 +19,69 @@ class AuditTestElement extends LitElement {
 }
 
 describe('audit.controller instance tracking', () => {
-  let element: AuditTestElement;
   let fixture: HTMLElement;
   let originalWarn = console.warn;
 
   beforeEach(async () => {
+    globalThis.NVE_ELEMENTS.state.env = 'development';
     console.warn = () => null;
     vi.spyOn(console, 'warn');
-
-    fixture = await createFixture(html`
-      <audit-test-element></audit-test-element>
-      <audit-test-element></audit-test-element>
-      <audit-test-element></audit-test-element>`);
-    element = fixture.querySelector<AuditTestElement>('audit-test-element');
-    await elementIsStable(element);
+    fixture = await createFixture(html``);
   });
 
   afterEach(() => {
+    globalThis.NVE_ELEMENTS.state.env = 'development';
     removeFixture(fixture);
     console.warn = originalWarn;
   });
 
-  it('should track audit instance count and warn if exceeded', () => {
+  it('should not have audit registry by default', () => {
     expect(GlobalStateService.state.env).toBe('development');
+    expect(GlobalStateService.state.audit['audit-test-element']).toBe(undefined);
+  });
+
+  it('should NOT create audit registry when element created in a production env', async () => {
+    globalThis.NVE_ELEMENTS.state.env = 'production';
+    expect(GlobalStateService.state.env).toBe('production');
+    const elementOne = document.createElement('audit-test-element');
+    fixture.appendChild(elementOne);
+    await elementIsStable(elementOne);
+    expect(GlobalStateService.state.audit['audit-test-element']).toBe(undefined);
+  });
+
+  it('should create audit registry when element created', async () => {
+    document.createElement('audit-test-element');
+    expect(GlobalStateService.state.audit['audit-test-element'].count).toBe(0);
+  });
+
+  it('should increment count when element added to DOM', async () => {
+    const elementOne = document.createElement('audit-test-element');
+    fixture.appendChild(elementOne);
+    await elementIsStable(elementOne);
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(GlobalStateService.state.audit['audit-test-element'].count).toBe(1);
+  });
+
+  it('should track audit instance count and warn if exceeded', async () => {
+    const elementOne = document.createElement('audit-test-element');
+    const elementTwo = document.createElement('audit-test-element');
+    const elementThree = document.createElement('audit-test-element');
+    fixture.appendChild(elementOne);
+    fixture.appendChild(elementTwo);
+    fixture.appendChild(elementThree);
+    await elementIsStable(elementTwo);
+    await elementIsStable(elementThree);
+
     expect(GlobalStateService.state.audit['audit-test-element'].count).toBe(3);
     expect(console.warn).toHaveBeenCalledWith(
       '@nve: Excessive rendering of 2 audit-test-element were detected in DOM. Recycle/reuse elements when possible to improve application performance.'
     );
+
+    vi.clearAllMocks();
+    elementTwo.remove();
+    elementThree.remove();
+    expect(GlobalStateService.state.audit['audit-test-element'].count).toBe(1);
+    expect(console.warn).not.toHaveBeenCalled();
   });
 });
 
