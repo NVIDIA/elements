f224fc4f91f5cca5d18eb8559c44a303fb515013
Cory Rylan <crylan@nvidia.com>
2024-10-03 12:50:42 -0500

    chore(starters): add additional static ssr examples
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/starters/nextjs/src/pages/_document.tsx
+++ projects/starters/nextjs/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en" nve-theme="dark">
+    <Html lang="en" nve-theme="">
       <Head />
       <body>
         <Main />
