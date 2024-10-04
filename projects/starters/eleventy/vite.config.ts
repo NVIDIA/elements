ad74d0d59fc4abd20f89f5d2476ea3cc75d2dda7
Cory Rylan <crylan@nvidia.com>
2024-10-04 09:09:06 -0500

    chore(starters): added eleventy starter
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/starters/eleventy/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig(() => {
+  return {
+    root: './_site',
+    base: '/elements/starters/eleventy/',
+    build: {
+      outDir: '../dist',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, './_site/index.html'),
+          about: resolve(__dirname, './_site/about/index.html'),
+          settings: resolve(__dirname, './_site/settings/index.html')
+        }
+      }
+    }
+  };
+});
