d00aa66232af900775592b85fa4c5ccabd089b2a
Cory Rylan <crylan@nvidia.com>
2024-10-14 17:35:59 -0500

    chore(core): update tests, metadata
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/internal/utils/supports.test.ts
+++ projects/elements/src/internal/utils/supports.test.ts
@@ -1,8 +1,20 @@
 import { describe, expect, it } from 'vitest';
-import { supportsNativeCSSAnchorPosition } from './supports.js';
+import { supportsCSSLegacyInsetArea, supportsCSSPositionArea, supportsNativeCSSAnchorPosition } from './supports.js';
 
 describe('supportsNativeCSSAnchorPosition', () => {
   it('should determine if CSS Anchor Positioning is supported ', async () => {
     expect(supportsNativeCSSAnchorPosition()).toBe(true);
+  });
+});
+
+describe('supportsCSSPositionArea', () => {
+  it('should determine if CSS Position Area is supported ', async () => {
+    expect(supportsCSSPositionArea()).toBe(true);
+  });
+});
+
+describe('supportsCSSLegacyInsetArea', () => {
+  it('should determine if CSS Legacy Inset Area Positioning is supported ', async () => {
+    expect(supportsCSSLegacyInsetArea()).toBe(true);
   });
 });
