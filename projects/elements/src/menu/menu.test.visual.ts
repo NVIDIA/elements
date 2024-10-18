8afe9aa4d7b6782b329e60131265bcf08287e732
Dhruvi Shah <dhruvis@nvidia.com>
2024-10-18 02:29:32 -0700

    feat(elements): add status='danger' for destructive styling for nve-menu-item

--- projects/elements/src/menu/menu.test.visual.ts
+++ projects/elements/src/menu/menu.test.visual.ts
@@ -15,6 +15,13 @@ describe('menu visual', () => {
 
 function template(theme: '' | 'dark' = '') {
   return /* html */ `
+  <style>
+    body {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      gap: 24px;
+    }
+  </style>
   <script type="module">
     import '@nvidia-elements/core/menu/define.js';
     import '@nvidia-elements/core/icon/define.js';
@@ -61,6 +68,15 @@ function template(theme: '' | 'dark' = '') {
     <nve-menu-item><nve-icon name="gear"></nve-icon> <a href="#">•︎•︎•</a></nve-menu-item>
     <nve-menu-item><nve-icon name="star"></nve-icon> <a href="#">•︎•︎•</a></nve-menu-item>
     <nve-menu-item><nve-icon name="logout"></nve-icon> <a href="#">•︎•︎•</a></nve-menu-item>
+  </nve-menu>
+
+  <nve-menu>
+    <nve-menu-item status="danger">•︎•︎•</nve-menu-item>
+    <nve-menu-item status="danger" disabled>•︎•︎•</nve-menu-item>
+    <nve-menu-item status="danger" selected>•︎•︎•</nve-menu-item>
+    <nve-menu-item status="danger" current="page">•︎•︎•</nve-menu-item>
+    <nve-menu-item status="danger"><nve-icon name="gear"></nve-icon> •︎•︎•</nve-menu-item>
+    <nve-menu-item status="danger">•︎•︎• <nve-icon id="warning-icon" size="md" name="exclamation-triangle" style="margin-left: auto"></nve-icon></nve-menu-item>
   </nve-menu>
   `;
 }
