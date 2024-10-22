259595669ae1f09a07810949dacb47dccb9c059f
Cory Rylan <crylan@nvidia.com>
2024-10-22 08:27:24 -0500

    fix(core): form control message with alert dep
    - Update form control meessage to remove dependency with alert. This removes a chair of deps with alert, alert group and button. Overall reduction of form control bundle sizes by 20%
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/forms/forms.test.lighthouse.ts
+++ projects/elements/src/forms/forms.test.lighthouse.ts
@@ -16,7 +16,7 @@ describe('forms lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(23);
+    expect(report.payload.javascript.kb).toBeLessThan(18);
   });
 
   test('form control message should meet lighthouse benchmarks', async () => {
@@ -58,6 +58,6 @@ describe('forms lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(23);
+    expect(report.payload.javascript.kb).toBeLessThan(17.2);
   });
 });
