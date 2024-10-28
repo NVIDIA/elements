19f9a29c54f6651bb98327b842fa817e29026f3e
Cory Rylan <crylan@nvidia.com>
2024-10-28 15:55:00 -0500

    chore(elements): cleanup internal controller stories
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/elements/src/internal/controllers/i18n.controller.stories.ts
@@ -0,0 +1,69 @@
+import { html, LitElement } from 'lit';
+import { property } from 'lit/decorators/property.js';
+import { I18nController } from '@nvidia-elements/core/internal';
+import { I18nService } from '@nvidia-elements/core';
+import '@nvidia-elements/core/card/define.js';
+import '@nvidia-elements/core/button/define.js';
+
+export default {
+  title: 'Internal/Controllers'
+}
+
+class I18nItem extends LitElement {
+  #i18nController: I18nController<this> = new I18nController<this>(this);
+  @property({ type: Object }) i18n = this.#i18nController.i18n;
+
+  render() {
+    return html`<nve-card><pre style="padding: 24px">${JSON.stringify(this.i18n, null, 2)}</pre></nve-card>`
+  }
+}
+customElements.get('i18n-item') || customElements.define('i18n-item', I18nItem);
+
+class I18nDemo extends LitElement {
+  render() {
+    return html`
+    <div nve-layout="column gap:md">
+      <div nve-layout="row gap:sm">
+        <nve-button @click=${() => this.#english()}>English</nve-button>
+        <nve-button @click=${() => this.#french()}>French</nve-button>
+      </div>
+      
+      <div nve-layout="grid span-items:6 gap:sm">
+        <i18n-item .i18n=${{ "close": "dismiss task failure warning" }}></i18n-item>
+        <i18n-item></i18n-item>
+      </div>
+    </div>
+    `
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+
+  #french() {
+    I18nService.update({
+      close: 'fermer',
+      expand: 'étendre',
+      sort: 'classer',
+      show: 'montrer',
+      hide: 'cacher',
+      loading: 'bourrage'
+    });
+  }
+
+  #english() {
+    I18nService.update({
+      close: 'close',
+      expand: 'expand',
+      sort: 'sort',
+      show: 'show',
+      hide: 'hide',
+      loading: 'loading'
+    });
+  }
+}
+customElements.get('i18n-demo') || customElements.define('i18n-demo', I18nDemo);
+
+export const I18nControllerDemo = {
+  render: () => html`<i18n-demo></i18n-demo>`
+}
---
+++ projects/elements/src/internal/controllers/keynav-grid.controller.stories.ts
@@ -0,0 +1,70 @@
+import { css, html, LitElement } from 'lit';
+import { state } from 'lit/decorators/state.js';
+import { keyNavigationGrid } from '@nvidia-elements/core/internal';
+
+export default {
+  title: 'Internal/Controllers'
+}
+
+const styles = css`
+  section {
+    display: grid;
+    gap: 4px;
+    grid-template-columns: repeat(10, 50px);
+  }
+
+  .vertical {
+    grid-template-rows: repeat(10, 1fr);
+    grid-template-columns: 50px;
+  }
+
+  .row {
+    display: contents;
+  }
+
+  button {
+    width: 100%;
+    height: 30px;
+    min-width: 30px;
+    display: block;
+  }
+
+  button[selected] {
+    background: green;
+    color: white;
+  }
+`;
+
+export function keyNavigationGridControllerDemo() {
+  @keyNavigationGrid<DemoKeyNavigationGridController>()
+  class DemoKeyNavigationGridController extends LitElement {
+    get keynavGridConfig() {
+      return {
+        rows: this.shadowRoot.querySelectorAll<HTMLElement>('.row'),
+        cells: this.shadowRoot.querySelectorAll<HTMLElement>('.cell')
+      }
+    }
+
+    @state() private selected = '0,0';
+    @state() private active = '';
+
+    static styles = [styles];
+
+    render() {
+      return html`
+        <p>Selected: ${this.selected}</p>
+        <p>Active: ${this.active}</p>
+        <section @nve-key-change=${(e: any) => (this.active = e.detail.activeItem.textContent)}>
+          ${Array.from(Array(10).keys()).map(() => Array.from(Array(10).keys())).map((r, ri) => html`<div class="row">
+            ${r.map(c => html`<div class="cell">
+              <button ?selected=${this.selected === `${ri}-${c}`} @click=${(e: any) => (this.selected = e.target.innerText)}>${ri}-${c}</button>
+            </div>`)}
+          </div>`)}
+        </section>
+      `;
+    }
+  }
+
+  customElements.get('demo-key-navigation-grid') || customElements.define('demo-key-navigation-grid', DemoKeyNavigationGridController);
+  return html`<demo-key-navigation-grid></demo-key-navigation-grid>`;
+}
