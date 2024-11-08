e1476f722ada4a3ea2639775ab4c30d41b2110c5
Cormac Rada <crada@nvidia.com>
2024-11-08 15:13:10 -0800

    feat(elements): add icons

--- projects/elements/src/breadcrumb/breadcrumb.test.lighthouse.ts
+++ projects/elements/src/breadcrumb/breadcrumb.test.lighthouse.ts
@@ -17,6 +17,6 @@ describe('breadcrumb lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(15.5);
+    expect(report.payload.javascript.kb).toBeLessThan(15.6);
   });
 });
