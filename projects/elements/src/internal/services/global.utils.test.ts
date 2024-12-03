0048d86aecd01a6ae8ba84d08705f16a4e57a9d9
Cory Rylan <crylan@nvidia.com>
2024-12-03 11:15:26 -0600

    chore(ci): update project dependencies
    - updates vite to latest
    - updates storybook to latest
    - updates angular to latest
    - cleanup workspace catalogs
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/internal/services/global.utils.test.ts
+++ projects/elements/src/internal/services/global.utils.test.ts
@@ -2,11 +2,6 @@ import { describe, expect, it } from 'vitest';
 import { getEnv } from './global.utils.js';
 
 describe('getEnv', () => {
-  it('should return the environment as development if NODE_ENV was set', async () => {
-    globalThis.process.env.NODE_ENV = 'development';
-    expect(getEnv()).toBe('development');
-  });
-
   it('should return the environment as development for localhost', async () => {
     expect(getEnv()).toBe('development');
   });
