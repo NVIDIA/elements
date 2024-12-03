b8253042e7f4d160dce1404b1b4fb0e9233324f0
Cory Rylan <crylan@nvidia.com>
2024-12-03 08:38:15 -0600

    chore(ci): update env vars
    - update env variables to be set within the gitlab pipeline, removes local variable warning on pnpm install
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,10 +1,8 @@
 // Skip Husky install in production and CI
-
-console.log(`Husky env process.env.NODE_ENV ${process.env.NODE_ENV}, process.env.CI: ${process.env.CI}`);
 if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
   console.log('Skipped Husky Install');
   process.exit(0);
 }
 
 const husky = await import('husky');
-console.log(husky.install());
+husky.install();
