8e39038d422c2d064fc0c4361650440be09bb453
Cory Rylan <crylan@nvidia.com>
2024-12-05 10:12:43 -0600

    fix(core): dynamic options selected state
    - fix issue with select options not reflected selected state in the UI
    - fixes underlying issue in both nve-select and nve-combobox
    
    closes #80
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/index.test.lighthouse.ts
+++ projects/elements/src/index.test.lighthouse.ts
@@ -24,7 +24,7 @@ describe('lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.requests[Object.keys(report.payload.javascript.requests)[0]].kb).toBeLessThan(124.5); // @nvidia-elements/core/bundles/index.VERSION.js
+    expect(report.payload.javascript.requests[Object.keys(report.payload.javascript.requests)[0]].kb).toBeLessThan(125); // @nvidia-elements/core/bundles/index.VERSION.js
   });
 
   test('JS imports should remain within compressed bundle limits', async () => {
