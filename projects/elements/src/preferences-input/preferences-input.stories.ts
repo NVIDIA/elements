e8acc64531036c9ba975331db714d0517e0e483b
Alberto Silva <albertos@nvidia.com>
2024-12-09 14:07:26 -0500

    feat(elements): preferences-input component

---
+++ projects/elements/src/preferences-input/preferences-input.stories.ts
@@ -0,0 +1,79 @@
+import { html } from 'lit';
+import '@nvidia-elements/core/button/define.js';
+import '@nvidia-elements/core/card/define.js';
+import '@nvidia-elements/core/card/define.js';
+import '@nvidia-elements/core/dropdown/define.js';
+import '@nvidia-elements/core/logo/define.js';
+import '@nvidia-elements/core/menu/define.js';
+import '@nvidia-elements/core/page-header/define.js';
+import '@nvidia-elements/core/switch/define.js';
+import '@nvidia-elements/core/preferences-input/define.js';
+
+export default {
+  title: 'Elements/Preferences Input/Examples',
+  component: 'nve-preferences-input'
+};
+
+export const Default = {
+  render: () => {
+    return html`
+      <style>
+        :root {
+          --nve-config-light: true;
+          --nve-config-dark: true;
+          --nve-config-high-contrast: true;
+          --nve-config-compact: true;
+          --nve-config-reduced-motion: true;
+        }
+      </style>
+      <div>
+        <nve-preferences-input name="theme" value='{ "theme": "dark", "scale": "compact", "reduced-motion": "true" }'></nve-preferences-input>
+      </div>
+    `
+  }
+};
+
+export const Forms = {
+  render: () => {
+    return html`
+      <form id="preferences-input-form" nve-layout="row gap:md">
+        <div nve-layout="column gap:md">
+          <nve-preferences-input name="theme" value='{ "theme": "light" }'></nve-preferences-input>
+          <nve-button>submit</nve-button>
+        </div>
+        <pre></pre>
+      </form>
+      <script type="module">
+        const form = document.querySelector('form');
+        const pre = document.querySelector('form pre');
+
+        form.addEventListener('change', renderValues);
+        form.addEventListener('input', renderValues);
+        form.addEventListener('submit', renderValues);
+
+        function renderValues(e) {
+          e.preventDefault();
+          console.log(e);
+          pre.innerText = JSON.stringify({
+            ...Object.fromEntries(new FormData(form)), event: e.type
+          }, null, 2);
+        }
+      </script>
+    `
+  }
+}
+
+export const Dropdown = {
+  render: () => {
+    return html`
+      <nve-page-header>
+        <nve-logo slot="prefix" size="sm"></nve-logo>
+        <h2 slot="prefix" nve-text="heading sm">NVIDIA</h2>
+        <nve-icon-button slot="suffix" icon-name="gear" size="sm" popovertarget="preferences-input"></nve-icon-button>
+      </nve-page-header>
+      <nve-dropdown id="preferences-input" position="bottom" alignment="end">
+        <nve-preferences-input name="theme" value='{ "theme": "dark" }'></nve-preferences-input>
+      </nve-dropdown>
+    `
+  }
+}
---
+++ projects/elements/src/preferences-input/preferences-input.test.visual.ts
@@ -0,0 +1,35 @@
+import { expect, test, describe } from 'vitest';
+import { visualRunner } from '@nve-internals/vite';
+
+describe('preferences input visual', () => {
+  test('preferences input should match visual baseline', async () => {
+    const report = await visualRunner.render('preferences-input', template());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('preferences input should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('preferences-input.dark', template('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+});
+
+function template(theme: '' | 'dark' = '') {
+  return /* html */ `
+  <script type="module">
+    import '@nvidia-elements/core/preferences-input/define.js';
+    document.documentElement.setAttribute('nve-theme', '${theme}');
+  </script>
+  <style>
+    :root {
+     --nve-config-light: true;
+     --nve-config-dark: true;
+     --nve-config-high-contrast: true;
+     --nve-config-compact: true;
+     --nve-config-reduced-motion: true;
+    }
+  </style>
+  <div nve-layout="row gap:xs">
+    <nve-preferences-input></nve-preferences-input>
+  </div>
+  `;
+}
