b2525bf122d4d1acde2419f6237054f9a852319c
Dhruvi Shah <dhruvis@nvidia.com>
2024-12-12 16:33:41 -0800

    feat(core): avatar component

---
+++ projects/elements/src/avatar/avatar.test.axe.ts
@@ -0,0 +1,50 @@
+import { html } from 'lit';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { createFixture, elementIsStable, removeFixture } from '@nvidia-elements/testing';
+import { runAxe } from '@nvidia-elements/testing/axe';
+import { Avatar } from '@nvidia-elements/core/avatar';
+import '@nvidia-elements/core/avatar/define.js';
+
+describe(Avatar.metadata.tag, () => {
+  let fixture: HTMLElement;
+  let element: Avatar;
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-avatar>AV</nve-avatar>
+      <nve-avatar color="red-cardinal">AV</nve-avatar>
+      <nve-avatar color="gray-slate">AV</nve-avatar>
+      <nve-avatar color="gray-denim">AV</nve-avatar>
+      <nve-avatar color="blue-indigo">AV</nve-avatar>
+      <nve-avatar color="blue-cobalt">AV</nve-avatar>
+      <nve-avatar color="blue-sky">AV</nve-avatar>
+      <nve-avatar color="teal-cyan">AV</nve-avatar>
+      <nve-avatar color="green-mint">AV</nve-avatar>
+      <nve-avatar color="teal-seafoam">AV</nve-avatar>
+      <nve-avatar color="green-grass">AV</nve-avatar>
+      <nve-avatar color="yellow-amber">AV</nve-avatar>
+      <nve-avatar color="orange-pumpkin">AV</nve-avatar>
+      <nve-avatar color="red-tomato">AV</nve-avatar>
+      <nve-avatar color="pink-magenta">AV</nve-avatar>
+      <nve-avatar color="purple-plum">AV</nve-avatar>
+      <nve-avatar color="purple-violet">AV</nve-avatar>
+      <nve-avatar color="purple-lavender">AV</nve-avatar>
+      <nve-avatar color="pink-rose">AV</nve-avatar>
+      <nve-avatar color="green-jade">AV</nve-avatar>
+      <nve-avatar color="lime-pear">AV</nve-avatar>
+      <nve-avatar color="yellow-nova">AV</nve-avatar>
+      <nve-avatar color="brand-green">AV</nve-avatar>
+    `);
+    element = fixture.querySelector(Avatar.metadata.tag);
+    await elementIsStable(element);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should pass axe check', async () => {
+    const results = await runAxe([Avatar.metadata.tag]);
+    expect(results.violations.length).toBe(0);
+  });
+});
---
+++ projects/elements/src/avatar/avatar.ts
@@ -0,0 +1,43 @@
+import { LitElement, html } from 'lit';
+import { property } from 'lit/decorators/property.js';
+import type { Size, Color } from '@nvidia-elements/core/internal';
+import { colorStateStyles } from '@nvidia-elements/core/internal';
+import { useStyles } from '@nvidia-elements/core/internal';
+import styles from './avatar.css?inline';
+
+/**
+ * @element nve-avatar
+ * @description Avatar represents a user/bot within a UI. Typically with text or image content.
+ * @since 1.18.0
+ * @slot - default slot for content
+ * @cssprop --background
+ * @cssprop --width
+ * @cssprop --height
+ * @cssprop --color
+ * @storybook https://NVIDIA.github.io/elements/api/?path=/docs/elements-avatar-documentation--docs
+ * @figma https://www.figma.com/design/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog?node-id=29-13&t=lPkYCkCgnoVaRSbI-0
+ * @aria https://www.w3.org/WAI/ARIA/apg/patterns/alert/
+ * @stable false
+ */
+export class Avatar extends LitElement {
+  static styles = useStyles([styles, colorStateStyles]);
+
+  static readonly metadata = {
+    tag: 'nve-avatar',
+    version: '0.0.0'
+  };
+
+  /** Sets size of the avatar component. Size can also be updated using the `width + height` css props. */
+  @property({ type: String, reflect: true }) size?: Size | 'xs';
+
+  /** Sets the color of the avatar component. */
+  @property({ type: String, reflect: true }) color: Color;
+
+  render() {
+    return html`
+    <div internal-host>
+        <slot></slot>
+    </div>
+  `;
+  }
+}
