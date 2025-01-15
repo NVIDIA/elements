87d694161745bcade634b91ba9fb6f6cfdafae66
Cory Rylan <crylan@nvidia.com>
2025-01-15 14:20:09 -0600

    chore(ci): update deps
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,8 +1,9 @@
+import husky from 'husky';
+
 // Skip Husky install in production and CI
 if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
   console.log('Skipped Husky Install');
   process.exit(0);
+} else {
+  husky();
 }
-
-const husky = await import('husky');
-husky.install();
