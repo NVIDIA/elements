5810657fc44f5ce21c756310249c0d7c65f9fd82
Cory Rylan <crylan@nvidia.com>
2025-01-22 13:27:01 -0600

    chore(docs): refactor sb system preferences
    - refactor SB system preferences drawer to elements-api package
    - share system settings with site and sb
    - cleanup base layout
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/internals/elements-api/src/system-settings/system-settings.ts
@@ -0,0 +1,106 @@
+import { html, LitElement } from 'lit';
+import { useStyles } from '@nvidia-elements/core/internal';
+import styles from './system-settings.css?inline';
+
+export class SystemSettings extends LitElement {
+  get #globals() {
+    return {
+      theme: 'dark',
+      font: '',
+      scale: '',
+      debug: '',
+      animation: '',
+      sourceType: 'html',
+      ...(JSON.parse(localStorage.getItem('elements-sb-globals')) ?? {})
+    };
+  }
+
+  set #globals(globals: Record<string, string>) {
+    try {
+      localStorage.setItem('elements-sb-globals', JSON.stringify({ ...this.#globals, ...globals }));
+    } catch (error) {
+      console.error('Could not store globals in local storage:', error);
+    }
+  }
+
+  static metadata = {
+    tag: 'nve-api-system-settings',
+    version: '0.0.0'
+  };
+
+  static styles = useStyles([styles]);
+
+  render() {
+    return html`
+      <form internal-host>
+        <nve-select style="--background: transparent; --min-width: 170px">
+          <label>Theme</label>
+          <select size="6" @change=${e => this.#writeGlobals({ theme: e.target.value })}>
+            <option ?selected=${this.#globals.theme === 'light'} value="light">Light</option>
+            <option ?selected=${this.#globals.theme === 'dark'} value="dark">Dark</option>
+            <option ?selected=${this.#globals.theme === 'high-contrast'} value="high-contrast">High Contrast</option>
+            <option ?selected=${this.#globals.theme === ''} value="ddb-dark">DDB Dark (experimental)</option>
+            <option ?selected=${this.#globals.theme === 'brand'} value="brand">Brand (experimental)</option>
+            <option ?selected=${this.#globals.theme === 'brand-dark'} value="brand-dark">Brand Dark (experimental)</option>
+          </select>
+        </nve-select>
+        <nve-select style="--background: transparent; --min-width: 170px">
+          <label>Data</label>
+          <nve-icon-button slot="label" popovertarget="demo-data-tooltip" size="sm" container="flat" icon-name="information-circle-stroke" style="--height: 12px"></nve-icon-button>
+          <select size="3" @change=${e => this.#writeGlobals({ dataTheme: e.target.value })}>
+            <option ?selected=${this.#globals.dataTheme === 'models'} value="models">AI/ML</option>
+            <option ?selected=${this.#globals.dataTheme === ''} value="">Infra</option>
+            <option ?selected=${this.#globals.dataTheme === 'hardware'} value="hardware">Hardware</option>
+          </select>
+        </nve-select>
+        <nve-select style="--background: transparent; --min-width: 170px">
+          <label>Font</label>
+          <select size="3" @change=${e => this.#writeGlobals({ font: e.target.value })}>
+            <option ?selected=${this.#globals.font === ''} value="">Default</option>
+            <option ?selected=${this.#globals.font === 'inter'} value="inter">Inter</option>
+            <option ?selected=${this.#globals.font === 'nvidia-sans'} value="nvidia-sans">NVIDIA Sans</option>
+          </select>
+        </nve-select>
+        <nve-switch-group>
+          <label>Options ${this.#globals.scale}</label>
+          <nve-switch>
+            <label>Compact</label>
+            <input type="checkbox" value="compact" .checked=${this.#globals.scale === 'compact'} @change=${e => this.#writeGlobals({ scale: e.target.checked ? 'compact' : '' })} />
+          </nve-switch>
+          <nve-switch>
+            <label>Reduced Motion</label>
+            <input type="checkbox" value="reduced-motion" .checked=${this.#globals.animation === 'reduced-motion'} @change=${e => this.#writeGlobals({ animation: e.target.checked ? 'reduced-motion' : '' })} />
+          </nve-switch>
+          <nve-switch>
+            <label>Debug</label>
+            <input type="checkbox" value="debug" .checked=${this.#globals.debug === 'debug'} @change=${e => this.#writeGlobals({ debug: e.target.checked ? 'debug' : '' })} />
+          </nve-switch>
+        </nve-switch-group>
+      </form>
+      <nve-tooltip id="demo-data-tooltip">Demo data to be displayed in examples (see datagrid)</nve-tooltip>
+    `;
+  }
+
+  #writeGlobals(update: Record<string, string>) {
+    const globals = { ...this.#globals, ...update };
+    const themes = [
+      globals.theme,
+      globals.font,
+      globals.scale,
+      globals.debug,
+      globals.animation,
+      globals.experimental,
+      globals.systemOptions
+    ]
+      .filter(i => i !== '')
+      .join(' ')
+      .trim();
+    this.requestUpdate();
+
+    this.#globals = globals;
+    globalThis.document.documentElement.setAttribute('nve-theme', themes);
+    (globalThis.document.querySelector('#storybook-preview-iframe') as any)?.contentDocument
+      ?.querySelector('html')
+      ?.setAttribute('nve-theme', themes);
+  }
+}
--- projects/storybook/.storybook/manager.tsx
+++ projects/storybook/.storybook/manager.tsx
@@ -13,9 +13,10 @@ import '@nvidia-elements/core/tooltip/define.js';
 import '@nvidia-elements/core/icon-button/define.js';
 import '@nvidia-elements/core/page-header/define.js';
 import '@nvidia-elements/core/logo/define.js';
+import '@internals/elements-api/system-settings/define.js';
 
 function updateTheme(themes) {
-  const previewIframe = document.querySelector('#storybook-preview-iframe');
+  const previewIframe = document.querySelector('#storybook-preview-iframe') as any;
   const manager = document.querySelector('html') as HTMLElement;
   manager.setAttribute('nve-theme', themes);
   manager.setAttribute('nve-transition', 'auto');
@@ -26,18 +27,9 @@ function updateTheme(themes) {
 }
 
 const PageHeader = () => {
-  const [globals, updateGlobals] = useGlobals();
+  const [globals] = useGlobals();
   const themes = [globals.theme, globals.font, globals.scale, globals.debug, globals.animation, globals.experimental, globals.systemOptions].filter(i => i !== '').join(' ').trim();
   updateTheme(themes);
-
-  const writeGlobals = (update) => {
-    updateGlobals({ ...globals, ...update });
-    try {
-      localStorage.setItem('elements-sb-globals', JSON.stringify({ ...globals, ...update }));
-    } catch (error) {
-      console.error('Could not store globals in local storage:', error);
-    }
-  }
 
   return (
     globals.theme || globals.theme === '' ?
@@ -53,61 +45,9 @@ const PageHeader = () => {
       </nve-page-header>
       <nve-drawer id="system-options-drawer" position="right" size="sm" closable style={{'--top': '47px', '--box-shadow': '0'}}>
         <nve-drawer-content style={{'height': 'initial', 'flex': 'initial'}}>
-          <nve-select style={{'--background': 'transparent', '--min-width': '180px'}}>
-            <label>Theme</label>
-            <select size={6} defaultValue={globals.theme} onChange={e => writeGlobals({ theme: e.target.value })}>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-              <option value="high-contrast">High Contrast</option>
-              <option value="ddb-dark">DDB Dark (experimental)</option>
-              <option value="brand">Brand (experimental)</option>
-              <option value="brand-dark">Brand Dark (experimental)</option>
-            </select>
-          </nve-select>
-        </nve-drawer-content>
-        <nve-divider style={{'width': '100%'}}></nve-divider>
-        <nve-drawer-content style={{'height': 'initial', 'flex': 'initial'}}>
-          <nve-select style={{'--background': 'transparent', '--min-width': '170px'}}>
-            <label>Data</label>
-            <nve-icon-button slot="label" popovertarget="demo-data-tooltip" size="sm" container="flat" icon-name="information-circle-stroke" style={{'--height': '12px'}}></nve-icon-button>
-            <select size={3} defaultValue={globals.dataTheme} onChange={e => writeGlobals({ dataTheme: e.target.value })}>
-              <option value="models">AI/ML</option>
-              <option value="">Infra</option>
-              <option value="hardware">Hardware</option>
-            </select>
-          </nve-select>
-        </nve-drawer-content>
-        <nve-divider style={{'width': '100%'}}></nve-divider>
-        <nve-drawer-content style={{'height': 'initial', 'flex': 'initial'}}>
-          <nve-select style={{'--background': 'transparent', '--min-width': '180px'}}>
-            <label>Font</label>
-            <select size={3} defaultValue={globals.font} onChange={e => writeGlobals({ font: e.target.value })}>
-              <option value="">Default</option>
-              <option value="inter">Inter</option>
-              <option value="nvidia-sans">NVIDIA Sans</option>
-            </select>
-          </nve-select>
-        </nve-drawer-content>
-        <nve-divider style={{'width': '100%'}}></nve-divider>
-        <nve-drawer-content style={{'height': 'initial', 'flex': 'initial'}}>
-          <nve-switch-group>
-            <label>Options</label>
-            <nve-switch>
-              <label>Compact</label>
-              <input type="checkbox" value="compact" defaultChecked={globals.scale === 'compact'} onChange={e => writeGlobals({ scale: e.target.checked ? 'compact' : ''})} />
-            </nve-switch>
-            <nve-switch>
-              <label>Reduced Motion</label>
-              <input type="checkbox" value="reduced-motion" defaultChecked={globals.animation === 'reduced-motion'} onChange={e => writeGlobals({ animation: e.target.checked ? 'reduced-motion' : '' })} />
-            </nve-switch>
-            <nve-switch>
-              <label>Debug</label>
-              <input type="checkbox" value="debug" defaultChecked={globals.debug === 'debug'} onChange={e => writeGlobals({ debug: e.target.checked ? 'debug' : '' })} />
-            </nve-switch>
-          </nve-switch-group>
+          <nve-api-system-settings></nve-api-system-settings>
         </nve-drawer-content>
       </nve-drawer>
-      <nve-tooltip id="demo-data-tooltip">Demo data to be displayed in examples (see datagrid)</nve-tooltip>
     </> : ''
   );
 }
@@ -146,6 +86,7 @@ declare global {
       ['nve-switch-group']: any;
       ['nve-theme']: any;
       ['nve-tooltip']: any;
+      ['nve-api-system-settings']: any;
     }
   }
 }
