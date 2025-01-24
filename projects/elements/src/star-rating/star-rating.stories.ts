a2a78ed3fbcf62dc1f19a9c323f4a38090a8cfd7
Dhruvi Shah <dhruvis@nvidia.com>
2025-01-24 14:43:20 -0800

    feat(core): star rating component

---
+++ projects/elements/src/star-rating/star-rating.stories.ts
@@ -0,0 +1,40 @@
+import { html } from 'lit';
+import '@nvidia-elements/core/star-rating/define.js';
+import '@nvidia-elements/core/forms/define.js';
+
+export default {
+  title: 'Elements/Star Rating/Examples',
+  component: 'nve-star-rating'
+};
+
+export const Default = {
+  render: () => html`
+  <nve-star-rating>
+    <label>rate this input</label>
+    <input id="range" type="range" max="5" value="3" min="0" />
+    <nve-control-message>message</nve-control-message>
+  </nve-star-rating>`
+};
+
+export const Disabled = {
+  render: () => html`
+  <nve-star-rating>
+    <label>disabled</label>
+    <input type="range" max="5" value="3" min="0" disabled />
+    <nve-control-message>message</nve-control-message>
+  </nve-star-rating>`
+};
+
+export const Toggle = {
+  render: () => html`
+  <nve-star-rating>
+    <label>toggle/favorite</label>
+    <input type="range" max="1" value="0" min="0" />
+  </nve-star-rating>
+  
+  <script type="module">
+      const input = document.querySelector('#range');
+      input.addEventListener('input', () => console.log('input', input.valueAsNumber));
+      input.addEventListener('change', () => console.log('change', input.valueAsNumber));
+    </script>`
+};
---
+++ projects/elements/src/star-rating/star-rating.test.axe.ts
@@ -0,0 +1,32 @@
+import { html } from 'lit';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { createFixture, removeFixture, elementIsStable } from '@nvidia-elements/testing';
+import { runAxe } from '@nvidia-elements/testing/axe';
+import { StarRating } from '@nvidia-elements/core/star-rating';
+import '@nvidia-elements/core/star-rating/define.js';
+
+describe(StarRating.metadata.tag, () => {
+  let fixture: HTMLElement;
+  let element: StarRating;
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-star-rating>
+        <label>label</label>
+        <input type="range" />
+        <nve-control-message>message</nve-control-message>
+      </nve-star-rating>
+    `);
+    element = fixture.querySelector(StarRating.metadata.tag);
+    await elementIsStable(element);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should pass axe check', async () => {
+    const results = await runAxe([StarRating.metadata.tag]);
+    expect(results.violations.length).toBe(0);
+  });
+});
