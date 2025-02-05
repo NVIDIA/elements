ed628041c8c2dd0e1b0fd1ecbe1c7fa1265b77b4
Dhruvi Shah <dhruvis@nvidia.com>
2025-02-05 15:49:28 -0800

    feat(elements): chat message component

---
+++ projects/elements/src/chat-message/chat-message.ts
@@ -0,0 +1,61 @@
+import { LitElement, html } from 'lit';
+import { property } from 'lit/decorators/property.js';
+import type { Color } from '@nvidia-elements/core/internal';
+import { colorStateStyles, appendRootNodeStyle } from '@nvidia-elements/core/internal';
+import { useStyles } from '@nvidia-elements/core/internal';
+import styles from './chat-message.css?inline';
+import globalStyles from './chat-message.global.css?inline';
+
+/**
+ * @element nve-chat-message
+ * @description A chat message component displays a text message within a conversation, sent between users or bots
+ * @since 1.25.0
+ * @slot - default slot for content
+ * @slot prefix - for avatar/img content
+ * @slot suffix - for avatar/img content
+ * @cssprop --background
+ * @cssprop --color
+ * @cssprop --padding
+ * @cssprop --gap
+ * @cssprop --border-radius
+ * @cssprop --font-size
+ * @cssprop --line-height
+ * @storybook https://NVIDIA.github.io/elements/api/?path=/docs/elements-chat-message-documentation--docs
+ * @figma https://www.figma.com/design/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog?node-id=1915-50748&p=f&t=CwggTnjcYTJTshz0-0
+ * @aria https://www.w3.org/WAI/ARIA/apg/patterns/alert/
+ * @stable false
+ */
+
+export class ChatMessage extends LitElement {
+  static styles = useStyles([styles, colorStateStyles]);
+
+  static readonly metadata = {
+    tag: 'nve-chat-message',
+    version: '0.0.0'
+  };
+
+  @property({ type: String, reflect: true }) container: 'flat';
+
+  @property({ type: String, reflect: true }) color: Color;
+
+  @property({ type: String, reflect: true, attribute: 'arrow-position' }) arrowPosition:
+    | 'top-start'
+    | 'top-end'
+    | 'bottom-start'
+    | 'bottom-end';
+
+  render() {
+    return html`
+      <div internal-host>
+        <slot name="prefix"></slot>
+        <slot part="_message"></slot>
+        <slot name="suffix"></slot>
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    appendRootNodeStyle(this, globalStyles);
+  }
+}
