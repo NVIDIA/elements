ea78cd09b39b738f973cab31fd222fae14b92cb6
Cory Rylan <crylan@nvidia.com>
2025-02-14 16:05:54 -0600

    chore(docs): site 11ty utils
    - cleanup 11ty utils
    - better isolation for client vs server/11ty code
    - simple lit element example
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/internals/eslint/src/configs/app.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js';
+import { config } from '@weiran.zsd/multi-eslint-rule-config'; // https://github.com/eslint/eslint/issues/19013
+
+const source = ['**/src/**/*.ts', '**/src/**/*.d.ts'];
+const ignores = ['**/node_modules/**', '**/dist/**', '**/coverage/**', '**/.lighthouse/**', '**/.wireit/**'];
+
+export const appConfig = [
+  {
+    files: [...source],
+    ignores: [...ignores],
+    rules: {
+      'no-implicit-globals': ['error'],
+      'no-restricted-globals': [
+        'error',
+        { name: 'window', message: 'Use globalThis instead.' },
+        { name: 'location', message: 'Use globalThis.location instead.' },
+        { name: 'document', message: 'Use globalThis.document instead.' }
+      ]
+    }
+  },
+  {
+    files: [...source],
+    ignores: [...ignores],
+    ...config(
+      [
+        {
+          rule: 'no-restricted-imports',
+          ruleConfig: [
+            'error',
+            {
+              patterns: [
+                {
+                  group: [
+                    '@nvidia-elements/core/**/*.css',
+                    '@nvidia-elements/core/**/*.css?inline',
+                    '@nvidia-elements/styles/**/*.css',
+                    '@nvidia-elements/styles/**/*.css?inline'
+                  ],
+                  message:
+                    'Inline CSS utils are not allowed to prevent performance issues, use shadow DOM encapsulated CSS instead'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      'no-inline-css'
+    )
+  }
+];
