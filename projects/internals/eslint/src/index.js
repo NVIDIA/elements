2150f268070f4f9cef5daa71b85ee7e8d1f9b398
Cory Rylan <crylan@nvidia.com>
2025-02-14 16:05:54 -0600

    chore(docs): site 11ty utils
    - cleanup 11ty utils
    - better isolation for client vs server/11ty code
    - simple lit element example
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/internals/eslint/src/index.js
+++ projects/internals/eslint/src/index.js
@@ -1,3 +1,4 @@
 export { libraryConfig } from './configs/library.js';
+export { appConfig } from './configs/app.js';
 export { litConfig } from './configs/lit.js';
 export { browserTypescriptConfig, nodeTypescriptConfig } from './configs/typescript.js';
