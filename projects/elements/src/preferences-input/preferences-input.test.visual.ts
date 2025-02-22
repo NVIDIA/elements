e8631572c5d45b49a5ebb160ea8af7946f06da3d
Cory Rylan <crylan@nvidia.com>
2025-02-22 00:04:13 -0600

    fix(core): preferences input config name
    - fix missing color-scheme prefix
    - fix broken css prop config flag selectors
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/preferences-input/preferences-input.test.visual.ts
+++ projects/elements/src/preferences-input/preferences-input.test.visual.ts
@@ -21,10 +21,10 @@ function template(theme: '' | 'dark' = '') {
   </script>
   <style>
     :root {
-     --nve-config-light: true;
-     --nve-config-dark: true;
-     --nve-config-high-contrast: true;
-     --nve-config-compact: true;
+     --nve-config-color-scheme-light: true;
+     --nve-config-color-scheme-dark: true;
+     --nve-config-color-scheme-high-contrast: true;
+     --nve-config-scale-compact: true;
      --nve-config-reduced-motion: true;
     }
   </style>
