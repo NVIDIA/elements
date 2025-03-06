b78491fcd5c19f95733d9b0a56cc03b880c44ccd
John Yanarella <jyanarella@nvidia.com>
2025-03-06 12:46:22 -0800

    chore(docs): splash page refinement

--- projects/site/src/index.ts
+++ projects/site/src/index.ts
@@ -9,16 +9,29 @@ import './_internal/glassmorphic-card/glassmorphic-card.js';
 import './_internal/metrics-carousel/metrics-carousel.js';
 import './_internal/svg-trend/svg-trend.js';
 import './_internal/starry-sky/starry-sky.js';
-import './_internal/theme-gen/theme-gen.js';
 
-await Promise.all([
+import './_internal/theme-form/theme-form.js';
+import type { ThemeForm } from './_internal/theme-form/theme-form.js';
+
+import './_internal/theme-preview/theme-preview.js';
+import type { ThemePreview } from './_internal/theme-preview/theme-preview.js';
+
+void Promise.all([
   globalThis.customElements.whenDefined('nvd-framework-selector'),
   globalThis.customElements.whenDefined('nve-codeblock')
 ]).then(() => {
   const selector = globalThis.document.querySelector<FrameworkSelector>('nvd-framework-selector')!;
   const codeblock = globalThis.document.querySelector<CodeBlock>('nve-codeblock')!;
   codeblock.code = `<!-- ${selector.value} -->`;
-  selector.addEventListener('change', _event => {
+  selector.addEventListener('change', () => {
     codeblock.code = `<!-- ${selector.value} -->`;
+  });
+});
+
+void globalThis.customElements.whenDefined('nvd-theme-form').then(() => {
+  const form = globalThis.document.querySelector<ThemeForm>('nvd-theme-form')!;
+  const preview = globalThis.document.querySelector<ThemePreview>('nvd-theme-preview')!;
+  form.addEventListener('input', () => {
+    preview.settings = form.value;
   });
 });
