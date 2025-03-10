668a806d3d0dc04ff28e2c1ee2c68cc147b3e82a
Jared De La Cruz <jdelacruz@nvidia.com>
2025-03-10 16:43:09 -0700

    feat(core): dropzone

---
+++ projects/elements/src/dropzone/dropzone.test.lighthouse.ts
@@ -0,0 +1,18 @@
+import { expect, test, describe } from 'vitest';
+import { lighthouseRunner } from '@nve-internals/vite';
+
+describe('dropzone lighthouse report', () => {
+  test('dropzone should meet lighthouse benchmarks', async () => {
+    const report = await lighthouseRunner.getReport('nve-dropzone', /* html */`
+      <nve-dropzone></nve-dropzone>
+      <script type="module">
+        import '@nvidia-elements/core/dropzone/define.js';
+      </script>
+    `);
+
+    expect(report.scores.performance).toBe(100);
+    expect(report.scores.accessibility).toBe(100);
+    expect(report.scores.bestPractices).toBe(100);
+    expect(report.payload.javascript.kb).toBeLessThan(16.2);
+  });
+});
