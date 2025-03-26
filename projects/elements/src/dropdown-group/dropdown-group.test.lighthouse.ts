8a40bc8a7eca8b9a4bd3df6b5ed239d684e4dac4
Dhruvi Shah <dhruvis@nvidia.com>
2025-03-26 12:10:44 -0700

    feat(elements): dropdown group

---
+++ projects/elements/src/dropdown-group/dropdown-group.test.lighthouse.ts
@@ -0,0 +1,31 @@
+import { expect, test, describe } from 'vitest';
+import { lighthouseRunner } from '@nve-internals/vite';
+
+describe('dropdown-group lighthouse report', () => {
+  test('dropdown-group should meet lighthouse benchmarks', async () => {
+    const report = await lighthouseRunner.getReport('nve-dropdown-group', /* html */`
+      <button popovertarget="menu-1">menu</button>
+      <nve-dropdown-group>
+        <nve-dropdown id="menu-1">
+          <button popovertarget="menu-2">item 1-1</button>
+          <button>item 1-2</button>
+          <button>item 1-3</button>
+        </nve-dropdown>
+        <nve-dropdown id="menu-2" position="right">
+          <button>item 2-1</button>
+          <button>item 2-2</button>
+          <button>item 2-3</button>
+        </nve-dropdown>
+      </nve-dropdown-group>
+      <script type="module">
+        import '@nvidia-elements/core/dropdown-group/define.js';
+        import '@nvidia-elements/core/dropdown/define.js';
+      </script>
+    `);
+
+    expect(report.scores.performance).toBe(100);
+    expect(report.scores.accessibility).toBe(100);
+    expect(report.scores.bestPractices).toBe(100);
+    expect(report.payload.javascript.kb).toBeLessThan(23.3);
+  });
+});
