4a1b8f19c7dd7b14929f9cf12446b70582db272f
Cory Rylan <crylan@nvidia.com>
2025-04-01 08:27:10 -0500

    fix(elements): audit controller prefered element
    - warn ssr related issue to layout with page panel
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/internal/utils/audit.ts
+++ projects/elements/src/internal/utils/audit.ts
@@ -3,9 +3,9 @@ function createDocLink(subpath = '') {
 }
 
 export function auditSlots(
-  host: HTMLElement & { constructor: { metadata?: { children?: string[] } } }
+  host: HTMLElement & { constructor: { metadata?: { children?: string[]; disallowedChildren?: string[] } } }
 ): [Element[], string[]] {
-  const validElements = ['template', ...host.constructor.metadata.children];
+  const validElements = ['template', ...host.constructor.metadata.children].filter(c => typeof c === 'string');
   const invalidElements = Array.from(host.shadowRoot.querySelectorAll('slot')).flatMap(slot =>
     slot.assignedElements().filter(e => !validElements.map(i => i).includes(e.localName))
   );
@@ -19,6 +19,19 @@ export function auditParentElement(
   return [host.parentElement?.localName === validParentTag, validParentTag];
 }
 
+export function auditAlternates(host: HTMLElement, alternates?: { name: string; use: string }[]) {
+  return alternates
+    .map(alternate => {
+      const found = host.querySelector(alternate.name);
+      if (found) {
+        return getUseElementWarning(host.localName, found.localName, alternate.use);
+      } else {
+        return null;
+      }
+    })
+    .filter(i => i?.length);
+}
+
 export function getExcessiveInstanceLimitWarning(count: number, localName: string) {
   return `Excessive rendering of ${count} ${localName} were detected in DOM. Recycle/reuse elements when possible to improve application performance.`;
 }
@@ -29,6 +42,14 @@ export function getInvalidSlotsWarning(localName: string, allowed: Array<string>
 
 export function getInvalidParentWarning(localName: string, allowedParent: string) {
   return `Element ${localName} can only be used as a direct child of ${allowedParent}.`;
+}
+
+export function getInvalidSlottedChildrenWarning(localName: string, disallowed: Array<string>) {
+  return `Invalid slotted children detected in ${localName}. Disallowed: ${disallowed.join(', ')}`;
+}
+
+export function getUseElementWarning(localName: string, found: string, use: string) {
+  return `Element ${found} found in ${localName}${use ? `, use ${use} instead` : ''}.`;
 }
 
 export function getIdMatchNotFoundWarning(id: string) {
