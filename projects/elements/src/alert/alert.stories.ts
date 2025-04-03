3eb0a47cf62b301194b75d86eb1ac2f171cb960d
Will Geller <wgeller@nvidia.com>
2025-04-03 17:22:14 -0400

    chore(docs): move alert stories to single stories file

--- projects/elements/src/alert/alert.stories.ts
+++ projects/elements/src/alert/alert.stories.ts
@@ -1,5 +1,7 @@
 import { html } from 'lit';
 import '@nvidia-elements/core/alert/define.js';
+import '@nvidia-elements/core/button/define.js';
+import '@nvidia-elements/core/divider/define.js';
 
 export default {
   title: 'Elements/Alert/Examples',
@@ -95,5 +97,208 @@ export const TaskStatusDarkTheme = {
   <nve-alert status="failed">Failed</nve-alert>
   <nve-alert status="unknown">Unknown</nve-alert>
 </div>
+  `
+}
+
+export const AlertGroupDefault = {
+  render: () => html`
+  <nve-alert-group>
+    <nve-alert>alert message</nve-alert>
+  </nve-alert-group>
+  `
+};
+
+export const AlertGroupStatus = {
+  render: () => html`
+  <div nve-layout="column gap:md">
+    <nve-alert-group>
+      <nve-alert>Standard</nve-alert>
+      <nve-alert>Standard</nve-alert>
+    </nve-alert-group>
+
+    <nve-alert-group status="accent">
+      <nve-alert>Accent</nve-alert>
+      <nve-alert>Accent</nve-alert>
+    </nve-alert-group>
+
+    <nve-alert-group status="warning">
+      <nve-alert>Warning</nve-alert>
+      <nve-alert>Warning</nve-alert>
+    </nve-alert-group>
+
+    <nve-alert-group status="success">
+      <nve-alert>Success</nve-alert>
+      <nve-alert>Success</nve-alert>
+    </nve-alert-group>
+
+    <nve-alert-group status="danger">
+      <nve-alert>Danger</nve-alert>
+      <nve-alert>Danger</nve-alert>
+    </nve-alert-group>
+  </div>
+  `
+}
+
+export const Closable = {
+  render: () => html`
+    <div nve-layout="column gap:md">
+      <nve-alert-group>
+        <nve-alert closable>Standard</nve-alert>
+        <nve-alert closable>Standard</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="accent">
+        <nve-alert closable>Accent</nve-alert>
+        <nve-alert closable>Accent</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="warning">
+        <nve-alert closable>Warning</nve-alert>
+        <nve-alert closable>Warning</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="success">
+        <nve-alert closable>Success</nve-alert>
+        <nve-alert closable>Success</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="danger">
+        <nve-alert closable>Danger</nve-alert>
+        <nve-alert closable>Danger</nve-alert>
+      </nve-alert-group>
+    </div>
+  `
+}
+
+export const Actions = {
+  render: () => html`
+  <div nve-layout="column gap:md">
+    <nve-alert-group>
+      <nve-alert>
+        Standard <nve-button slot="actions" container="flat">action</nve-button>
+      </nve-alert>
+    </nve-alert-group>
+    <nve-alert-group status="accent">
+      <nve-alert>
+        Standard <nve-button slot="actions" container="flat">action</nve-button>
+      </nve-alert>
+    </nve-alert-group>
+    <nve-alert-group status="warning">
+      <nve-alert>
+        Warning <nve-button slot="actions" container="flat">action</nve-button>
+      </nve-alert>
+    </nve-alert-group>
+    <nve-alert-group status="success">
+      <nve-alert>
+        Success <nve-button slot="actions" container="flat">action</nve-button>
+      </nve-alert>
+    </nve-alert-group>
+    <nve-alert-group status="danger">
+      <nve-alert>
+        Danger <nve-button slot="actions" container="flat">action</nve-button>
+      </nve-alert>
+    </nve-alert-group>
+  </div>
+  `
+}
+
+export const Prominence = {
+  render: () => html`
+    <div nve-layout="column gap:md">
+      <nve-alert-group prominence="emphasis" container="full">
+        <nve-alert closable><span slot="prefix">Standard</span> banner message <a href="#" nve-text="link" slot="actions">view details</a></nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="accent" prominence="emphasis" container="full">
+        <nve-alert closable><span slot="prefix">Accent</span> banner message <a href="#" nve-text="link" slot="actions">view details</a></nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="warning" prominence="emphasis" container="full">
+        <nve-alert closable><span slot="prefix">Warning</span> banner message <a href="#" nve-text="link" slot="actions">view details</a></nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="success" prominence="emphasis" container="full">
+        <nve-alert closable><span slot="prefix">Success</span> banner message <a href="#" nve-text="link" slot="actions">view details</a></nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="danger" prominence="emphasis" container="full">
+        <nve-alert closable><span slot="prefix">Danger</span> banner message <a href="#" nve-text="link" slot="actions">view details</a></nve-alert>
+      </nve-alert-group>
+    </div>
+  `
+}
+
+export const Themes = {
+  render: () => html`
+    <div nve-theme="root light" nve-layout="column gap:md pad:md">
+      <nve-alert-group>
+        <nve-alert>Standard</nve-alert>
+        <nve-alert>Standard</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="accent">
+        <nve-alert>Accent</nve-alert>
+        <nve-alert>Accent</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="warning">
+        <nve-alert>Warning</nve-alert>
+        <nve-alert>Warning</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="success">
+        <nve-alert>Success</nve-alert>
+        <nve-alert>Success</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="danger">
+        <nve-alert>Danger</nve-alert>
+        <nve-alert>Danger</nve-alert>
+      </nve-alert-group>
+    </div>
+    <div nve-theme="root dark" nve-layout="column gap:md pad:md">
+      <nve-alert-group>
+        <nve-alert>default</nve-alert>
+        <nve-alert>default</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="accent">
+        <nve-alert>Accent</nve-alert>
+        <nve-alert>Accent</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="warning">
+        <nve-alert>Warning</nve-alert>
+        <nve-alert>Warning</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="success">
+        <nve-alert>Success</nve-alert>
+        <nve-alert>Success</nve-alert>
+      </nve-alert-group>
+
+      <nve-alert-group status="danger">
+        <nve-alert>Danger</nve-alert>
+        <nve-alert>Danger</nve-alert>
+      </nve-alert-group>
+    </div>
+  `
+}
+
+export const Multiline = {
+  render: () => html`
+    <nve-alert-group status="danger">
+      <nve-alert>
+        Session Paused:
+        <div slot="content" nve-layout="column gap:sm align:stretch">
+          <p nve-text="body sm">Failed to verify drives. <a href="#" nve-text="link">Check Status</a></p>
+          <nve-divider style="--color: var(--nve-sys-text-muted-color)"></nve-divider>
+          <pre nve-text="code sm" style="background: transparent !important; margin: 0;">
+10:06:01 AM [verify] update /virtual:/verify/
+10:06:11 AM [build] update /virtual:/build/
+10:06:22 AM [error] update /virtual:/error/ - failed to connect to remote</pre>
+        </div>
+      </nve-alert>
+    </nve-alert-group>
   `
 }
---
+++ projects/site/src/docs/elements/alert.md
@@ -0,0 +1,64 @@
+---
+{
+  title: 'Alert',
+  layout: 'docs.11ty.js',
+  tag: 'nve-alert',
+  associatedElements: ['nve-alert-group']
+}
+---
+
+## Installation
+
+```typescript
+import '@nvidia-elements/core/alert/define.js';
+```
+
+```html
+<nve-alert status="success">success message</nve-alert>
+```
+
+## Standard
+
+{% story 'nve-alert', 'Default' %}
+
+## Status
+
+{% api 'nve-alert', 'property', 'status' %}
+
+{% story 'nve-alert', 'Status' %}
+
+## Alert Group
+
+An alert group is an element that displays a group of related and important messages in a way that attracts the user's attention without interrupting the user's task.
+
+{% story 'nve-alert', 'AlertGroupDefault' %}
+
+## Alert Group Status
+
+{% api 'nve-alert-group', 'property', 'status' %}
+
+{% story 'nve-alert', 'AlertGroupStatus' %}
+
+## Closable
+
+{% api 'nve-alert', 'property', 'closable' %}
+
+{% story 'nve-alert', 'Closable' %}
+
+## Actions
+
+{% api 'nve-alert', 'slot', 'actions' %}
+
+{% story 'nve-alert', 'Actions' %}
+
+## Prominence
+
+{% api 'nve-alert-group', 'property', 'prominence' %}
+
+{% story 'nve-alert', 'Prominence' %}
+
+## Multiline
+
+{% api 'nve-alert', 'property', 'multiline' %}
+
+{% story 'nve-alert', 'Multiline' %}
