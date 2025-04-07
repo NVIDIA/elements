a02d460c2d45719485520ef06f2489259408a06b
Cory Rylan <crylan@nvidia.com>
2025-04-07 14:45:08 -0500

    fix(core): ssr logging
    - enable silent logs for certain ssr components
    - undocument experimental renderer
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/internal/controllers/type-ssr.controller.ts
+++ projects/elements/src/internal/controllers/type-ssr.controller.ts
@@ -14,32 +14,37 @@ type UnprotectedLitElement = ReactiveElement & {
 /**
  * Catch and manage hydration related fallbacks for SSR
  */
-export function typeSSR<T extends UnprotectedLitElement>(): ClassDecorator {
-  return (target: LegacyDecoratorTarget) => target.addInitializer((instance: T) => new TypeSSRController(instance));
+export function typeSSR<T extends UnprotectedLitElement>(options: { log?: boolean } = { log: true }): ClassDecorator {
+  return (target: LegacyDecoratorTarget) =>
+    target.addInitializer((instance: T) => new TypeSSRController(instance, options));
 }
 
 export class TypeSSRController<T extends ReactiveElement> implements ReactiveController {
   // https://www.konnorrogers.com/posts/2024/running-lit-ssr-in-web-awesome#hydration-errors
   didSSR = Boolean(this.host.shadowRoot);
 
-  constructor(private host: T & UnprotectedLitElement) {
+  constructor(
+    private host: T & UnprotectedLitElement,
+    private options: { log?: boolean }
+  ) {
     this.host.addController(this);
 
     // https://github.com/lit/lit/issues/1434#issuecomment-1598332000
     if (this.didSSR) {
+      const options = this.options;
       const updateOriginal = host.update;
       Object.defineProperty(host, 'update', {
         value: function (...args) {
           try {
             updateOriginal.call(host, args);
-          } catch (e) {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          } catch (_) {
             const renderRoot = host.renderRoot as HTMLElement;
             renderRoot.innerHTML = renderRoot.innerHTML.split('<!--lit-part ')[0];
             render(host.render(), renderRoot, host.renderOptions);
 
-            if (getEnv() !== 'production') {
+            if (getEnv() !== 'production' && options.log) {
               LogService.warn(getSSRMismatchWarning(host.localName));
-              console.log(e);
             }
           }
           return updateOriginal;
