5472395de989aec3825e8f7b574cd6b2fabf91f5
Cory Rylan <crylan@nvidia.com>
2025-04-09 13:39:10 -0500

    fix(elements): popover anchoring
    - fix cross shadow root trigger check
    - fix cross shadow root anchor selection
    - add opt in for force use of css absolute positioning
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/internal/controllers/type-native-popover-trigger.controller.ts
+++ projects/elements/src/internal/controllers/type-native-popover-trigger.controller.ts
@@ -1,6 +1,6 @@
 import type { ReactiveElement, ReactiveController } from 'lit';
 import type { LegacyDecoratorTarget } from '../types/index.js';
-import { getFlattenedDOMTree } from '../utils/dom.js';
+import { getFlattenedDOMTree, sameRenderRoot } from '../utils/dom.js';
 
 export interface NativePopoverTrigger extends ReactiveElement {
   disabled: boolean;
@@ -24,7 +24,9 @@ export class TypeNativePopoverTriggerController<T extends NativePopoverTrigger> 
       const id = this.host.popovertarget?.length ? this.host.popovertarget : this.host.popoverTargetElement?.id;
 
       if (id && !this.host.disabled) {
-        const popover = getFlattenedDOMTree(this.host.getRootNode()).find(e => e.id === id);
+        const popover = getFlattenedDOMTree(this.host.getRootNode()).find(
+          e => e.id === id && sameRenderRoot(this.host, e)
+        );
 
         if (this.host.popoverTargetAction === 'hide') {
           popover?.hidePopover();
