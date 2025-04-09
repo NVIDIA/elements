411b48fa1fb7f3ea314393ff2569aff27f71c69a
Cory Rylan <crylan@nvidia.com>
2025-04-09 13:39:10 -0500

    fix(core): popover anchoring
    - fix cross shadow root trigger check
    - fix cross shadow root anchor selection
    - add opt in for force use of css absolute positioning
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/panel/panel.test.lighthouse.ts
+++ projects/elements/src/panel/panel.test.lighthouse.ts
@@ -22,6 +22,6 @@ describe('panel lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(19.2);
+    expect(report.payload.javascript.kb).toBeLessThan(19.5);
   });
 });
