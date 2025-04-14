cef4cb856f0b038fa898a5d9dbac9a49ac1bd5f0
Cory Rylan <crylan@nvidia.com>
2025-04-14 12:16:13 -0500

    chore(ci): eslint-html
    - adds basic eslint-html config for html files/projects
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/eslint.config.js
+++ projects/elements/eslint.config.js
@@ -1,4 +1,10 @@
-import { browserJavaScriptConfig, browserTypescriptConfig, libraryConfig, litConfig } from '@internals/eslint';
+import {
+  browserJavaScriptConfig,
+  browserTypescriptConfig,
+  libraryConfig,
+  litConfig,
+  htmlConfig
+} from '@internals/eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [...browserJavaScriptConfig, ...browserTypescriptConfig, ...libraryConfig, ...litConfig];
+export default [...browserJavaScriptConfig, ...browserTypescriptConfig, ...libraryConfig, ...litConfig, ...htmlConfig];
