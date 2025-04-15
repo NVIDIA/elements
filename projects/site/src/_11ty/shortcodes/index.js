39e1e334f352cd5217f116eead42f24040bf7e63
Cory Rylan <crylan@nvidia.com>
2025-04-15 13:22:30 -0500

    chore(ci): enable incremental story to json vite builds
    - enables story to json plugin to be incrementally compiled
    - enables site to live reload when a story changes (elements package only atm)
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/site/src/_11ty/shortcodes/index.js
+++ projects/site/src/_11ty/shortcodes/index.js
@@ -1,8 +1,5 @@
-import { MetadataService } from '@nve-internals/metadata';
 import { createPlaygroundURLFromStorySource } from '@nve-internals/elements-api';
 import markdown from '../libraries/markdown.js';
-
-const metrics = await MetadataService.getMetadata();
 
 export async function apiShortcode(tag, type, value) {
   return /* html */ `<nve-api-detail tag="${tag}" type="${type}" value="${value}"></nve-api-detail>`;
@@ -10,13 +7,26 @@ export async function apiShortcode(tag, type, value) {
 
 export async function storyShortcode(tag, storyName, userConfig = { inline: true, height: '95%' }) {
   const config = typeof userConfig === 'string' ? JSON.parse(userConfig) : userConfig;
-  const story = metrics['@nvidia-elements/core'].elements.find(s => s.name === tag)?.stories.find(s => s.id === storyName);
+  const name = tag.replace('nve-', '');
+  const storyModule = await import(`@nvidia-elements/core/${name}/${name}.stories.json`, { with: { type: 'json' } });
+  const story = storyModule.default.stories.find(s => s.id === storyName);
+  const reload =
+    process.env.ELEVENTY_RUN_MODE === 'serve' && config.inline && story
+      ? /* html */ `
+  <script type="module">
+    import stories from '@nvidia-elements/core/${name}/${name}.stories.json' with { type: 'json' };
+    const canvas = document.querySelector('nve-api-canvas#${story.id}');
+    const story = stories.stories.find(s => s.id === '${story.id}');
+    canvas.innerHTML = story.template ?? '';
+    canvas.source = story.template ?? '';
+  </script>`
+      : '';
 
   return story
     ? /* html */ `
-<nve-api-canvas>
+<nve-api-canvas id="${story.id}">
 <nve-button container="flat" slot="suffix"><a href="${createPlaygroundURLFromStorySource(story.template, { id: storyName, globals: {} })}" target="_blank">Playground</a></nve-button>
 <template>${markdown.utils.escapeHtml(story.template.replace(/\n\n/g, '\n'))}</template>${config.inline ? story.template.replaceAll('\n', '') : `<iframe loading="lazy" src="stories/${tag.replace('nve-', '')}/${story.id.toLowerCase()}.html" style="height: ${config.height}; width: 95%; border: none;" />`}
-</nve-api-canvas>`
+</nve-api-canvas>${reload}`
     : '';
 }
