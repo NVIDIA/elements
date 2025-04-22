9fc0cdce5f4aea1b2369752e8a933a561c203460
Cory Rylan <crylan@nvidia.com>
2025-04-22 10:26:37 -0500

    fix(core): combobox disabled and checkmarks
    - fixes issue with not disabling select options
    - fixes disabled style issue with menu item
    - renders checkbox for multi select states
    
    closes #120
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/combobox/define.ts
+++ projects/elements/src/combobox/define.ts
@@ -4,6 +4,7 @@ import '@nvidia-elements/core/forms/define.js';
 import '@nvidia-elements/core/dropdown/define.js';
 import '@nvidia-elements/core/menu/define.js';
 import '@nvidia-elements/core/tag/define.js';
+import '@nvidia-elements/core/checkbox/define.js';
 
 define(Combobox);
 
