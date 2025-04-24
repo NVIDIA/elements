f761a3c44b0951a27388a8bf8e058d846817aca6
Will Geller <wgeller@nvidia.com>
2025-04-23 21:14:15 -0400

    chore(docs): migrate tabs...week elements to 11ty docs format

---
+++ projects/site/src/_internal/stories/tooltip/dynamic-anchor.ts
@@ -0,0 +1,51 @@
+import { css, html, LitElement } from 'lit';
+import type { PropertyValues } from 'lit';
+
+import { customElement } from 'lit/decorators/custom-element.js';
+import type { Ref } from 'lit/directives/ref.js';
+import { ref, createRef } from 'lit/directives/ref.js';
+
+import '@nvidia-elements/core/tooltip/define.js';
+
+@customElement('dynamic-anchor-position-demo')
+export class DynamicAnchorPositionDemo extends LitElement {
+  /* eslint no-unused-vars: 0 */
+  static styles = [
+    css`
+    :host {
+      position: relative;
+      width: 97vw;
+      height: 97vh;
+      display: block;
+      left: 0;
+      transition: left 1s linear;
+    }
+
+    #anchor {
+      top: 50vh;
+      left: 50vw;
+      width: 1px;
+      height: 1px;
+      position: absolute;
+    }
+  `
+  ];
+
+  #anchor: Ref<HTMLElement> = createRef();
+
+  render() {
+    return html`
+      <!-- <div ${ref(this.#anchor)} id="anchor"></div> -->
+       <!-- TODO why is this not working? -->
+      <nve-tooltip anchor="anchor">tooltip</nve-tooltip>
+    `;
+  }
+
+  firstUpdated(props: PropertyValues<this>) {
+    super.firstUpdated(props);
+    this.addEventListener(
+      'mousemove',
+      e => (this.#anchor.value.style.inset = `${e.clientY - 15}px auto auto ${e.clientX - 15}px`)
+    );
+  }
+}
