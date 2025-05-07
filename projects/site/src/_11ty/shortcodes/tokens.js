1deeaeb4e78112f839b86198ac09a4aeb6d91c21
Cory Rylan <crylan@nvidia.com>
2025-05-07 13:39:14 -0500

    chore(docs): tokens shortcode support
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/site/src/_11ty/shortcodes/tokens.js
+++ projects/site/src/_11ty/shortcodes/tokens.js
@@ -1,10 +1,91 @@
 import tokens from '@nvidia-elements/themes/index.json' with { type: 'json' };
 
 export async function tokensShortcode(token) {
-  return Object.entries(tokens)
+  const formattedTokens = Object.entries(tokens)
     .filter(([name]) => name.includes(token))
-    .map(([name, value]) => {
-      return /* html */ `- ${name}: ${value}`;
-    })
-    .join('\n');
+    .map(([name, value]) => [`--${name}`, `--${value}`]);
+  return renderTokenTable(formattedTokens);
+}
+
+function renderTokenTable(tokens) {
+  return /* html */ `
+  <style>
+    .tokens-table {
+      width: 100%;
+      text-align: left;
+      background: var(--nve-sys-layer-canvas-background);
+
+      th {
+        font-weight: 400;
+      }
+      
+      td {
+        padding-bottom: var(--nve-ref-space-xxs);
+      }
+      
+      td div {
+        background: var(--nve-ref-border-color);
+        min-height: var(--nve-ref-size-1000);
+        display: flex;
+        align-items: center;
+        width: 100%;
+      }
+      
+      td:last-child,
+      td:nth-child(2) {
+        min-width: 190px;
+      }
+      
+      nve-copy-button {
+        opacity: 0;
+      }
+      
+      td:hover nve-copy-button {
+        opacity: 1;
+      }
+    }
+  </style>
+  <table class="tokens-table">
+    <thead>
+      <tr>
+        <th>Token</th>
+        <th>Value</th>
+        <th>Demo</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      ${tokens
+        .map(([name, value]) => {
+          return /* html */ `<tr>
+        <td nve-layout="row gap:xs align:vertical-center">
+          ${name} <nve-copy-button aria-label="Copy to clipboard" value="${name}" container="flat" behavior-copy></nve-copy-button>
+        </td>
+        <td>
+          <code style="user-select: none">${value}</code>
+        </td>
+        <td>
+          ${name.includes('ref-size') || name.includes('ref-space') ? /* html */ `<div style="width: var(${name})"></div>` : ''}
+          ${name.includes('ref-color') || name.includes('sys-visualization') ? /* html */ `<div style="background: var(${name})"></div>` : ''}
+          ${name.includes('ref-font-family') ? /* html */ `<div style="font-family: var(${name}); background: transparent">font</div>` : ''}
+          ${name.includes('ref-font-weight') ? /* html */ `<div style="font-weight: var(${name}); background: transparent">weight</div>` : ''}
+          ${name.includes('ref-font-line-height') ? /* html */ `<div style="font-line-height: var(${name}); background: transparent">line-height</div>` : ''}
+          ${name.includes('ref-font-size') ? /* html */ `<div style="font-size: var(${name}); background: transparent">size</div>` : ''}
+          ${name.includes('ref-border-radius') ? /* html */ `<div style="border-radius: var(${name}); width: 100px; height: 100px"></div>` : ''}
+          ${name.includes('ref-border-color') ? /* html */ `<div style="background: var(${name})"></div>` : ''}
+          ${name.includes('ref-border-width') ? /* html */ `<div style="border: 1px solid var(--nve-ref-border-color); background: transparent; border-width: var(${name})"></div>` : ''}
+          ${name.includes('ref-opacity') ? /* html */ `<div style="opacity: var(${name}); background: #000"></div>` : ''}
+          ${name.includes('ref-shadow') ? /* html */ `<div style="padding: 12px; background: transparent;"><div style="box-shadow: var(${name}); background: var(--nve-sys-layer-container-background)"></div></div>` : ''}
+          ${name.includes('ref-animation') ? /* html */ `<div style="background: transparent"></div>` : ''}
+          ${name.includes('sys-status') || name.includes('sys-support') ? /* html */ `<div style="background: var(${name})"></div>` : ''}
+          ${name.includes('sys-text') ? /* html */ `<div style="background: var(${name})"></div>` : ''}
+          ${name.includes('sys-accent') ? /* html */ `<div style="background: var(${name})"></div>` : ''}
+          ${name.includes('sys-interaction') ? /* html */ `<div style="background: var(${name})"></div>` : ''}
+          ${name.includes('sys-layer') ? /* html */ `<div style="background: var(${name})"></div>` : ''}
+        </td>
+      </tr>`;
+        })
+        .join('')}
+    </tbody>
+  </table>`.replaceAll('\n', '');
 }
---
+++ projects/site/src/docs/foundations/themes/support.md
@@ -0,0 +1,54 @@
+---
+{
+  title: 'Support',
+  layout: 'docs.11ty.js'
+}
+---
+
+# {{ title }}
+
+Support colors are used in combination of text or iconography to convey status of an element or interation. Status type components inlcude `alerts`, `badges`, `tags` and `form control` messages.
+
+- `default`
+- `accent`
+- `success`
+- `warning`
+- `danger`
+
+## Icon Support
+
+{% story '@nvidia-elements/core/icon/icon.stories.json', 'Statuses' %}
+
+## Dot Support
+
+{% story '@nvidia-elements/core/dot/dot.stories.json', 'SupportStatus' %}
+
+## Alert Support
+
+{% story '@nvidia-elements/core/alert/alert.stories.json', 'Status' %}
+
+## Alert Group
+
+{% story '@nvidia-elements/core/alert/alert.stories.json', 'AlertGroupStatus' %}
+
+## Alert Group Banner
+
+{% story '@nvidia-elements/core/alert/alert.stories.json', 'Prominence' %}
+
+## Notification Snackbar
+
+{% story '@nvidia-elements/core/notification/notification.stories.json', 'Status', '{ "inline": false, "height": "640px" }' %}
+
+## Toasts
+
+{% story '@nvidia-elements/core/toast/toast.stories.json', 'Status', '{ "inline": false, "height": "600px" }'  %}
+
+## Support Tokens
+
+Support tokens define supplementary colors and styles for the design system.
+
+{% tokens 'sys-support' %}
+
+## Accent Tokens
+
+{% tokens 'sys-accent' %}
