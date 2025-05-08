31f5e2b01f467d6125b9798f751923630a663acd
Will Geller <wgeller@nvidia.com>
2025-05-07 22:43:33 -0400

    feat(elements): upgrade component status for elements stable for > 90 days

--- projects/elements/src/tree/tree.ts
+++ projects/elements/src/tree/tree.ts
@@ -23,7 +23,7 @@ import globalStyles from './tree.global.css?inline';
  * @storybook https://NVIDIA.github.io/elements/docs/elements/tree/
  * @figma https://www.figma.com/design/vbcJuxNZO6t2KScQ8y5H7z/%F0%9F%93%9A-MagLev-Elements-Design-Catalog?node-id=30-30&t=TiwKVvP1YHl3NylZ-0
  * @aria https://www.w3.org/WAI/ARIA/apg/patterns/treeview/
- * @stable false
+ *
  */
 @keyNavigationList<Tree>()
 export class Tree extends LitElement {
