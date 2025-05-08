fea6b5afa02367fdbfdbd443e59698404dd25468
Cory Rylan <crylan@nvidia.com>
2025-05-08 10:32:52 -0500

    chore(docs): story/demo cleanup
    - fixes javascript language alias for codeblock
    - fixes statefull stories
    - fixes doc pages with few headings
    - fixes token table background
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/json-viewer/json-viewer.stories.ts
+++ projects/elements/src/json-viewer/json-viewer.stories.ts
@@ -6,10 +6,8 @@ export default {
   component: 'nve-json-viewer',
 };
 
-const json = {"tasks": [{ "id": 1, "status": "success", "date": "08/03/2023 14:22:44", "url": "https://elements.nvidia.com/av/ingestion/uploads", "message": "task completed" },{ "id": 2, "status": "failed", "date": "08/09/2023 14:22:44", "url": "https://elements.nvidia.com/av/ingestion/uploads", "message": "Error: cy.request() failed on:\n\nThe response we received from your web server was:\n\n  > 401: Unauthorized\n" }]};
-
 export const Default = {
   render: () => html`
-  <nve-json-viewer expanded>${JSON.stringify(json)}</nve-json-viewer>
+  <nve-json-viewer expanded>${JSON.stringify({"tasks": [{ "id": 1, "status": "success", "date": "08/03/2023 14:22:44", "url": "https://elements.nvidia.com/av/ingestion/uploads", "message": "task completed" },{ "id": 2, "status": "failed", "date": "08/09/2023 14:22:44", "url": "https://elements.nvidia.com/av/ingestion/uploads", "message": "Error: cy.request() failed on:\n\nThe response we received from your web server was:\n\n  > 401: Unauthorized\n" }]})}</nve-json-viewer>
   `
 };
