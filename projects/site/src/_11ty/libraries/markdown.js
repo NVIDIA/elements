0cf5dbe99c69299a8761bf6dc89f71a480e192a5
Cory Rylan <crylan@nvidia.com>
2025-05-09 18:40:18 -0500

    chore(ci): site memory
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/site/src/_11ty/libraries/markdown.js
+++ projects/site/src/_11ty/libraries/markdown.js
@@ -6,7 +6,7 @@ const markdown = markdownIt({
   linkify: true,
   highlight: function (str, lang) {
     lang = lang === 'javascript' ? 'typescript' : lang; // alias javascript to typescript
-    return /* html */ `<nve-codeblock language="${lang}" style="--padding: var(--nve-ref-space-lg)"><template>${markdown.utils.escapeHtml(str)}</template></nve-codeblock>`;
+    return /* html */ `<nve-codeblock language="${lang}"><template>${markdown.utils.escapeHtml(str)}</template></nve-codeblock>`;
   }
 });
 
