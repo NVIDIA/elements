508be5ded2d070a7ca3bc12d06f548e984f3df7f
Cory Rylan <crylan@nvidia.com>
2025-05-09 18:40:18 -0500

    chore(ci): site memory
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/site/src/_11ty/layouts/docs.ts
@@ -0,0 +1,52 @@
+import '/_internal/search/search.js';
+
+// panel toggles
+const systemOptionsPanel = globalThis.document.querySelector<HTMLElement>('#system-options-panel')!;
+const systemOptionsPanelBtn = globalThis.document.querySelector<HTMLElement>('#system-options-panel-btn')!;
+systemOptionsPanel.addEventListener('close', () => (systemOptionsPanel.hidden = true));
+systemOptionsPanelBtn.addEventListener('click', () => (systemOptionsPanel.hidden = !systemOptionsPanel.hidden));
+
+// resize panels
+const handle = globalThis.document.querySelector<HTMLElement>('nve-resize-handle[slot="left"]')!;
+const panel = globalThis.document.querySelector<HTMLElement>('nve-page-panel[slot="left"]')!;
+handle.addEventListener('input', e => (panel.style.width = (e.target as HTMLInputElement).value + 'px'));
+
+// auto-scroll to deep-link headers
+setTimeout(() => {
+  const headerId = new URL(globalThis.window.parent.location.href).hash.replace('#', '');
+  globalThis.document.getElementById(headerId)?.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+}, 1500);
+
+// preserve scroll position between page transitions
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const content = globalThis.document.querySelector<any>('#sidenav-panel nve-page-panel-content')!;
+globalThis.window.addEventListener('beforeunload', () => {
+  sessionStorage.setItem('sidenav-scroll-position', content.scrollTop);
+});
+
+const savedPosition = sessionStorage.getItem('sidenav-scroll-position');
+if (savedPosition) {
+  await customElements.whenDefined('nve-page-panel-content');
+  content.scrollTop = parseInt(savedPosition);
+  await content.updateComplete;
+  content.scrollTop = parseInt(savedPosition);
+}
+
+// search
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const docsSearch = globalThis.document.querySelector<any>('#docs-search')!;
+const docsNav = globalThis.document.querySelector<HTMLElement>('#docs-nav')!;
+const docsNavDisplay = docsNav.style.display;
+
+let isSearching = false;
+docsSearch.addEventListener('search-change', (event: CustomEvent) => (isSearching = event.detail.length > 0));
+docsSearch.addEventListener('search-reset', () => toggleSideNav(true));
+docsSearch.addEventListener('search-focus', () => toggleSideNav(false));
+docsSearch.addEventListener('search-blur', () => !isSearching && toggleSideNav(true));
+
+const toggleSideNav = (state: boolean) => {
+  docsNav.style.display = state ? docsNavDisplay : 'none';
+};
--- projects/site/src/_internal/search/search-bar.ts
+++ projects/site/src/_internal/search/search-bar.ts
@@ -1,19 +1,12 @@
-import { html, LitElement, css, unsafeCSS } from 'lit';
+import { html, LitElement, css } from 'lit';
 import { query } from 'lit/decorators/query.js';
 import { customElement } from 'lit/decorators/custom-element.js';
-
-/* eslint-disable no-inline-css/no-restricted-imports */
-import layout from '@nvidia-elements/styles/layout.css?inline';
-import typography from '@nvidia-elements/styles/typography.css?inline';
-
 import '@nvidia-elements/core/icon-button/define.js';
 import '@nvidia-elements/core/search/define.js';
 
-@customElement('nve-docs-search-bar')
+@customElement('nvd-search-bar')
 export class DocsSearchBar extends LitElement {
   static styles = [
-    unsafeCSS(layout),
-    unsafeCSS(typography),
     css`
     nve-search:has(input:placeholder-shown) nve-icon-button {
       display: none;
