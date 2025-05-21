53d24e9ebda30e0def8d3d53f42bf9c1f1a6e8fc
Cory Rylan <crylan@nvidia.com>
2025-05-20 19:32:13 -0500

    chore(core): update lighthouse results
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/grid/grid.test.lighthouse.ts
+++ projects/elements/src/grid/grid.test.lighthouse.ts
@@ -26,6 +26,6 @@ describe('grid lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(89); // https://github.com/dequelabs/axe-core/issues/4259
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(22.6);
+    expect(report.payload.javascript.kb).toBeLessThan(22.8);
   });
 });
