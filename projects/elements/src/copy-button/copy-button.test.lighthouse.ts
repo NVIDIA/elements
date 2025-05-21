e8681afccb9f968fdff1760de4430d5fcb864d10
Cory Rylan <crylan@nvidia.com>
2025-05-20 19:32:13 -0500

    chore(core): update lighthouse results
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/copy-button/copy-button.test.lighthouse.ts
+++ projects/elements/src/copy-button/copy-button.test.lighthouse.ts
@@ -13,6 +13,6 @@ describe('copy-button lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(25.5);
+    expect(report.payload.javascript.kb).toBeLessThan(25.6);
   });
 });
