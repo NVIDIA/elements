84fcfb31d2c229ef1bdc97daf80bfa7dacc867b3
John Yanarella <jyanarella@nvidia.com>
2025-05-27 15:05:49 -0700

    fix(monaco): prebundle vendored code

--- projects/monaco/src/workers/ts.worker.ts
+++ projects/monaco/src/workers/ts.worker.ts
@@ -1,2 +1,2 @@
-import * as worker from 'monaco-editor/esm/vs/language/typescript/ts.worker.js';
+import * as worker from '../vendor/monaco-editor/language/typescript/ts.worker.js';
 export default worker;
