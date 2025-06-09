b401572f2e3cb1d1564d7ed124c746e05be11868
Will Geller <wgeller@nvidia.com>
2025-06-09 10:46:43 -0400

    chore(docs): update indentation on right-side anchor nav

--- projects/site/src/_11ty/transforms/anchor-generator.js
+++ projects/site/src/_11ty/transforms/anchor-generator.js
@@ -8,13 +8,13 @@ export async function anchorGeneratorTransform(content) {
         const idMatch = line.match(/id="([^"]+)"/);
         const id = idMatch ? idMatch[1] : '';
         const textContent = line.replace(/<[^>]+>/g, '').trim();
-        return { id, textContent };
+        return { id, textContent, subHeading: !line.includes('<h2') };
       });
 
     const sidenav =
       !this.page.url.includes('changelog') && headings.length > 2
         ? /* html */ `
-    <ul id="anchor-generator" nve-text="list">${headings.map(h => `<li><a href="${this.page.url.replace('/docs', 'docs')}#${h.id}" nve-text="link truncate">${h.textContent}</a></li>`).join('')}</ul>`
+    <ul id="anchor-generator" nve-text="list">${headings.map(h => `<li><a href="${this.page.url.replace('/docs', 'docs')}#${h.id}" nve-text="link truncate" ${h.subHeading ? 'nve-layout="pad-left:md"' : ''}>${h.textContent}</a></li>`).join('')}</ul>`
         : '';
 
     return content.replace('<!-- ANCHOR-GENERATOR -->', sidenav);
