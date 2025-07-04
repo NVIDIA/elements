0e0699857f7f24de39235100bc593da28564c47a
John Yanarella <jyanarella@nvidia.com>
2025-07-03 17:14:43 -0700

    feat(elements): support nve-monaco-diff-input in nve-control

--- projects/elements/src/date/date.test.lighthouse.ts
+++ projects/elements/src/date/date.test.lighthouse.ts
@@ -16,6 +16,6 @@ describe('date lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(24.1);
+    expect(report.payload.javascript.kb).toBeLessThan(24.2);
   });
 });
