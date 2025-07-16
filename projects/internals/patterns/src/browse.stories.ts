543bd1f18893dd8eb233cf978ef8983b0aa1ff6e
Cory Rylan <crylan@nvidia.com>
2025-07-16 12:24:31 -0500

    chore(docs): site watch mode
    - fixes issue with vite/11ty watch mode conflicting updates
    - fixes eslint ignores on temp 11ty dir
    - adds missing patterns project to metadata
    - add test descriptions for metadata improvements
    - improve conditional data of cem plugins
    - increase timeout for playwright lock for monaco tests
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/internals/patterns/src/browse.stories.ts
+++ projects/internals/patterns/src/browse.stories.ts
@@ -5,33 +5,32 @@ import '@nvidia-elements/core/card/define.js';
 import '@nvidia-elements/core/divider/define.js';
 
 export default {
-  title: 'Patterns/Examples'
+  title: 'Patterns/Examples',
+  component: 'nve-internal-patterns'
 };
 
 export const ContentRow = {
   render: () => html`
-  <div>
-    <nve-card role="listitem" container="full">
-      <nve-card-content nve-layout="grid align:vertical-center align:space-between gap:md">
-        <div nve-layout="span:4 row gap:md align:vertical-center">
-          <img src="https://placehold.co/600x400" alt="placeholder" style="max-width: 100px; border-radius: var(--nve-ref-border-radius-sm)" />
-          <div nve-layout="column gap:sm">
-            <h2 nve-text="label medium">Activity Dashboard</h2>
-            <p nve-text="body sm muted">Last saved: Oct 19, 21 by Camru</p>
-          </div>
+  <nve-card role="listitem" container="full">
+    <nve-card-content nve-layout="grid align:vertical-center align:space-between gap:md">
+      <div nve-layout="span:4 row gap:md align:vertical-center">
+        <img src="https://placehold.co/600x400" alt="placeholder" style="max-width: 100px; border-radius: var(--nve-ref-border-radius-sm)" />
+        <div nve-layout="column gap:sm">
+          <h2 nve-text="label medium">Activity Dashboard</h2>
+          <p nve-text="body sm muted">Last saved: Oct 19, 21 by Camru</p>
         </div>
-        <p nve-text="body sm" nve-layout="span:5">A dashboard displaying current project activity grouped by User, Host or IP</p>
-        <div nve-layout="span:3 row gap:sm align:right">
-          <div nve-layout="row gap:xs">
-            <nve-icon-button icon-name="eye"></nve-icon-button>
-            <nve-icon-button icon-name="copy"></nve-icon-button>
-            <nve-icon-button icon-name="delete"></nve-icon-button>
-          </div>
-          <nve-divider orientation="vertical"></nve-divider>
-          <nve-button>Add Panel</nve-button>
+      </div>
+      <p nve-text="body sm" nve-layout="span:5">A dashboard displaying current project activity grouped by User, Host or IP</p>
+      <div nve-layout="span:3 row gap:sm align:right">
+        <div nve-layout="row gap:xs">
+          <nve-icon-button icon-name="eye"></nve-icon-button>
+          <nve-icon-button icon-name="copy"></nve-icon-button>
+          <nve-icon-button icon-name="delete"></nve-icon-button>
         </div>
-      </nve-card-content>
-    </nve-card>
-  </div>
+        <nve-divider orientation="vertical"></nve-divider>
+        <nve-button>Add Panel</nve-button>
+      </div>
+    </nve-card-content>
+  </nve-card>
   `
 }
