f27237c072969d9ae2d8c1aca682d2308b3db310
Will Geller <wgeller@nvidia.com>
2025-08-01 15:34:55 -0400

    chore(docs): fix lighthouse check preventing deploys

--- projects/site/eslint.config.js
+++ projects/site/eslint.config.js
@@ -1,4 +1,15 @@
 import { browserJavaScriptConfig, browserTypescriptConfig, appConfig, htmlConfig } from '@internals/eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [...browserJavaScriptConfig, ...browserTypescriptConfig, ...appConfig, ...htmlConfig];
+export default [
+  ...browserJavaScriptConfig,
+  ...browserTypescriptConfig,
+  ...appConfig,
+  ...htmlConfig,
+  {
+    files: ['src/_internal/canvas-editable/canvas-editable.ts'],
+    rules: {
+      'no-inline-css/no-restricted-imports': 'off' // temporary workaround as latest version of responsive.css is not published yet, and needs a direct ?inline import for the iFramed editable canvas example of Combined Layout
+    }
+  }
+];
