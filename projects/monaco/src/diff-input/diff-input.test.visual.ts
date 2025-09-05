fe8fc7ba39341cbb337fe872b59dd006e3962b6d
Cory Rylan <crylan@nvidia.com>
2025-09-05 15:19:25 -0500

    chore(monaco): visual test optimizations
    - group visual test run into a single instance reducing visual test time from ~2mins to ~20sec
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/monaco/src/diff-input/diff-input.test.visual.ts
+++ projects/monaco/src/diff-input/diff-input.test.visual.ts
@@ -1,32 +1,3 @@
-import { expect, test, describe } from 'vitest';
-import { visualRunner } from '@internals/vite';
-
-describe('monaco diff input visual', () => {
-  test('input should match visual baseline when narrow', async () => {
-    const report = await visualRunner.render('monaco-diff-input.narrow', template('', 640, 1252), { network: true });
-    expect(report.maxDiffPercentage).toBeLessThanOrEqual(16);
-  });
-
-  test('input should match visual baseline when wide', async () => {
-    const report = await visualRunner.render('monaco-diff-input.wide', template('', 1280, 1252), { network: true });
-    expect(report.maxDiffPercentage).toBeLessThanOrEqual(16);
-  });
-
-  test('input should match visual baseline dark theme when narrow', async () => {
-    const report = await visualRunner.render('monaco-diff-input.dark.narrow', template('dark', 640, 1252), {
-      network: true
-    });
-    expect(report.maxDiffPercentage).toBeLessThanOrEqual(16);
-  });
-
-  test('input should match visual baseline dark theme when wide', async () => {
-    const report = await visualRunner.render('monaco-diff-input.dark.wide', template('dark', 1280, 1252), {
-      network: true
-    });
-    expect(report.maxDiffPercentage).toBeLessThanOrEqual(16);
-  });
-});
-
 const originalJsonValue = `{
   "name": "my-original-app",
   "version": "0.0.0"
@@ -53,15 +24,29 @@ const jsonSchema = `{
   "required": ["name", "version"]
 }`;
 
-function template(theme: '' | 'dark' = '', width: number, height: number) {
+export function template() {
   return /* html */ `
-  <script type="module">
-    import '@nvidia-elements/core/forms/define.js';
-    import '@nvidia-elements/monaco/diff-input/define.js';
-    document.documentElement.setAttribute('nve-theme', '${theme} reduced-motion');
-  </script>
+  <div nve-layout="column gap:md" style="width: 1280px; height: 1252px;">
+    <nve-monaco-diff-input language="json" schema='${jsonSchema}' original='${originalJsonValue}' value='${jsonValue}'></nve-monaco-diff-input>
 
-  <div nve-layout="column gap:md" style="width: ${width}px; height: ${height}px;">
+    <nve-monaco-diff-input language="json" schema='${jsonSchema}' original='${originalJsonValue}' value='${jsonValue}' disabled></nve-monaco-diff-input>
+
+    <nve-monaco-diff-input language="json" schema='${jsonSchema}' original='${originalJsonValue}' value='${jsonValue}' line-numbers="on" side-by-side></nve-monaco-diff-input>
+
+    <nve-control style="width: 100%;">
+      <label>•︎•︎•︎•︎•︎•︎</label>
+      <nve-monaco-diff-input nve-control language="json" schema='${jsonSchema}' original='${originalJsonValue}' value='${jsonValue}' line-numbers="on" side-by-side></nve-monaco-diff-input>
+      <nve-control-message>•︎•︎•︎•︎•︎•︎</nve-control-message>
+    </nve-control>
+
+    <nve-control style="width: 100%;">
+      <label>•︎•︎•︎•︎•︎•︎</label>
+      <nve-monaco-diff-input nve-control language="json" schema='${jsonSchema}' original='${originalJsonValue}' value='${invalidJsonValue}' line-numbers="on" side-by-side></nve-monaco-diff-input>
+      <nve-control-message>•︎•︎•︎•︎•︎•︎</nve-control-message>
+    </nve-control>
+  </div>
+
+  <div nve-layout="column gap:md" style="width: 640px; height: 1252px;">
     <nve-monaco-diff-input language="json" schema='${jsonSchema}' original='${originalJsonValue}' value='${jsonValue}'></nve-monaco-diff-input>
 
     <nve-monaco-diff-input language="json" schema='${jsonSchema}' original='${originalJsonValue}' value='${jsonValue}' disabled></nve-monaco-diff-input>
