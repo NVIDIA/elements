a9381b262d97866c5b90e52c1b349d4fd2536ca0
Cory Rylan <crylan@nvidia.com>
2025-09-17 10:22:23 -0500

    chore(core): bump lighthouse tests
    - update bundle sizes due to minor build dep update, less than 0.2kb on average
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/sort-button/sort-button.test.lighthouse.ts
+++ projects/elements/src/sort-button/sort-button.test.lighthouse.ts
@@ -13,6 +13,6 @@ describe('sort-button lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(17.1);
+    expect(report.payload.javascript.kb).toBeLessThan(17.2);
   });
 });
