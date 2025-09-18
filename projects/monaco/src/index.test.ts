a023e30f2c6b9333d73200c8a2e4f3722cea3cf4
John Yanarella <jyanarella@nvidia.com>
2025-09-18 08:48:29 -0700

    fix(monaco): typescript default options without side effects

--- projects/monaco/src/index.test.ts
+++ projects/monaco/src/index.test.ts
@@ -1,8 +1,18 @@
 import { describe, expect, it } from 'vitest';
 import { VERSION } from './index.js';
+import * as monaco from './index.js';
 
 describe('VERSION', () => {
   it('should export a VERSION const', () => {
     expect(VERSION).toBe('0.0.0');
+  });
+
+  it('should be pre-configured with modern TypeScript compiler options defaults suitable for in-browser validation', () => {
+    const compilerOptions = monaco.languages.typescript.typescriptDefaults.getCompilerOptions();
+    expect(compilerOptions.module).toBe(monaco.languages.typescript.ModuleKind.ESNext);
+    expect(compilerOptions.target).toBe(monaco.languages.typescript.ScriptTarget.ESNext);
+    expect(compilerOptions.isolatedModules).toBe(true);
+    expect(compilerOptions.allowNonTsExtensions).toBe(true);
+    expect(compilerOptions.moduleDetection).toBe(3 /* monaco.languages.typescript.ModuleDetectionKind.Force */);
   });
 });
