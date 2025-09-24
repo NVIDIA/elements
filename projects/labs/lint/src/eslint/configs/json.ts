3c6323af1561d0ce4babf1fa396b4438433f1822
Cory Rylan <crylan@nvidia.com>
2025-09-24 14:24:50 -0500

    feat(labs-lint): no-unexpected-library-dependencies
    - add new no-unexpected-library-dependencies rule
    - remove dependency of JSON import type internally for better browser support
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/labs/lint/src/eslint/configs/json.ts
@@ -0,0 +1,32 @@
+import type { Linter } from 'eslint';
+import json from '@eslint/json';
+import noUnexpectedLibraryDependencies from '../rules/no-unexpected-library-dependencies.js';
+
+const source = ['package.json'];
+const ignores = [
+  'node_modules/',
+  'coverage/',
+  'dist/',
+  'build/',
+  '.visual/',
+  '.lighthouse/',
+  '.wireit/',
+  '.11ty-vite/'
+];
+
+export const elementsJsonConfig: Linter.Config = {
+  files: [...source],
+  ignores,
+  language: 'json/json',
+  plugins: {
+    json,
+    '@nvidia-elements/lint': {
+      rules: {
+        'no-unexpected-library-dependencies': noUnexpectedLibraryDependencies
+      }
+    }
+  },
+  rules: {
+    '@nvidia-elements/lint/no-unexpected-library-dependencies': ['error']
+  }
+};
--- projects/labs/lint/src/eslint/index.ts
+++ projects/labs/lint/src/eslint/index.ts
@@ -1,9 +1,10 @@
 import type { Linter } from 'eslint';
 import { elementsHtmlConfig } from './configs/html.js';
 import { elementsCssConfig } from './configs/css.js';
+import { elementsJsonConfig } from './configs/json.js';
 
 export const VERSION = '0.0.0';
 
-export const elementsRecommended: Linter.Config[] = [elementsHtmlConfig, elementsCssConfig];
+export const elementsRecommended: Linter.Config[] = [elementsHtmlConfig, elementsCssConfig, elementsJsonConfig];
 
-export { elementsHtmlConfig, elementsCssConfig };
+export { elementsHtmlConfig, elementsCssConfig, elementsJsonConfig };
