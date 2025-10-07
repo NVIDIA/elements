8df33ec6ec2448dac1b904c3f25083b48f88541f
John Yanarella <jyanarella@nvidia.com>
2025-10-06 18:23:37 -0700

    fix(monaco): avoid unnecessary problems re-rendering

---
+++ projects/monaco/src/internal/utils/problem-utils.ts
@@ -0,0 +1,19 @@
+import type { Problem } from '../types/index.js';
+
+export function equalsProblem(a: Problem, b: Problem): boolean {
+  return (
+    a.owner === b.owner &&
+    a.resource === b.resource &&
+    a.severity === b.severity &&
+    a.message === b.message &&
+    a.source === b.source &&
+    a.startLineNumber === b.startLineNumber &&
+    a.startColumn === b.startColumn &&
+    a.endLineNumber === b.endLineNumber &&
+    a.endColumn === b.endColumn
+  );
+}
+
+export function equalsProblems(a: Problem[], b: Problem[]): boolean {
+  return a.length === b.length && a.every((problem, index) => equalsProblem(problem, b[index]));
+}
