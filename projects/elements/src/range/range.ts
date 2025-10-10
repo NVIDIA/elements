8ded7f93d45b4811f58c386e4de63ba0769be905
Will Geller <wgeller@nvidia.com>
2025-10-09 22:25:51 -0400

    feat(core): update nve-range styling; custom datalist tick rendering

--- projects/elements/src/range/range.ts
+++ projects/elements/src/range/range.ts
@@ -3,13 +3,12 @@ import { Control } from '@nvidia-elements/core/forms';
 import globalStyles from './range.global.css?inline';
 import styles from './range.css?inline';
 import type { PropertyValues } from 'lit';
-
+import { html, nothing, isServer } from 'lit';
 /**
  * @element nve-range
  * @description A range slider is a control that enables users to choose a value from a continuous range of values.
  * @since 0.3.0
  * @entrypoint \@nvidia-elements/core/range
- * @cssprop --track-width
  * @cssprop --track-height
  * @cssprop --track-border-radius
  * @cssprop --thumb-height
@@ -45,6 +44,27 @@ export class Range extends Control {
     const value = val ?? this.input.valueAsNumber;
     const min = this.input.min ? parseFloat(this.input.min) : 0;
     const max = this.input.max ? parseFloat(this.input.max) : 100;
-    this.style.setProperty('--track-width', `${Math.floor(((value - min) / (max - min)) * 100)}%`);
+    this.style.setProperty('--track-width', `${Math.floor(((value - min) / (max - min)) * 100) / 100}`);
+  }
+
+  protected get suffixContent() {
+    const datalist = !isServer ? this.querySelector<HTMLDataListElement>('datalist') : null;
+    if (datalist) {
+      const options = Array.from(datalist?.options ?? []);
+      const min = this.input?.min ? parseFloat(this.input?.min) : 0;
+      const max = this.input?.max ? parseFloat(this.input?.max) : 100;
+
+      return html`<div class="datalist-labels">
+        ${options.map(option => {
+          const value = parseFloat(option.value);
+          const position = ((value - min) / (max - min)) * 100;
+          const style = `left: ${position}%; transform: translateX(-${position}%)`;
+          const template = html`<span class="datalist-tick" style=${style}>${option.textContent || value}</span>`;
+          return template;
+        })}
+      </div>`;
+    } else {
+      return nothing;
+    }
   }
 }
