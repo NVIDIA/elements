4e176e3db0447da81d7e95814c05fe623ecc06a6
Cory Rylan <crylan@nvidia.com>
2025-10-29 15:56:19 -0500

    chore(ci): gitlab metrics improvements
    - better track template and import usage on gitlab repos
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/site/src/docs/metrics/usage-metrics.11ty.js
+++ projects/site/src/docs/metrics/usage-metrics.11ty.js
@@ -10,6 +10,12 @@ export const data = {
   layout: 'docs.11ty.js'
 };
 
+const elementReferences = metricsUsage.projects.reduce((p, n) => ({ ...p, [n.name]: n.elementReferenceTotal }), {});
+const importReferences = metricsUsage.projects.reduce((p, n) => ({ ...p, [n.name]: n.importReferenceTotal }), {});
+
+const totalElementReferenceTotal = Object.values(elementReferences).reduce((p, n) => n + p, 0);
+const totalImportReferenceTotal = Object.values(importReferences).reduce((p, n) => n + p, 0);
+
 export function render() {
   return this.renderTemplate(
     /* html */ `
@@ -38,21 +44,22 @@ export function render() {
       </section>
       <section nve-layout="row gap:xs align:center">
         <span nve-text="body sm muted">Total Element Template References</span>
-        <nve-badge status="accent">${metricsUsage.projects.reduce((p, n) => n.elementReferenceTotal + p, 0)}</nve-badge>
+        <nve-badge status="accent">${totalElementReferenceTotal}</nve-badge>
       </section>
       <section nve-layout="row gap:xs align:center">
         <span nve-text="body sm muted">Total Import Source References</span>
-        <nve-badge status="accent">${metricsUsage.projects.reduce((p, n) => n.importReferenceTotal + p, 0)}</nve-badge>
+        <nve-badge status="accent">${totalImportReferenceTotal}</nve-badge>
       </section>
+      <nve-icon-button popovertarget="additional-information-dialog" icon-name="information-circle-stroke" container="flat" aria-label="additional information"></nve-icon-button>
     </div>
     <nve-grid style="--scroll-height: calc(100vh - 330px)">
       <nve-grid-header>
         <nve-grid-column name="Project" width="300px">Project</nve-grid-column>
-        <nve-grid-column name="Version" width="150px">Version</nve-grid-column>
         <nve-grid-column name="Repo" width="150px">Repo</nve-grid-column>
-        <nve-grid-column name="Element References" width="180px">Element References</nve-grid-column>
-        <nve-grid-column name="Import References" width="180px">Import References</nve-grid-column>
-        <nve-grid-column name="Source" width="">Source</nve-grid-column>
+        <nve-grid-column name="Source" width="450px">Path</nve-grid-column>
+        <nve-grid-column name="Version" width="180px">Version</nve-grid-column>
+        <nve-grid-column name="Template References" width="180px">Template References</nve-grid-column>
+        <nve-grid-column name="Import References">Import References</nve-grid-column>
       </nve-grid-header>
       ${metricsUsage.projects
         .sort((a, b) => {
@@ -63,25 +70,66 @@ export function render() {
         })
         .sort((a, b) => (a.url?.includes('git-av.nvidia.com') ? 1 : -1))
         .reverse()
-        .map(
-          project => /* html */ `<nve-grid-row>
-        <nve-grid-cell><a href="${project.url}" target="_blank">${project.name ?? 'unknown'}</a></nve-grid-cell>
-        <nve-grid-cell>${getVersionBadge(project.elementsVersion)}</nve-grid-cell>
-        <nve-grid-cell>${project.url.includes('git-av.nvidia.com') ? 'AV Infra' : 'Gitlab'}</nve-grid-cell>
-        <nve-grid-cell>${project.elementReferenceTotal !== 0 ? project.elementReferenceTotal : 'unknown'}</nve-grid-cell>
-        <nve-grid-cell>${project.importReferenceTotal !== 0 ? project.importReferenceTotal : 'unknown'}</nve-grid-cell>
-        <nve-grid-cell><code nve-text="code">${project.path}</code></nve-grid-cell>
-      </nve-grid-row>`
-        )
+        .map(project => {
+          const { elementReferenceTotal, importReferenceTotal } = computeReferences(project);
+          return /* html */ `<nve-grid-row>
+          <nve-grid-cell><a href="${project.url}" target="_blank">${project.name ?? 'unknown'}</a></nve-grid-cell>
+          <nve-grid-cell>${project.url.includes('git-av.nvidia.com') ? 'AV Infra' : 'Gitlab'}</nve-grid-cell>
+          <nve-grid-cell><code nve-text="code">${project.path}</code></nve-grid-cell>
+          <nve-grid-cell>${getVersionBadge(project.elementsVersion)}</nve-grid-cell>
+          <nve-grid-cell>${elementReferenceTotal}</nve-grid-cell>
+          <nve-grid-cell>${importReferenceTotal}</nve-grid-cell>
+        </nve-grid-row>`;
+        })
         .join('')}
       <nve-grid-footer>
         <p nve-text="body muted sm">Report Created on ${reportDate}</p>
       </nve-grid-footer>
     </nve-grid>
   </div>
-</div>`,
-    'md'
+</div>
+<nve-dialog id="additional-information-dialog" modal closable>
+  <nve-dialog-header>
+    <h3 nve-text="heading">Additional Information</h3>
+  </nve-dialog-header>
+  <nve-dialog-content>
+    <dl nve-layout="grid gap:lg">
+      <dt nve-layout="span:4" nve-text="body muted medium">Template Reference</dt>
+      <dd nve-layout="span:8" nve-text="body">A reference to an Elements API in a template/html format file. (html, js, ts, tsx)</dd>
+      <dt nve-layout="span:4" nve-text="body muted medium">Import Reference</dt>
+      <dd nve-layout="span:8" nve-text="body">A reference a Elements library module import. (js, ts, css)</dd>
+      <dt nve-layout="span:4" nve-text="body muted medium"><code>unknown</code></dt>
+      <dd nve-layout="span:8" nve-text="body">The project has not been reported yet or a reference could not be determined yet.</dd>
+      <dt nve-layout="span:4" nve-text="body muted medium">(<code>-</code>)</dt>
+      <dd nve-layout="span:8" nve-text="body">The project references are already accounted for in a parent directory project.</dd>
+    </dl>
+  </nve-dialog-content>
+</nve-dialog>`,
+    'html'
   );
+}
+
+const reportedRepos = new Set();
+
+function computeReferences(project) {
+  let elementReferenceTotal = '-';
+  let importReferenceTotal = '-';
+
+  if (project.url.includes('gitlab') && !reportedRepos.has(project.url)) {
+    elementReferenceTotal = project.elementReferenceTotal;
+    importReferenceTotal = project.importReferenceTotal;
+    reportedRepos.add(project.url);
+  }
+
+  if (project.url.includes('git-av.nvidia.com')) {
+    elementReferenceTotal = project.elementReferenceTotal;
+    importReferenceTotal = project.importReferenceTotal;
+  }
+
+  elementReferenceTotal = elementReferenceTotal === 0 ? 'unknown' : elementReferenceTotal;
+  importReferenceTotal = importReferenceTotal === 0 ? 'unknown' : importReferenceTotal;
+
+  return { elementReferenceTotal, importReferenceTotal };
 }
 
 const reportDate = new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'long' }).format(
@@ -100,6 +148,11 @@ function getVersionBadge(value) {
 
   if (value === 'catalog:' || value === 'latest') {
     status = 'queued';
+  }
+
+  if (value === '0.0.0') {
+    status = 'pending';
+    value = 'unknown';
   }
 
   if (getVersionNum(value) < 1.0) {
