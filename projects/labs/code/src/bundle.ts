75734422f787d2f372f862f1b5eaeb1fc4043906
Cory Rylan <crylan@nvidia.com>
2025-11-19 13:12:49 -0600

    feat(labs-code): add bundle support
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/labs/code/src/bundle.ts
@@ -0,0 +1,13 @@
+import '@nvidia-elements/code/codeblock/languages/bash.js';
+import '@nvidia-elements/code/codeblock/languages/css.js';
+import '@nvidia-elements/code/codeblock/languages/go.js';
+import '@nvidia-elements/code/codeblock/languages/html.js';
+import '@nvidia-elements/code/codeblock/languages/javascript.js';
+import '@nvidia-elements/code/codeblock/languages/json.js';
+import '@nvidia-elements/code/codeblock/languages/markdown.js';
+import '@nvidia-elements/code/codeblock/languages/python.js';
+import '@nvidia-elements/code/codeblock/languages/toml.js';
+import '@nvidia-elements/code/codeblock/languages/typescript.js';
+import '@nvidia-elements/code/codeblock/languages/xml.js';
+import '@nvidia-elements/code/codeblock/languages/yaml.js';
+import '@nvidia-elements/code/codeblock/define.js';
