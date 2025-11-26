6586b4ce71e2b2fd34b740571f3dba01ceb5785e
Cory Rylan <crylan@nvidia.com>
2025-11-26 14:58:55 -0600

    chore(internals): split metadata services and add search indexes
    - split metadata service into two new smaller services, api and projects services
    - standardize on service API surface
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/internals/metadata/src/utils/reports.ts
+++ projects/internals/metadata/src/utils/reports.ts
@@ -1,27 +1,4 @@
-import type { PackageDownload, Project, ProjectTestSummary, Release } from '../types.js';
-
-/**
- * @summary A summary of the metadata for all Elements projects.
- */
-export interface MetadataSummary {
-  created: string;
-  projects: {
-    '@nvidia-elements/core': Project;
-    '@nvidia-elements/core-react': Project;
-    '@nvidia-elements/styles': Project;
-    '@nvidia-elements/testing': Project;
-    '@nvidia-elements/themes': Project;
-    '@nvidia-elements/behaviors-alpine': Project;
-    '@nvidia-elements/brand': Project;
-    '@nvidia-elements/code': Project;
-    '@nvidia-elements/forms': Project;
-    '@nvidia-elements/markdown': Project;
-    '@nvidia-elements/playwright-screencast': Project;
-    '@nvidia-elements/monaco': Project;
-    '@internals/metadata': Project;
-    '@internals/patterns': Project;
-  };
-}
+import type { PackageDownload, ProjectTestSummary, Release } from '../types.js';
 
 export interface ProjectsTestSummary {
   created: string;
@@ -30,10 +7,10 @@ export interface ProjectsTestSummary {
 
 export interface ReleasesSummary {
   created: string;
-  releases: Release[];
+  data: Release[];
 }
 
-export interface DownloadsReport {
+export interface DownloadsSummary {
   created: string;
   packages: PackageDownload[];
   totalDownloads: number;
