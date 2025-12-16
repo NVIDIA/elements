de018264128bc51a3fdb39f43558333cb1314fc4
Cory Rylan <crylan@nvidia.com>
2025-12-16 11:41:49 -0600

    chore(elements): update lighthouse bundle values
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/tabs/tabs.test.lighthouse.ts
+++ projects/elements/src/tabs/tabs.test.lighthouse.ts
@@ -17,6 +17,6 @@ describe('tabs lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(91);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(14.6);
+    expect(report.payload.javascript.kb).toBeLessThan(14.7);
   });
 });
