dde651f9cc0647c3bdb532a30fe29eb2a17abda3
Cory Rylan <crylan@nvidia.com>
2026-01-08 12:05:14 -0600

    feat(create): init
    - Introduced the @nve/create package to facilitate the creation of new Elements projects using `npm init @nve`.
    - Added necessary configuration files including package.json, README.md, and TypeScript configurations.
    - Integrated with existing CI/CD processes and updated relevant documentation to include the new package.
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/create/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, mergeConfig } from 'vite';
+import { libraryNodeBuildConfig } from '@nve-internals/vite';
+import { builtinModules } from 'node:module';
+
+const NODE_BUILT_IN_MODULES = builtinModules.filter(m => !m.startsWith('_'));
+NODE_BUILT_IN_MODULES.push(...NODE_BUILT_IN_MODULES.map(m => `node:${m}`));
+
+export default defineConfig(() => {
+  const libConfig = libraryNodeBuildConfig;
+  if (libConfig.build?.rollupOptions?.external) {
+    libConfig.build.rollupOptions.external = NODE_BUILT_IN_MODULES;
+    if (libConfig.build.rollupOptions.output) {
+      libConfig.build.rollupOptions.output[0].preserveModules = false;
+    }
+  }
+
+  return mergeConfig(libConfig, {
+    build: {
+      ssr: true // trick vite to build with node deps
+    }
+  });
+});
