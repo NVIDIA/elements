05831551db7472c1f891103b8c4cdaf17ab188d1
Cory Rylan <crylan@nvidia.com>
2026-01-08 12:05:14 -0600

    feat(create): init
    - Introduced the @nvidia-elements/create package to facilitate the creation of new Elements projects using `npm init @nve`.
    - Added necessary configuration files including package.json, README.md, and TypeScript configurations.
    - Integrated with existing CI/CD processes and updated relevant documentation to include the new package.
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/create/src/index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { spawn } from 'node:child_process';
+
+const banner = `"░██████████ ░██                                                     ░██               \\n░██         ░██                                                     ░██               \\n░██         ░██  ░███████  ░█████████████   ░███████  ░████████  ░████████  ░███████  \\n░█████████  ░██ ░██    ░██ ░██   ░██   ░██ ░██    ░██ ░██    ░██    ░██    ░██        \\n░██         ░██ ░█████████ ░██   ░██   ░██ ░█████████ ░██    ░██    ░██     ░███████  \\n░██         ░██ ░██        ░██   ░██   ░██ ░██        ░██    ░██    ░██           ░██ \\n░██████████ ░██  ░███████  ░██   ░██   ░██  ░███████  ░██    ░██     ░████  ░███████"`;
+const greeting = `\x1b[32m\x1b[?7l\n${JSON.parse(banner)}\n\n\x1b[0m`;
+
+export async function main() {
+  console.log(`${greeting}Starting up Elements CLI...\n`);
+  const [command, args] = getCommand();
+  spawn(command, args, { stdio: 'inherit' }).on('close', code => process.exit(code ?? 0));
+}
+
+function getCommand(): [string, string[]] {
+  const client = process.env.npm_config_user_agent ?? '';
+  const args = ['--package=@nvidia-elements/cli@latest', '--', 'nve', 'project.create', '--start'];
+  if (process.argv[2]) args.push(`--type=${process.argv[2]}`);
+  if (client.startsWith('pnpm')) return ['pnpm', ['dlx', ...args]];
+  if (client.startsWith('yarn')) return ['yarn', ['dlx', ...args]];
+  return ['npm', ['exec', '-y', ...args]];
+}
+
+void main();
