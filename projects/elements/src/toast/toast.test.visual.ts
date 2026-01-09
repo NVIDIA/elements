d63e95dd4b587bb7c927170512f6b13a27cd00fb
Cory Rylan <crylan@nvidia.com>
2026-01-09 13:41:38 -0600

    fix(elements): popover position for body anchor
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/toast/toast.test.visual.ts
+++ projects/elements/src/toast/toast.test.visual.ts
@@ -11,6 +11,11 @@ describe('toast visual', () => {
     const report = await visualRunner.render('toast.dark', template('dark'));
     expect(report.maxDiffPercentage).toBeLessThan(1);
   });
+
+  test('should position correctly with body anchor', async () => {
+    const report = await visualRunner.render('toast.body-anchor', bodyAnchorTemplate());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
 });
 
 function template(theme: '' | 'dark' = '') {
@@ -26,5 +31,32 @@ function template(theme: '' | 'dark' = '') {
   <div nve-layout="row align:center" style="height: 400px; width: 100%;">
     <nve-button id="btn">•︎•︎•︎•︎•︎•︎</nve-button>
   </div>
+  `;
+}
+
+function bodyAnchorTemplate() {
+  return /* html */ `
+  <script type="module">
+    import '@nvidia-elements/core/toast/define.js';
+  </script>
+  <style>
+    body {
+      height: 920px;
+      width: 1200px;
+    }
+  </style>
+  <nve-toast style="--background: red" position="center" alignment="center">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: blue" position="top" alignment="start">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: yellow" position="top">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: green" position="top" alignment="end">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: purple" position="bottom" alignment="start">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: orange" position="bottom">•︎︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: pink" position="bottom" alignment="end">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: brown; margin-left: 150px" position="left" alignment="start">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: gray;" position="left">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: black; margin-left: 150px" position="left" alignment="end">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: red; margin-right: 150px" position="right" alignment="start">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: blue" position="right">•︎•︎•︎•︎•︎•︎</nve-toast>
+  <nve-toast style="--background: yellow; margin-right: 150px" position="right" alignment="end">•︎•︎•︎•︎•︎•︎</nve-toast>
   `;
 }
