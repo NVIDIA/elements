2c076a0f3bfe5fb7cc4c115f4606712937d30053
Cory Rylan <crylan@nvidia.com>
2026-01-14 21:26:23 -0600

    chore(ci): fix vite cache issues
    Fixes issue where vite can have a race condition writing to its cache
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/starters/nuxt/eslint.config.js
+++ projects/starters/nuxt/eslint.config.js
@@ -3,7 +3,7 @@ import { elementsRecommended } from '@nvidia-elements/lint/eslint';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ['.output/**', '.nuxt/**', '.nitro/**', 'dist/**']
+    ignores: ['.output/**', '.nuxt/**', '.nitro/**', '.wireit/**', 'dist/**']
   },
   ...elementsRecommended
 ];
