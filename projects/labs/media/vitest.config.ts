00820f352dc4c64c8f8e6b9cdb650ac30d8f1fe5
Cory Rylan <crylan@nvidia.com>
2026-01-30 18:42:23 -0600

    chore(labs-media): project setup
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/labs/media/vitest.config.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+import { mergeConfig } from 'vitest/config';
+import { libraryTestConfig } from '@nve-internals/vite/configs/test.js';
+
+const config = mergeConfig(libraryTestConfig, {
+  root: import.meta.dirname,
+  resolve: {
+    alias: { '@nvidia-elements/media': resolve(import.meta.dirname, './src') }
+  },
+  test: {
+    include: ['./src/**/*.test.ts'],
+    setupFiles: [],
+    coverage: {
+      thresholds: {
+        lines: 80,
+        branches: 80,
+        functions: 80,
+        statements: 80
+      }
+    }
+  }
+});
+
+config.test.setupFiles = [];
+
+export default config;
