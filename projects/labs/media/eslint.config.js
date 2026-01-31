322d8bc66e3cc47f998cab05f6328bf1d0eb8c84
Cory Rylan <crylan@nvidia.com>
2026-01-30 18:42:23 -0600

    chore(media): project setup
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/labs/media/eslint.config.js
@@ -0,0 +1,5 @@
+import { elementsRecommended } from '@nvidia-elements/lint/eslint';
+import { browserTypescriptConfig, libraryConfig, litConfig } from '@internals/eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [...elementsRecommended, ...browserTypescriptConfig, ...libraryConfig, ...litConfig];
---
+++ projects/labs/media/vite.config.ts
@@ -0,0 +1,4 @@
+import { mergeConfig } from 'vite';
+import { libraryBuildConfig } from '@internals/vite';
+
+export default mergeConfig(libraryBuildConfig, {});
