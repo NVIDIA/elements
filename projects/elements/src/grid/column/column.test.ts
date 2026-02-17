e10394c4adcf7e23d7fdb99e6bf060417468de60
Cory Rylan <crylan@nvidia.com>
2026-02-17 08:52:20 -0600

    fix(core): grid column positioning
    - fixes issue where template tags impacted css selectors for fixed column positioning
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/grid/column/column.test.ts
+++ projects/elements/src/grid/column/column.test.ts
@@ -1,7 +1,7 @@
 import { html } from 'lit';
 import { describe, expect, it, beforeEach, afterEach } from 'vitest';
 import { createFixture, elementIsStable, removeFixture, untilEvent } from '@internals/testing';
-import { Grid, GridColumn } from '@nvidia-elements/core/grid';
+import { Grid, GridCell, GridColumn } from '@nvidia-elements/core/grid';
 import '@nvidia-elements/core/grid/define.js';
 import '@nvidia-elements/core/icon-button/define.js';
 
@@ -62,7 +62,7 @@ describe(GridColumn.metadata.tag, () => {
     await elementIsStable(columns[0]);
     const sheets = (grid.getRootNode() as Document).adoptedStyleSheets;
     expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('--justify-content: center')).toBe(true);
-    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nve-grid-column:nth-child(1)')).toBe(true);
+    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nve-grid-column:nth-of-type(1)')).toBe(true);
   });
 
   it('should update fixed left column position', async () => {
@@ -170,7 +170,7 @@ describe(GridColumn.metadata.tag, () => {
     await elementIsStable(grid);
     let sheets = (grid.getRootNode() as Document).adoptedStyleSheets;
 
-    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nth-child(1)')).toBe(true);
+    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nth-of-type(1)')).toBe(true);
     expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('position: sticky')).toBe(true);
 
     columns[0].ariaColIndex = '3';
@@ -178,7 +178,7 @@ describe(GridColumn.metadata.tag, () => {
     await elementIsStable(grid);
     sheets = (grid.getRootNode() as Document).adoptedStyleSheets;
 
-    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nth-child(3)')).toBe(true);
+    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nth-of-type(3)')).toBe(true);
     expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('position: sticky')).toBe(true);
   });
 
@@ -187,14 +187,65 @@ describe(GridColumn.metadata.tag, () => {
     await elementIsStable(columns[0]);
     let sheets = (grid.getRootNode() as Document).adoptedStyleSheets;
 
-    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nth-child(1)')).toBe(true);
+    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nth-of-type(1)')).toBe(true);
     expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('--justify-content: center')).toBe(true);
 
     columns[0].ariaColIndex = '2';
     await elementIsStable(columns[0]);
     sheets = (grid.getRootNode() as Document).adoptedStyleSheets;
 
-    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nth-child(2)')).toBe(true);
+    expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('nth-of-type(2)')).toBe(true);
     expect(sheets[sheets.length - 1].cssRules[0].cssText.includes('--justify-content: center')).toBe(true);
+  });
+});
+
+describe(GridColumn.metadata.tag, () => {
+  let fixture: HTMLElement;
+  let grid: Grid;
+  let columns: GridColumn[];
+  let cells: GridCell[];
+
+  beforeEach(async () => {
+    fixture = await createFixture(html`
+      <nve-grid>
+        <template></template>
+        <nve-grid-header>
+          <template></template>
+          <nve-grid-column>column 1</nve-grid-column>
+          <nve-grid-column>column 2</nve-grid-column>
+          <nve-grid-column>column 3</nve-grid-column>
+        </nve-grid-header>
+        <nve-grid-row>
+          <template></template>
+          <nve-grid-cell>cell 1-1</nve-grid-cell>
+          <nve-grid-cell>cell 1-2</nve-grid-cell>
+          <nve-grid-cell>cell 1-3</nve-grid-cell>
+        </nve-grid-row>
+      </nve-grid>
+    `);
+    grid = fixture.querySelector(Grid.metadata.tag);
+    columns = Array.from(fixture.querySelectorAll(GridColumn.metadata.tag));
+    cells = Array.from(fixture.querySelectorAll(GridCell.metadata.tag));
+    await elementIsStable(columns[0]);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('should update column positions when ariaColIndex changes when using <template>', async () => {
+    await elementIsStable(columns[0]);
+    columns[0].position = 'fixed';
+    await elementIsStable(columns[0]);
+    await elementIsStable(grid);
+    const sheets = (grid.getRootNode() as Document).adoptedStyleSheets;
+    const positionRule = sheets[sheets.length - 1].cssRules[0].cssText;
+
+    expect(positionRule).toContain('nve-grid-column:nth-of-type(1)');
+    expect(positionRule).toContain('nve-grid-cell:nth-of-type(1)');
+    expect(positionRule).not.toContain('nth-child');
+    expect(positionRule).toContain('position: sticky');
+    expect(getComputedStyle(columns[0]).position).toBe('sticky');
+    expect(getComputedStyle(cells[0]).position).toBe('sticky');
   });
 });
