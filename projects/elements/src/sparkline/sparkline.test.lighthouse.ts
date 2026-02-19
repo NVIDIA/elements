3e885d49eebf1d8173efffb67ca5328464f15a4a
John Yanarella <jyanarella@nvidia.com>
2026-02-19 15:14:25 -0800

    feat(elements): add sparkline element
    Signed-off-by: John Yanarella <jyanarella@nvidia.com>

---
+++ projects/elements/src/sparkline/sparkline.test.lighthouse.ts
@@ -0,0 +1,18 @@
+import { expect, test, describe } from 'vitest';
+import { lighthouseRunner } from '@internals/vite';
+
+describe('sparkline lighthouse report', () => {
+  test('sparkline should meet lighthouse benchmarks', async () => {
+    const report = await lighthouseRunner.getReport('nve-sparkline', /* html */`
+      <nve-sparkline data="[4, 7, 5, 8, 6, 10]"></nve-sparkline>
+      <script type="module">
+        import '@nvidia-elements/core/sparkline/define.js';
+      </script>
+    `);
+
+    expect(report.scores.performance).toBe(100);
+    expect(report.scores.accessibility).toBe(100);
+    expect(report.scores.bestPractices).toBe(100);
+    expect(report.payload.javascript.kb).toBeLessThan(12.1);
+  });
+});
