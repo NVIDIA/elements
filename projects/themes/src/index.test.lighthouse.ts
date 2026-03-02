8df25d57093fdc5443402fb299a217b6930422aa
Cory Rylan <crylan@nvidia.com>
2026-03-02 11:10:35 -0600

    chore(themes): align tsconfig
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/themes/src/index.test.lighthouse.ts
+++ projects/themes/src/index.test.lighthouse.ts
@@ -18,27 +18,27 @@ describe('lighthouse report', () => {
   });
 
   test('index.css should remain within compressed bundle limits', async () => {
-    expect(report.payload.css.requests['index.css'].kb).toBeLessThan(6.1);
+    expect(report.payload.css.requests['index.css']!.kb).toBeLessThan(6.1);
   });
 
   test('compact.css should remain within compressed bundle limits', async () => {
-    expect(report.payload.css.requests['compact.css'].kb).toBeLessThan(0.5);
+    expect(report.payload.css.requests['compact.css']!.kb).toBeLessThan(0.5);
   });
 
   test('dark.css should remain within compressed bundle limits', async () => {
-    expect(report.payload.css.requests['dark.css'].kb).toBeLessThan(4.6);
+    expect(report.payload.css.requests['dark.css']!.kb).toBeLessThan(4.6);
   });
 
   test('high-contrast.css should remain within compressed bundle limits', async () => {
-    expect(report.payload.css.requests['high-contrast.css'].kb).toBeLessThan(1);
+    expect(report.payload.css.requests['high-contrast.css']!.kb).toBeLessThan(1);
   });
 
   test('reduced-motion.css should remain within compressed bundle limits', async () => {
-    expect(report.payload.css.requests['reduced-motion.css'].kb).toBeLessThan(0.7);
+    expect(report.payload.css.requests['reduced-motion.css']!.kb).toBeLessThan(0.7);
   });
 
   test('debug.css should remain within compressed bundle limits', async () => {
-    expect(report.payload.css.requests['debug.css'].kb).toBeLessThan(0.75);
+    expect(report.payload.css.requests['debug.css']!.kb).toBeLessThan(0.75);
   });
 });
 
@@ -53,6 +53,6 @@ describe('lighthouse report', () => {
   });
 
   test('bundles/index.css should remain within compressed bundle limits', async () => {
-    expect(report.payload.css.requests['index.css'].kb).toBeLessThan(9.8);
+    expect(report.payload.css.requests['index.css']!.kb).toBeLessThan(9.8);
   });
 });
