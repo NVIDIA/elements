4a2326138b5e9260fac84ec39663c94e8c19d003
Cory Rylan <crylan@nvidia.com>
2026-03-02 12:11:43 -0600

    chore(markdown): align tsconfig
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/labs/markdown/src/declarations.d.ts
+++ projects/labs/markdown/src/declarations.d.ts
@@ -2,3 +2,14 @@ declare module '*.css?inline' {
   const content: string;
   export = content;
 }
+
+declare module 'markdown-it' {
+  interface MarkdownIt {
+    render(markdown: string): string;
+  }
+  interface MarkdownItConstructor {
+    new (config?: Record<string, unknown>): MarkdownIt;
+  }
+  const MarkdownIt: MarkdownItConstructor;
+  export default MarkdownIt;
+}
