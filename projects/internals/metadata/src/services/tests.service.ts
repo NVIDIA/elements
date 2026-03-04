ba89856b6213484a55e97b6299235c3d0e94908e
Cory Rylan <crylan@nvidia.com>
2026-03-04 10:59:18 -0600

    chore(internals): update metadata to tsconfig and remove runtime data
    - align tsconfigs
    - remove unnecessary runtime metadata logic
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/internals/metadata/src/services/tests.service.ts
+++ projects/internals/metadata/src/services/tests.service.ts
@@ -1,18 +1,15 @@
 import type { ProjectsTestSummary } from '../utils/reports.js';
 
 export class TestsService {
-  static #tests = null;
+  static #tests: ProjectsTestSummary = {
+    created: '',
+    projects: {}
+  };
 
   static async getData(): Promise<ProjectsTestSummary> {
-    if (!TestsService.#tests) {
-      try {
-        TestsService.#tests = (await import('../../static/tests.json', { with: { type: 'json' } })).default;
-      } catch {
-        /* istanbul ignore next -- @preserve */
-        TestsService.#tests = await fetch(
-          'https://NVIDIA.github.io/elements/metadata/tests.json'
-        ).then(res => res.json());
-      }
+    if (TestsService.#tests.created === '') {
+      TestsService.#tests = (await import('../../static/tests.json', { with: { type: 'json' } }))
+        .default as unknown as ProjectsTestSummary;
     }
     return TestsService.#tests;
   }
