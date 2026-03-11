d96d7e728438b17ad1da198d99b3210652e9f00f
Cory Rylan <crylan@nvidia.com>
2026-03-11 18:42:11 -0500

    chore(ci): update lighthouse tests
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/page-loader/page-loader.test.lighthouse.ts
+++ projects/elements/src/page-loader/page-loader.test.lighthouse.ts
@@ -13,6 +13,6 @@ describe('page-loader lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(18.6);
+    expect(report.payload.javascript.kb).toBeLessThan(18.8);
   });
 });
