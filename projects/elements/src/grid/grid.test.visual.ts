58c7a236d8544c276f39fafba13c97e462fdf731
Cory Rylan <crylan@nvidia.com>
2026-03-20 10:34:48 -0500

    fix(core): grid column max-content
    - Introduced multiple new visual assets for grid components, including various alignment, container, and selection styles in both light and dark themes.
    - Updated the visual test structure to include new test cases for multi-select and single-select grid components, ensuring they match the visual baseline.
    - Refactored the template generation functions for better organization and readability.
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/elements/src/grid/grid.test.visual.ts
+++ projects/elements/src/grid/grid.test.visual.ts
@@ -1,19 +1,7 @@
 import { expect, test, describe } from 'vitest';
 import { visualRunner } from '@internals/vite';
 
-describe('grid visual', () => {
-  test('grid should match visual baseline', async () => {
-    const report = await visualRunner.render('grid', template());
-    expect(report.maxDiffPercentage).toBeLessThan(1);
-  });
-
-  test('grid should match visual baseline dark theme', async () => {
-    const report = await visualRunner.render('grid.dark', template('dark'));
-    expect(report.maxDiffPercentage).toBeLessThan(1);
-  });
-});
-
-function template(theme: '' | 'dark' = '') {
+function head(theme: '' | 'dark' = '') {
   return /* html */ `
   <script type="module">
     import '@nvidia-elements/core/grid/define.js';
@@ -23,14 +11,15 @@ function template(theme: '' | 'dark' = '') {
     document.documentElement.setAttribute('nve-theme', '${theme}');
   </script>
   <style>
-    body {
-      min-height: 4600px;
-    }
-
     nve-grid-row {
       content-visibility: visible !important;
     }
-  </style>
+  </style>`;
+}
+
+function template(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       ${Array(5)
@@ -51,9 +40,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- multi select -->
+function multiSelect(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       <nve-grid-column width="max-content" position="fixed">
@@ -84,9 +76,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- single select -->
+function singleSelect(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       <nve-grid-column width="max-content" position="fixed"></nve-grid-column>
@@ -113,9 +108,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- row action -->
+function rowAction(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       ${Array(5)
@@ -140,9 +138,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- footer scroll height -->
+function footerScrollHeight(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid style="--scroll-height: 255px">
     <nve-grid-header>
       ${Array(5)
@@ -166,9 +167,12 @@ function template(theme: '' | 'dark' = '') {
     <nve-grid-footer>
       •︎•︎•︎•︎•︎•︎
     </nve-grid-footer>
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- column width -->
+function columnWidth(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       <nve-grid-column width="80px">•︎•︎•︎•︎•︎•︎</nve-grid-column>
@@ -190,9 +194,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- column fixed -->
+function columnFixed(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       ${Array(5)
@@ -216,9 +223,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- column multi fixed -->
+function columnMultiFixed(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       ${Array(5)
@@ -242,9 +252,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- column stacked fixed -->
+function columnStackedFixed(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       <nve-grid-column position="fixed" width="100px">•︎•︎•︎•︎•︎•︎</nve-grid-column>
@@ -269,9 +282,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- stripe -->
+function stripe(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid stripe>
     <nve-grid-header>
       ${Array(5)
@@ -292,9 +308,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- placeholder -->
+function placeholder(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid style="min-height: 300px">
     <nve-grid-header>
       <nve-grid-column></nve-grid-column>
@@ -302,9 +321,12 @@ function template(theme: '' | 'dark' = '') {
     <nve-grid-placeholder>
       •︎•︎•︎•︎•︎•︎
     </nve-grid-placeholder>
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- content wrap -->
+function contentWrap(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       ${Array(5)
@@ -328,9 +350,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- column align center -->
+function columnAlignCenter(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       ${Array(5)
@@ -351,9 +376,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- column align end -->
+function columnAlignEnd(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid>
     <nve-grid-header>
       ${Array(5)
@@ -374,9 +402,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- container full -->
+function containerFull(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid container="full">
     <nve-grid-header>
       ${Array(5)
@@ -397,9 +428,12 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
+  </nve-grid>`;
+}
 
-  <!-- container flat -->
+function containerFlat(theme: '' | 'dark' = '') {
+  return /* html */ `
+  ${head(theme)}
   <nve-grid container="flat">
     <nve-grid-header>
       ${Array(5)
@@ -420,6 +454,167 @@ function template(theme: '' | 'dark' = '') {
     `
       )
       .join('')}
-  </nve-grid>
-  `;
+  </nve-grid>`;
 }
+
+describe('grid visual', () => {
+  test('grid should match visual baseline', async () => {
+    const report = await visualRunner.render('grid', template());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.dark', template('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid multi select should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.multi-select', multiSelect());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid multi select should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.multi-select.dark', multiSelect('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid single select should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.single-select', singleSelect());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid single select should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.single-select.dark', singleSelect('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid row action should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.row-action', rowAction());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid row action should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.row-action.dark', rowAction('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid footer scroll height should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.footer-scroll-height', footerScrollHeight());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid footer scroll height should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.footer-scroll-height.dark', footerScrollHeight('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column width should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.column-width', columnWidth());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column width should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.column-width.dark', columnWidth('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column fixed should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.column-fixed', columnFixed());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column fixed should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.column-fixed.dark', columnFixed('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column multi fixed should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.column-multi-fixed', columnMultiFixed());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column multi fixed should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.column-multi-fixed.dark', columnMultiFixed('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column stacked fixed should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.column-stacked-fixed', columnStackedFixed());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column stacked fixed should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.column-stacked-fixed.dark', columnStackedFixed('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid stripe should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.stripe', stripe());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid stripe should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.stripe.dark', stripe('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid placeholder should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.placeholder', placeholder());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid placeholder should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.placeholder.dark', placeholder('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid content wrap should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.content-wrap', contentWrap());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid content wrap should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.content-wrap.dark', contentWrap('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column align center should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.column-align-center', columnAlignCenter());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column align center should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.column-align-center.dark', columnAlignCenter('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column align end should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.column-align-end', columnAlignEnd());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid column align end should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.column-align-end.dark', columnAlignEnd('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid container full should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.container-full', containerFull());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid container full should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.container-full.dark', containerFull('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid container flat should match visual baseline', async () => {
+    const report = await visualRunner.render('grid.container-flat', containerFlat());
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+
+  test('grid container flat should match visual baseline dark theme', async () => {
+    const report = await visualRunner.render('grid.container-flat.dark', containerFlat('dark'));
+    expect(report.maxDiffPercentage).toBeLessThan(1);
+  });
+});
