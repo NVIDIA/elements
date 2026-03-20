d61746df28168c56b72979f1b21c0e59d4670166
Cory Rylan <crylan@nvidia.com>
2026-03-20 00:15:17 -0500

    chore(forms): reduce cyclomatic complexity
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/forms/eslint.config.js
+++ projects/forms/eslint.config.js
@@ -2,15 +2,4 @@ import { elementsRecommended } from '@nvidia-elements/lint/eslint';
 import { browserTypescriptConfig, libraryConfig, litConfig } from '@internals/eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  ...elementsRecommended,
-  ...browserTypescriptConfig,
-  ...libraryConfig,
-  ...litConfig,
-  {
-    files: ['src/**/*.ts'],
-    rules: {
-      complexity: 'off' // todo
-    }
-  }
-];
+export default [...elementsRecommended, ...browserTypescriptConfig, ...libraryConfig, ...litConfig];
