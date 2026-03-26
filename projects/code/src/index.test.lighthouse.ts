af8a3045bc0072909f0d54bf189247086d7db806
Cory Rylan <crylan@nvidia.com>
2026-03-25 22:15:13 -0500

    fix(core): bundle side effects import
    - fixes issue where side effect imports were dropped in vite bundle builds preventing elements from being registered.
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/code/src/index.test.lighthouse.ts
+++ projects/code/src/index.test.lighthouse.ts
@@ -47,7 +47,7 @@ describe('lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.requests[Object.keys(report.payload.javascript.requests)[0]!]!.kb).toBeLessThan(31);
+    expect(report.payload.javascript.requests[Object.keys(report.payload.javascript.requests)[0]!]!.kb).toBeLessThan(36);
   });
 });
 
@@ -59,6 +59,6 @@ describe('lighthouse report', () => {
       </script>
     `);
 
-    expect(report.payload.javascript.kb).toBeLessThan(31.1);
+    expect(report.payload.javascript.kb).toBeLessThan(36);
   });
 });
