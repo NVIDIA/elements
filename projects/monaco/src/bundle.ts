160259cac781148ceed09f7ae96d7874cbca18a1
Cory Rylan <crylan@nvidia.com>
2026-04-07 14:02:04 -0500

    feat(monaco): add bundle support
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

---
+++ projects/monaco/src/bundle.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./declarations.d.ts" />
+
+import '@nvidia-elements/monaco/diff-editor/define.js';
+import '@nvidia-elements/monaco/diff-input/define.js';
+import '@nvidia-elements/monaco/editor/define.js';
+import '@nvidia-elements/monaco/input/define.js';
+import '@nvidia-elements/monaco/problems/define.js';
