b355d86dac589405f55b8900f978f037a50a5554
Cory Rylan <crylan@nvidia.com>
2026-04-07 16:07:45 -0500

    chore(ci): update lighthouse
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/monaco/src/diff-input/diff-input.test.lighthouse.ts
+++ projects/monaco/src/diff-input/diff-input.test.lighthouse.ts
@@ -13,7 +13,7 @@ describe('monaco-diff-input lighthouse report', () => {
     expect(report.scores.performance).toBeGreaterThanOrEqual(90);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(1320);
+    expect(report.payload.javascript.kb).toBeLessThan(1321);
     expect(report.payload.javascript.requests['index.js'].kb).toBeLessThan(14.2);
     expect(report.payload.javascript.requests['editor2.global.js'].kb).toBeLessThan(78);
     expect(report.payload.javascript.requests['editor2.main.js'].kb).toBeLessThan(24);
