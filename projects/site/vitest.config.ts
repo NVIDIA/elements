1f4a84a52e777e9bc56656fd91a56ee2332f1400
Cory Rylan <crylan@nvidia.com>
2026-09-18 11:54:17 -0500

    chore(docs): add browser testing support
    - adds browser/client side testing support
    - adds unit tests to doc search and fixes layout performance/thrash issue from events triggering initial page layout recalc
    
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/site/vitest.config.ts
+++ projects/site/vitest.config.ts
@@ -4,6 +4,7 @@ import { libraryNodeTestConfig } from '@internals/vite/configs/test.node.js';
 export default mergeConfig(libraryNodeTestConfig, {
   root: import.meta.dirname,
   test: {
-    include: ['./src/**/*.test.ts']
+    include: ['./src/**/*.test.ts'],
+    exclude: ['./src/**/*.browser.test.ts']
   }
 });
